18ed40046359136238e8339797de9e83022cf572
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2024-10-30 18:49:16 +0900

    firebaseの設定ファイルをDartで読み込みし、初期化

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'infrastructure/firebase/firebase_options_dev.dart' as dev;
+import 'infrastructure/firebase/firebase_options_prod.dart' as prod;
 import 'presentation/app.dart';
 
-void main() {
+const flavor = String.fromEnvironment('flavor');
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final firebaseOptions = flavor == 'prod'
+      ? prod.DefaultFirebaseOptions.currentPlatform
+      : dev.DefaultFirebaseOptions.currentPlatform;
+  await Firebase.initializeApp(options: firebaseOptions);
   runApp(const App());
 }
