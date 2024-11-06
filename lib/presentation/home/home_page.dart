6240daa28ebe4b83142c008234d9f392144d2362
shi0n0 <shiono.yui1080@gmail.com>
2024-11-06 16:51:10 +0900

    keyを削除

--- lib/presentation/home/home_page.dart
+++ lib/presentation/home/home_page.dart
@@ -9,7 +9,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: SafeArea(
-        child: ComplicatedImageCarousel(key: Key('carousel_key')),
+        child: ComplicatedImageCarousel(),
       ),
     );
   }
