a431d51d00b3441a4101b9f133d74cd2cb985ca9
Tomoya <92212932+PenginPenPen@users.noreply.github.com>
2024-11-07 13:58:20 +0900

    ボタン機能の追加

--- lib/presentation/components/topicCard.dart
+++ lib/presentation/components/topicCard.dart
@@ -18,68 +18,74 @@ class topicCard extends StatelessWidget {
     double cardHeight = screenHeight * 0.18;
     double cardWidth = cardHeight * (5 / 4);
 
-    return Padding(
-      padding: const EdgeInsets.only(left: 5),
-      child: Container(
-        height: cardHeight,
-        width: cardWidth,
-        decoration: BoxDecoration(
-          color: Color.fromRGBO(255, 255, 255, 0.5),
-          borderRadius: BorderRadius.circular(20),
-        ),
-        child: Column(
-          children: [
-            Container(
-              height: cardHeight * 0.75,
-              width: cardWidth,
-              decoration: BoxDecoration(
-                borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(20),
-                  topRight: Radius.circular(20),
-                ),
-                image: DecorationImage(
-                  image: AssetImage(imagePath),
-                  fit: BoxFit.cover,
-                ),
-              ),
+    return GestureDetector(
+        onTap: () {
+          print("タップ" + title);
+        },
+        child: Padding(
+          padding: const EdgeInsets.only(left: 5),
+          child: Container(
+            height: cardHeight,
+            width: cardWidth,
+            decoration: BoxDecoration(
+              color: Color.fromRGBO(255, 255, 255, 0.5),
+              borderRadius: BorderRadius.circular(20),
             ),
-            Container(
-                height: cardHeight * 0.25,
-                width: cardWidth,
-                padding: const EdgeInsets.only(left: 10, top: 7),
-                decoration: BoxDecoration(
-                  color: Colors.grey[300],
-                  borderRadius: const BorderRadius.only(
-                    bottomLeft: Radius.circular(20),
-                    bottomRight: Radius.circular(20),
+            child: Column(
+              children: [
+                Container(
+                  height: cardHeight * 0.75,
+                  width: cardWidth,
+                  decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20),
+                    ),
+                    image: DecorationImage(
+                      image: AssetImage(imagePath),
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 3, bottom: 8),
-                  child: Row(
-                    children: [
-                      Text(
-                        title,
-                        style: const TextStyle(
-                          fontSize: 16,
-                          color: Colors.black87,
-                          fontWeight: FontWeight.bold,
-                        ),
+                Container(
+                    height: cardHeight * 0.25,
+                    width: cardWidth,
+                    padding: const EdgeInsets.only(left: 10, top: 7),
+                    decoration: BoxDecoration(
+                      color: Colors.grey[300],
+                      borderRadius: const BorderRadius.only(
+                        bottomLeft: Radius.circular(20),
+                        bottomRight: Radius.circular(20),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 5, top: 5),
-                        child: Text(
-                          number_ofTopics.toString() + "件~",
-                          style: const TextStyle(
-                              fontSize: 10, fontWeight: FontWeight.bold),
-                        ),
-                      )
-                    ],
-                  ),
-                )),
-          ],
-        ),
-      ),
-    );
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 3, bottom: 8),
+                      child: Row(
+                        children: [
+                          Text(
+                            title,
+                            style: const TextStyle(
+                              fontSize: 16,
+                              color: Colors.black87,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(
+                              left: 5,
+                            ),
+                            child: Text(
+                              number_ofTopics.toString() + "件~",
+                              style: const TextStyle(
+                                  fontSize: 10, fontWeight: FontWeight.bold),
+                            ),
+                          )
+                        ],
+                      ),
+                    )),
+              ],
+            ),
+          ),
+        ));
   }
 }
--- lib/presentation/home/popular_topics.dart
+++ lib/presentation/home/popular_topics.dart
@@ -10,21 +10,31 @@ class PopularTopics extends StatelessWidget {
       Padding(
         padding: EdgeInsets.only(left: 15, bottom: 0),
         child: Align(
-          alignment: Alignment.centerLeft,
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text(
-                "人気のトピック",
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-              Icon(Icons.chevron_right, size: 40),
-            ],
-          ),
-        ),
+            alignment: Alignment.centerLeft,
+            child: GestureDetector(
+                onTap: () {
+                  print("人気のトピック");
+                },
+                child: Padding(
+                  padding: EdgeInsets.only(bottom: 10),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        "人気のトピック",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 3),
+                        child: Icon(Icons.arrow_forward_ios, size: 23),
+                      )
+                    ],
+                  ),
+                ))),
       ),
       Padding(
-        padding: const EdgeInsets.only(left: 10),
+        padding: EdgeInsets.only(left: 10),
         child: SingleChildScrollView(
           scrollDirection: Axis.horizontal,
           child: Row(
@@ -32,22 +42,42 @@ class PopularTopics extends StatelessWidget {
               topicCard(
                 title: "アニメ",
                 imagePath: "images/anime.png",
-                number_ofTopics: 999,
+                number_ofTopics: 30,
               ),
               topicCard(
                 title: "ショッピング",
                 imagePath: "images/shopping.png",
-                number_ofTopics: 999,
+                number_ofTopics: 208,
               ),
               topicCard(
                 title: "アクティビティー",
                 imagePath: "images/activity.png",
-                number_ofTopics: 999,
+                number_ofTopics: 16,
               ),
               topicCard(
                 title: "グルメ",
                 imagePath: "images/gourmet.png",
-                number_ofTopics: 999,
+                number_ofTopics: 430,
+              ),
+              topicCard(
+                title: "グルメ",
+                imagePath: "images/gourmet.png",
+                number_ofTopics: 430,
+              ),
+              topicCard(
+                title: "グルメ",
+                imagePath: "images/gourmet.png",
+                number_ofTopics: 430,
+              ),
+              topicCard(
+                title: "グルメ",
+                imagePath: "images/gourmet.png",
+                number_ofTopics: 430,
+              ),
+              topicCard(
+                title: "グルメ",
+                imagePath: "images/gourmet.png",
+                number_ofTopics: 430,
               ),
             ],
           ),
