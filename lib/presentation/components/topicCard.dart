f5e1ab9db00f6bd99f6846b4333fa905bd131e63
Tomoya <92212932+PenginPenPen@users.noreply.github.com>
2024-11-07 12:49:05 +0900

    共通化したウィジェットをコンポーネントファイルに移動

---
+++ lib/presentation/components/topicCard.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+class topicCard extends StatelessWidget {
+  final String title;
+  final String imagePath;
+  final int number_ofTopics;
+
+  const topicCard({
+    super.key,
+    required this.title,
+    required this.imagePath,
+    required this.number_ofTopics,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    double cardHeight = screenHeight * 0.18;
+    double cardWidth = cardHeight * (5 / 4);
+
+    return Padding(
+      padding: const EdgeInsets.only(left: 5),
+      child: Container(
+        height: cardHeight,
+        width: cardWidth,
+        decoration: BoxDecoration(
+          color: Color.fromRGBO(255, 255, 255, 0.5),
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Column(
+          children: [
+            Container(
+              height: cardHeight * 0.75,
+              width: cardWidth,
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                ),
+                image: DecorationImage(
+                  image: AssetImage(imagePath),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Container(
+                height: cardHeight * 0.25,
+                width: cardWidth,
+                padding: const EdgeInsets.only(left: 10, top: 7),
+                decoration: BoxDecoration(
+                  color: Colors.grey[300],
+                  borderRadius: const BorderRadius.only(
+                    bottomLeft: Radius.circular(20),
+                    bottomRight: Radius.circular(20),
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 3, bottom: 8),
+                  child: Row(
+                    children: [
+                      Text(
+                        title,
+                        style: const TextStyle(
+                          fontSize: 16,
+                          color: Colors.black87,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 5, top: 5),
+                        child: Text(
+                          number_ofTopics.toString() + "件~",
+                          style: const TextStyle(
+                              fontSize: 10, fontWeight: FontWeight.bold),
+                        ),
+                      )
+                    ],
+                  ),
+                )),
+          ],
+        ),
+      ),
+    );
+  }
+}
