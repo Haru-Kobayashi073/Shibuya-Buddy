677705fe39156b1edaf581de9979b03560166927
Tomoya <92212932+PenginPenPen@users.noreply.github.com>
2024-11-07 16:07:06 +0900

    画像の指定の仕方を変更、printをdebugprintに変更、constの追加

--- lib/presentation/home/popular_topics.dart
+++ lib/presentation/home/popular_topics.dart
@@ -1,88 +1,77 @@
 import 'package:flutter/material.dart';
-import '../components/topicCard.dart';
+import '/gen/assets.gen.dart';
+import '../components/topic_card.dart';
 
 class PopularTopics extends StatelessWidget {
   const PopularTopics({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      Padding(
-        padding: EdgeInsets.only(left: 15, bottom: 0),
-        child: Align(
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(
+            left: 15,
+          ),
+          child: Align(
             alignment: Alignment.centerLeft,
             child: GestureDetector(
-                onTap: () {
-                  print("人気のトピック");
-                },
-                child: Padding(
-                  padding: EdgeInsets.only(bottom: 10),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Text(
-                        "人気のトピック",
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.bold),
+              onTap: () {
+                debugPrint('人気のトピック');
+              },
+              child: const Padding(
+                padding: EdgeInsets.only(bottom: 10),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      '人気のトピック',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
                       ),
-                      Padding(
-                        padding: EdgeInsets.only(top: 3),
-                        child: Icon(Icons.arrow_forward_ios, size: 23),
-                      )
-                    ],
-                  ),
-                ))),
-      ),
-      Padding(
-        padding: EdgeInsets.only(left: 10),
-        child: SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Row(
-            children: [
-              topicCard(
-                title: "アニメ",
-                imagePath: "images/anime.png",
-                number_ofTopics: 30,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 3),
+                      child: Icon(Icons.arrow_forward_ios, size: 23),
+                    ),
+                  ],
+                ),
               ),
-              topicCard(
-                title: "ショッピング",
-                imagePath: "images/shopping.png",
-                number_ofTopics: 208,
-              ),
-              topicCard(
-                title: "アクティビティー",
-                imagePath: "images/activity.png",
-                number_ofTopics: 16,
-              ),
-              topicCard(
-                title: "グルメ",
-                imagePath: "images/gourmet.png",
-                number_ofTopics: 430,
-              ),
-              topicCard(
-                title: "グルメ",
-                imagePath: "images/gourmet.png",
-                number_ofTopics: 430,
-              ),
-              topicCard(
-                title: "グルメ",
-                imagePath: "images/gourmet.png",
-                number_ofTopics: 430,
-              ),
-              topicCard(
-                title: "グルメ",
-                imagePath: "images/gourmet.png",
-                number_ofTopics: 430,
-              ),
-              topicCard(
-                title: "グルメ",
-                imagePath: "images/gourmet.png",
-                number_ofTopics: 430,
-              ),
-            ],
+            ),
           ),
         ),
-      )
-    ]);
+        Padding(
+          padding: const EdgeInsets.only(left: 10),
+          child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                TopicCard(
+                  title: 'アニメ',
+                  imagePath: Assets.images.anime.path,
+                  numberOfTopics: 30,
+                ),
+                TopicCard(
+                  title: 'ショッピング',
+                  imagePath: Assets.images.shopping.path,
+                  numberOfTopics: 208,
+                ),
+                TopicCard(
+                  title: 'アクティビティー',
+                  imagePath: Assets.images.activity.path,
+                  numberOfTopics: 16,
+                ),
+                TopicCard(
+                  title: 'グルメ',
+                  imagePath: Assets.images.gourmet.path,
+                  numberOfTopics: 430,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
