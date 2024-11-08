5be4417dc79b37b0aa0252c2567b33af70d39fd5
ryunosuke_sg4i <azatose8684@gmail.com>
2024-11-08 18:35:44 +0900

    category_tagsを作成

---
+++ lib/presentation/home/components/category_tags.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import '../../../utils/styles/app_color.dart';
+import '../../../utils/styles/app_text_style.dart';
+
+class TagFeature extends StatelessWidget {
+
+  const TagFeature({super.key, required this.tags});
+  final List<String> tags;
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      spacing: 8,
+      runSpacing: 4,
+      children: tags.map((tag) {
+        return Container(
+          padding:
+            const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+          decoration: BoxDecoration(
+            color: AppColor.blue50Background,
+            borderRadius: BorderRadius.circular(4),
+          ),
+          child: Text(
+            tag,
+            style: AppTextStyle.textStyle.copyWith(
+              fontSize: 10,
+            ),
+          ),
+        );
+      }).toList(),
+    );
+  }
+}
