690f29d135f17effdf512aa59a0d187d32eb584c
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2024-11-08 15:40:24 +0900

    全体のテーマを簡単に設定

--- lib/presentation/app.dart
+++ lib/presentation/app.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+
 import '../utils/routes/app_router.dart';
+import '../utils/styles/app_color.dart';
+import '../utils/styles/app_text_style.dart';
 
 class App extends ConsumerWidget {
   const App({super.key});
@@ -12,6 +15,28 @@ class App extends ConsumerWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        scaffoldBackgroundColor: AppColor.white,
+        appBarTheme: const AppBarTheme(backgroundColor: AppColor.white),
+        navigationBarTheme: NavigationBarThemeData(
+          backgroundColor: AppColor.white,
+          indicatorColor: AppColor.blue50Background,
+          labelTextStyle: WidgetStateProperty.resolveWith((states) {
+            final style = AppTextStyle.textStyle
+                .copyWith(color: AppColor.blue900Tertiary);
+            if (states.contains(WidgetState.selected)) {
+              return style.copyWith(
+                color: AppColor.blue900Tertiary,
+                fontWeight: FontWeight.bold,
+              );
+            }
+            return style;
+          }),
+          iconTheme: WidgetStateProperty.all(
+            const IconThemeData(
+              color: AppColor.blue900Tertiary,
+            ),
+          ),
+        ),
         useMaterial3: true,
       ),
       routerDelegate: goRouter.routerDelegate,
