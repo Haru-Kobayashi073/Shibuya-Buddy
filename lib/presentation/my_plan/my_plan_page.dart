b7fbd0d897c9103fa207397146821924b41f6b6c
Tomoya <92212932+PenginPenPen@users.noreply.github.com>
2024-11-08 19:08:23 +0900

    Tabの追加

--- lib/presentation/my_plan/my_plan_page.dart
+++ lib/presentation/my_plan/my_plan_page.dart
@@ -1,10 +1,52 @@
 import 'package:flutter/material.dart';
 
+import '../../utils/styles/app_color.dart';
+import '../../utils/styles/app_text_style.dart';
+import 'bookmarkplan.dart';
+import 'created_plan.dart';
+
 class MyPlanPage extends StatelessWidget {
   const MyPlanPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'マイプラン',
+            style: AppTextStyle.textStyle.copyWith(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          bottom: TabBar(
+            labelStyle: AppTextStyle.textStyle
+                .copyWith(fontSize: 14, fontWeight: FontWeight.bold),
+            unselectedLabelStyle: AppTextStyle.textStyle
+                .copyWith(fontSize: 14, fontWeight: FontWeight.bold),
+            labelColor: AppColor.black,
+            unselectedLabelColor: AppColor.black,
+            indicatorColor: AppColor.blue800Secondary,
+            dividerColor: AppColor.black,
+            tabs: const <Widget>[
+              Tab(
+                text: '作成したプラン',
+              ),
+              Tab(
+                text: 'ブックマーク',
+              ),
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: [
+            CreatedPlan(),
+            Bookmarkplan(),
+          ],
+        ),
+      ),
+    );
   }
 }
