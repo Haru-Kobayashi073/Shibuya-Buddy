9ba49cc27f8bec3bf35384e6117b79c7f06440f5
ryunosuke_sg4i <azatose8684@gmail.com>
2024-11-08 18:37:56 +0900

    outlinebuttonをtagsに変更 paddingを変更 余計なSizedboxとpaddingを削除 fontsizeを変更

--- lib/presentation/home/components/recent_plan.dart
+++ lib/presentation/home/components/recent_plan.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
-import '../../../utils/styles/app_color.dart';
+// import '../../../utils/styles/app_color.dart';
 import '../../../utils/styles/app_text_style.dart';
+import 'category_tags.dart';
 
 class RecentPlan extends StatelessWidget {
   const RecentPlan({
@@ -13,92 +14,46 @@ class RecentPlan extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(12),
-                child: Image.network(
-                  'https://placehold.jp/80x50.png',
-                  width: 80,
-                  height: 50,
-                  fit: BoxFit.cover,
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8),
+      child: Column(
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(right: 8),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: Image.network(
+                    'https://placehold.jp/80x50.png',
+                    width: 80,
+                    height: 50,
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
-            ),
-            Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(height: 6),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 4, 0, 0),
-                  child: Text(
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
                     label,
                     style: AppTextStyle.textStyle.copyWith(
-                      fontSize: 14,
+                      fontSize: 16,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
-                ),
-                const SizedBox(height: 6),
-                Row(
-                  children: [
-                    _buildTextButton(
-                      label: '所要時間: 1時間〜',
-                      onPressed: () {},
-                    ),
-                    _buildTextButton(
-                      label: '人数: １人〜',
-                      onPressed: () {},
-                    ),
-                    _buildTextButton(
-                      label: '＃ショッピング ',
-                      onPressed: () {},
-                    ),
-                    _buildTextButton(
-                      label: '＃アクティビティ',
-                      onPressed: () {},
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ],
-        ),
-      ],
-    );
-  }
-
-  Widget _buildTextButton({
-    required String label,
-    required VoidCallback onPressed,
-  }) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 2),
-      child: SizedBox(
-        width: 75,
-        height: 18,
-        child: TextButton(
-          onPressed: onPressed,
-          style: TextButton.styleFrom(
-            padding: EdgeInsets.zero,
-            backgroundColor: AppColor.blue50Background,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(4),
-            ),
+                  const Row(
+                    children: [
+                      TagFeature (tags: ['所要時間: 1時間〜','人数: １人','＃ショッピング'],),
+                    ],
+                  ),
+                ],
+              ),
+            ],
           ),
-          child: Text(
-            label,
-            style: AppTextStyle.textStyle.copyWith(
-              fontSize: 8,
-            ),
-          ),
-        ),
+        ],
       ),
     );
   }
