774f7121b8479296f3cf691d42a17c4577932519
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2024-11-10 01:09:27 +0900

    言語参照・変更のクラスを実装

---
+++ lib/utils/providers/locale/locale_service.dart
@@ -0,0 +1,39 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import '../../../i18n/strings.g.dart';
+import '../shared_preferences/shared_preferences_config.dart';
+import '../shared_preferences/shared_preferences_service.dart';
+
+part 'locale_service.g.dart';
+
+@riverpod
+class LocaleService extends _$LocaleService {
+  SharedPreferencesService get sharedPreferencesService =>
+      ref.read(sharedPreferencesServiceProvider.notifier);
+
+  @override
+  AppLocale build() {
+    final userLocale = sharedPreferencesService.getStringValue(
+      key: SharedPreferencesKey.languageCode,
+    );
+    return switch (userLocale) {
+      'en' => AppLocale.en,
+      'ja' => AppLocale.ja,
+      'zh' => AppLocale.zh,
+      _ => AppLocale.en,
+    };
+  }
+
+  // NOTE set new locale
+  Future<void> changeLocale(AppLocale newLocale) async {
+    state = newLocale;
+    await sharedPreferencesService.setStringValue(
+      key: SharedPreferencesKey.languageCode,
+      value: newLocale.name,
+    );
+    await LocaleSettings.setLocale(newLocale);
+  }
+
+  Future<void> changeLocaleToPreviousValue() async {
+    await LocaleSettings.setLocale(state);
+  }
+}
---
+++ lib/utils/providers/locale/locale_service.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'locale_service.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$localeServiceHash() => r'113456799e2a6ac452c400e4a7de4e45c80cf577';
+
+/// See also [LocaleService].
+@ProviderFor(LocaleService)
+final localeServiceProvider =
+    AutoDisposeNotifierProvider<LocaleService, AppLocale>.internal(
+  LocaleService.new,
+  name: r'localeServiceProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$localeServiceHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$LocaleService = AutoDisposeNotifier<AppLocale>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
