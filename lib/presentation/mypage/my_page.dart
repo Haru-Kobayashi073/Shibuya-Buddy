038666a4d3b13bdee087334728f8edb164d9e30d
Tomoya <92212932+PenginPenPen@users.noreply.github.com>
2024-11-11 18:19:12 +0900

    マイページの画面の作成

--- lib/presentation/mypage/my_page.dart
+++ lib/presentation/mypage/my_page.dart
@@ -1,10 +1,117 @@
 import 'package:flutter/material.dart';
 
+import '../../utils/styles/app_text_style.dart';
+import '../components/textArrowButton.dart';
+
 class MyPage extends StatelessWidget {
   const MyPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final screenWidth = MediaQuery.of(context).size.width;
+    // final screenHeight = MediaQuery.of(context).size.height;
+    final circleSize = screenWidth * 0.1;
+    return Scaffold(
+      body: Column(
+        children: [
+          // const Spacer(),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 50, left: 16, right: 16),
+              child: Row(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(right: 10),
+                    child: Container(
+                      width: circleSize,
+                      height: circleSize,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.black,
+                        // image: DecorationImage(
+                        //   fit: BoxFit.fill,
+                        //   image: AssetImage('/images/sample.jpg'),
+                        // ),
+                      ),
+                    ),
+                  ),
+                  // const SizedBox(width: 15),
+                  Text(
+                    'Name',
+                    style: AppTextStyle.textStyle.copyWith(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const Spacer(),
+                  const Icon(Icons.edit, size: 24),
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Container(),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 16),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                '設定',
+                style: AppTextStyle.textStyle.copyWith(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              padding: const EdgeInsets.all(16),
+              physics: const NeverScrollableScrollPhysics(),
+              children: <Widget>[
+                Textarrowbutton(
+                  text: 'アカウント',
+                  onTapCallback: () {
+                    debugPrint('TAP');
+                  },
+                ),
+                Textarrowbutton(
+                  text: '言語',
+                  onTapCallback: () {
+                    debugPrint('TAP');
+                  },
+                ),
+                Textarrowbutton(
+                  text: 'テーマ',
+                  onTapCallback: () {
+                    debugPrint('TAP');
+                  },
+                ),
+                Textarrowbutton(
+                  text: '利用規約・プライバシーポリシー',
+                  onTapCallback: () {
+                    debugPrint('TAP');
+                  },
+                ),
+                Textarrowbutton(
+                  text: 'このアプリについて',
+                  onTapCallback: () {
+                    debugPrint('TAP');
+                  },
+                ),
+                Textarrowbutton(
+                  text: '開発者について',
+                  onTapCallback: () {
+                    debugPrint('TAP');
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
