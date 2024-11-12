331fc5b16f4c3e8f71fc6d80993b0bb1a6e54d41
Tomoya <92212932+PenginPenPen@users.noreply.github.com>
2024-11-12 22:25:19 +0900

    デバッグ用に画面遷移の機能を一時的に追加

--- lib/presentation/home/home_page.dart
+++ lib/presentation/home/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+
+import '../setting/change_language.dart';
 import 'popular_topics.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,8 +13,21 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: PopularTopics(),
+    return Scaffold(
+      body: Column(
+        children: [
+          const PopularTopics(),
+          TextButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => ChangeLanguage()), //gorouterを使う
+              );
+            },
+            child: const Text('遷移'),
+          ),
+        ],
+      ),
     );
   }
 }
