054c90b6810c20df5e3ba3247ff0b4acb26737ff
Tomoya <92212932+PenginPenPen@users.noreply.github.com>
2024-11-14 02:49:23 +0900

    dart formatterを実行

--- lib/i18n/strings_en.g.dart
+++ lib/i18n/strings_en.g.dart
@@ -8,108 +8,130 @@ part of 'strings.g.dart';
 
 // Path: <root>
 typedef TranslationsEn = Translations; // ignore: unused_element
+
 class Translations implements BaseTranslations<AppLocale, Translations> {
-	/// Returns the current translations of the given [context].
-	///
-	/// Usage:
-	/// final t = Translations.of(context);
-	static Translations of(BuildContext context) => InheritedLocaleData.of<AppLocale, Translations>(context).translations;
+  /// Returns the current translations of the given [context].
+  ///
+  /// Usage:
+  /// final t = Translations.of(context);
+  static Translations of(BuildContext context) =>
+      InheritedLocaleData.of<AppLocale, Translations>(context).translations;
 
-	/// You can call this constructor and build your own translation instance of this locale.
-	/// Constructing via the enum [AppLocale.build] is preferred.
-	Translations({Map<String, Node>? overrides, PluralResolver? cardinalResolver, PluralResolver? ordinalResolver})
-		: assert(overrides == null, 'Set "translation_overrides: true" in order to enable this feature.'),
-		  $meta = TranslationMetadata(
-		    locale: AppLocale.en,
-		    overrides: overrides ?? {},
-		    cardinalResolver: cardinalResolver,
-		    ordinalResolver: ordinalResolver,
-		  ) {
-		$meta.setFlatMapFunction(_flatMapFunction);
-	}
+  /// You can call this constructor and build your own translation instance of this locale.
+  /// Constructing via the enum [AppLocale.build] is preferred.
+  Translations(
+      {Map<String, Node>? overrides,
+      PluralResolver? cardinalResolver,
+      PluralResolver? ordinalResolver})
+      : assert(overrides == null,
+            'Set "translation_overrides: true" in order to enable this feature.'),
+        $meta = TranslationMetadata(
+          locale: AppLocale.en,
+          overrides: overrides ?? {},
+          cardinalResolver: cardinalResolver,
+          ordinalResolver: ordinalResolver,
+        ) {
+    $meta.setFlatMapFunction(_flatMapFunction);
+  }
 
-	/// Metadata for the translations of <en>.
-	@override final TranslationMetadata<AppLocale, Translations> $meta;
+  /// Metadata for the translations of <en>.
+  @override
+  final TranslationMetadata<AppLocale, Translations> $meta;
 
-	/// Access flat map
-	dynamic operator[](String key) => $meta.getTranslation(key);
+  /// Access flat map
+  dynamic operator [](String key) => $meta.getTranslation(key);
 
-	late final Translations _root = this; // ignore: unused_field
+  late final Translations _root = this; // ignore: unused_field
 
-	// Translations
-	late final TranslationsNavigationBarEn NavigationBar = TranslationsNavigationBarEn._(_root);
-	late final TranslationsChangeLanguageEn ChangeLanguage = TranslationsChangeLanguageEn._(_root);
-	Map<String, String> get locales => {
-		'en': 'English',
-		'ja': 'Japanese',
-		'zh': 'Chinese',
-	};
+  // Translations
+  late final TranslationsNavigationBarEn NavigationBar =
+      TranslationsNavigationBarEn._(_root);
+  late final TranslationsChangeLanguageEn ChangeLanguage =
+      TranslationsChangeLanguageEn._(_root);
+  Map<String, String> get locales => {
+        'en': 'English',
+        'ja': 'Japanese',
+        'zh': 'Chinese',
+      };
 }
 
 // Path: NavigationBar
 class TranslationsNavigationBarEn {
-	TranslationsNavigationBarEn._(this._root);
+  TranslationsNavigationBarEn._(this._root);
 
-	final Translations _root; // ignore: unused_field
+  final Translations _root; // ignore: unused_field
 
-	// Translations
-	late final TranslationsNavigationBarItemsEn items = TranslationsNavigationBarItemsEn._(_root);
+  // Translations
+  late final TranslationsNavigationBarItemsEn items =
+      TranslationsNavigationBarItemsEn._(_root);
 }
 
 // Path: ChangeLanguage
 class TranslationsChangeLanguageEn {
-	TranslationsChangeLanguageEn._(this._root);
+  TranslationsChangeLanguageEn._(this._root);
 
-	final Translations _root; // ignore: unused_field
+  final Translations _root; // ignore: unused_field
 
-	// Translations
-	late final TranslationsChangeLanguageItemsEn items = TranslationsChangeLanguageItemsEn._(_root);
+  // Translations
+  late final TranslationsChangeLanguageItemsEn items =
+      TranslationsChangeLanguageItemsEn._(_root);
 }
 
 // Path: NavigationBar.items
 class TranslationsNavigationBarItemsEn {
-	TranslationsNavigationBarItemsEn._(this._root);
+  TranslationsNavigationBarItemsEn._(this._root);
 
-	final Translations _root; // ignore: unused_field
+  final Translations _root; // ignore: unused_field
 
-	// Translations
-	String get home => 'Home';
-	String get myPlan => 'My Plan';
-	String get myPage => 'My Page';
+  // Translations
+  String get home => 'Home';
+  String get myPlan => 'My Plan';
+  String get myPage => 'My Page';
 }
 
 // Path: ChangeLanguage.items
 class TranslationsChangeLanguageItemsEn {
-	TranslationsChangeLanguageItemsEn._(this._root);
+  TranslationsChangeLanguageItemsEn._(this._root);
 
-	final Translations _root; // ignore: unused_field
+  final Translations _root; // ignore: unused_field
 
-	// Translations
-	String get japanese => 'Japanese';
-	String get english => 'English';
-	String get chinese_simplified => 'Chinese (Simplified)';
-	String get chinese_traditional => 'Chinese (Traditional)';
-	String get language => 'Language';
+  // Translations
+  String get japanese => 'Japanese';
+  String get english => 'English';
+  String get chinese_simplified => 'Chinese (Simplified)';
+  String get chinese_traditional => 'Chinese (Traditional)';
+  String get language => 'Language';
 }
 
 /// Flat map(s) containing all translations.
 /// Only for edge cases! For simple maps, use the map function of this library.
 extension on Translations {
-	dynamic _flatMapFunction(String path) {
-		switch (path) {
-			case 'NavigationBar.items.home': return 'Home';
-			case 'NavigationBar.items.myPlan': return 'My Plan';
-			case 'NavigationBar.items.myPage': return 'My Page';
-			case 'ChangeLanguage.items.japanese': return 'Japanese';
-			case 'ChangeLanguage.items.english': return 'English';
-			case 'ChangeLanguage.items.chinese_simplified': return 'Chinese (Simplified)';
-			case 'ChangeLanguage.items.chinese_traditional': return 'Chinese (Traditional)';
-			case 'ChangeLanguage.items.language': return 'Language';
-			case 'locales.en': return 'English';
-			case 'locales.ja': return 'Japanese';
-			case 'locales.zh': return 'Chinese';
-			default: return null;
-		}
-	}
+  dynamic _flatMapFunction(String path) {
+    switch (path) {
+      case 'NavigationBar.items.home':
+        return 'Home';
+      case 'NavigationBar.items.myPlan':
+        return 'My Plan';
+      case 'NavigationBar.items.myPage':
+        return 'My Page';
+      case 'ChangeLanguage.items.japanese':
+        return 'Japanese';
+      case 'ChangeLanguage.items.english':
+        return 'English';
+      case 'ChangeLanguage.items.chinese_simplified':
+        return 'Chinese (Simplified)';
+      case 'ChangeLanguage.items.chinese_traditional':
+        return 'Chinese (Traditional)';
+      case 'ChangeLanguage.items.language':
+        return 'Language';
+      case 'locales.en':
+        return 'English';
+      case 'locales.ja':
+        return 'Japanese';
+      case 'locales.zh':
+        return 'Chinese';
+      default:
+        return null;
+    }
+  }
 }
-
--- lib/i18n/strings_zh_Hant.g.dart
+++ lib/i18n/strings_zh_Hant.g.dart
@@ -11,101 +11,138 @@ import 'strings.g.dart';
 
 // Path: <root>
 class TranslationsZhHant implements Translations {
-	/// You can call this constructor and build your own translation instance of this locale.
-	/// Constructing via the enum [AppLocale.build] is preferred.
-	TranslationsZhHant({Map<String, Node>? overrides, PluralResolver? cardinalResolver, PluralResolver? ordinalResolver})
-		: assert(overrides == null, 'Set "translation_overrides: true" in order to enable this feature.'),
-		  $meta = TranslationMetadata(
-		    locale: AppLocale.zhHant,
-		    overrides: overrides ?? {},
-		    cardinalResolver: cardinalResolver,
-		    ordinalResolver: ordinalResolver,
-		  ) {
-		$meta.setFlatMapFunction(_flatMapFunction);
-	}
+  /// You can call this constructor and build your own translation instance of this locale.
+  /// Constructing via the enum [AppLocale.build] is preferred.
+  TranslationsZhHant(
+      {Map<String, Node>? overrides,
+      PluralResolver? cardinalResolver,
+      PluralResolver? ordinalResolver})
+      : assert(overrides == null,
+            'Set "translation_overrides: true" in order to enable this feature.'),
+        $meta = TranslationMetadata(
+          locale: AppLocale.zhHant,
+          overrides: overrides ?? {},
+          cardinalResolver: cardinalResolver,
+          ordinalResolver: ordinalResolver,
+        ) {
+    $meta.setFlatMapFunction(_flatMapFunction);
+  }
 
-	/// Metadata for the translations of <zh-Hant>.
-	@override final TranslationMetadata<AppLocale, Translations> $meta;
+  /// Metadata for the translations of <zh-Hant>.
+  @override
+  final TranslationMetadata<AppLocale, Translations> $meta;
 
-	/// Access flat map
-	@override dynamic operator[](String key) => $meta.getTranslation(key);
+  /// Access flat map
+  @override
+  dynamic operator [](String key) => $meta.getTranslation(key);
 
-	late final TranslationsZhHant _root = this; // ignore: unused_field
+  late final TranslationsZhHant _root = this; // ignore: unused_field
 
-	// Translations
-	@override late final _TranslationsNavigationBarZhHant NavigationBar = _TranslationsNavigationBarZhHant._(_root);
-	@override late final _TranslationsChangeLanguageZhHant ChangeLanguage = _TranslationsChangeLanguageZhHant._(_root);
-	@override Map<String, String> get locales => {
-		'en': '英語',
-		'ja': '日語',
-		'zh': '中文',
-	};
+  // Translations
+  @override
+  late final _TranslationsNavigationBarZhHant NavigationBar =
+      _TranslationsNavigationBarZhHant._(_root);
+  @override
+  late final _TranslationsChangeLanguageZhHant ChangeLanguage =
+      _TranslationsChangeLanguageZhHant._(_root);
+  @override
+  Map<String, String> get locales => {
+        'en': '英語',
+        'ja': '日語',
+        'zh': '中文',
+      };
 }
 
 // Path: NavigationBar
 class _TranslationsNavigationBarZhHant implements TranslationsNavigationBarEn {
-	_TranslationsNavigationBarZhHant._(this._root);
+  _TranslationsNavigationBarZhHant._(this._root);
 
-	final TranslationsZhHant _root; // ignore: unused_field
+  final TranslationsZhHant _root; // ignore: unused_field
 
-	// Translations
-	@override late final _TranslationsNavigationBarItemsZhHant items = _TranslationsNavigationBarItemsZhHant._(_root);
+  // Translations
+  @override
+  late final _TranslationsNavigationBarItemsZhHant items =
+      _TranslationsNavigationBarItemsZhHant._(_root);
 }
 
 // Path: ChangeLanguage
-class _TranslationsChangeLanguageZhHant implements TranslationsChangeLanguageEn {
-	_TranslationsChangeLanguageZhHant._(this._root);
+class _TranslationsChangeLanguageZhHant
+    implements TranslationsChangeLanguageEn {
+  _TranslationsChangeLanguageZhHant._(this._root);
 
-	final TranslationsZhHant _root; // ignore: unused_field
+  final TranslationsZhHant _root; // ignore: unused_field
 
-	// Translations
-	@override late final _TranslationsChangeLanguageItemsZhHant items = _TranslationsChangeLanguageItemsZhHant._(_root);
+  // Translations
+  @override
+  late final _TranslationsChangeLanguageItemsZhHant items =
+      _TranslationsChangeLanguageItemsZhHant._(_root);
 }
 
 // Path: NavigationBar.items
-class _TranslationsNavigationBarItemsZhHant implements TranslationsNavigationBarItemsEn {
-	_TranslationsNavigationBarItemsZhHant._(this._root);
+class _TranslationsNavigationBarItemsZhHant
+    implements TranslationsNavigationBarItemsEn {
+  _TranslationsNavigationBarItemsZhHant._(this._root);
 
-	final TranslationsZhHant _root; // ignore: unused_field
+  final TranslationsZhHant _root; // ignore: unused_field
 
-	// Translations
-	@override String get home => '首頁';
-	@override String get myPlan => '我的計劃';
-	@override String get myPage => '我的頁面';
+  // Translations
+  @override
+  String get home => '首頁';
+  @override
+  String get myPlan => '我的計劃';
+  @override
+  String get myPage => '我的頁面';
 }
 
 // Path: ChangeLanguage.items
-class _TranslationsChangeLanguageItemsZhHant implements TranslationsChangeLanguageItemsEn {
-	_TranslationsChangeLanguageItemsZhHant._(this._root);
+class _TranslationsChangeLanguageItemsZhHant
+    implements TranslationsChangeLanguageItemsEn {
+  _TranslationsChangeLanguageItemsZhHant._(this._root);
 
-	final TranslationsZhHant _root; // ignore: unused_field
+  final TranslationsZhHant _root; // ignore: unused_field
 
-	// Translations
-	@override String get japanese => '日语';
-	@override String get english => '英语';
-	@override String get chinese_simplified => '中文(简体字)';
-	@override String get chinese_traditional => '中文(繁体字)';
-	@override String get language => '语言';
+  // Translations
+  @override
+  String get japanese => '日语';
+  @override
+  String get english => '英语';
+  @override
+  String get chinese_simplified => '中文(简体字)';
+  @override
+  String get chinese_traditional => '中文(繁体字)';
+  @override
+  String get language => '语言';
 }
 
 /// Flat map(s) containing all translations.
 /// Only for edge cases! For simple maps, use the map function of this library.
 extension on TranslationsZhHant {
-	dynamic _flatMapFunction(String path) {
-		switch (path) {
-			case 'NavigationBar.items.home': return '首頁';
-			case 'NavigationBar.items.myPlan': return '我的計劃';
-			case 'NavigationBar.items.myPage': return '我的頁面';
-			case 'ChangeLanguage.items.japanese': return '日语';
-			case 'ChangeLanguage.items.english': return '英语';
-			case 'ChangeLanguage.items.chinese_simplified': return '中文(简体字)';
-			case 'ChangeLanguage.items.chinese_traditional': return '中文(繁体字)';
-			case 'ChangeLanguage.items.language': return '语言';
-			case 'locales.en': return '英語';
-			case 'locales.ja': return '日語';
-			case 'locales.zh': return '中文';
-			default: return null;
-		}
-	}
+  dynamic _flatMapFunction(String path) {
+    switch (path) {
+      case 'NavigationBar.items.home':
+        return '首頁';
+      case 'NavigationBar.items.myPlan':
+        return '我的計劃';
+      case 'NavigationBar.items.myPage':
+        return '我的頁面';
+      case 'ChangeLanguage.items.japanese':
+        return '日语';
+      case 'ChangeLanguage.items.english':
+        return '英语';
+      case 'ChangeLanguage.items.chinese_simplified':
+        return '中文(简体字)';
+      case 'ChangeLanguage.items.chinese_traditional':
+        return '中文(繁体字)';
+      case 'ChangeLanguage.items.language':
+        return '语言';
+      case 'locales.en':
+        return '英語';
+      case 'locales.ja':
+        return '日語';
+      case 'locales.zh':
+        return '中文';
+      default:
+        return null;
+    }
+  }
 }
-
