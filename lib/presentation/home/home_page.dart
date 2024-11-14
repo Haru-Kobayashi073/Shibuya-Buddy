72386238f2cdc14603f23ab9b64d43545be2578f
Tomoya <92212932+PenginPenPen@users.noreply.github.com>
2024-11-14 15:15:55 +0900

    不要なものを削除

--- lib/presentation/home/home_page.dart
+++ lib/presentation/home/home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-import '../change_language/change_language_page.dart';
 import 'components/complicated_image_carousel.dart';
 import 'popular_topics.dart';
 
@@ -16,22 +15,12 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: SafeArea(
         child: Column(
           children: [
-            const ComplicatedImageCarousel(),
-            const PopularTopics(),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const ChangeLanguagePage()),
-                );
-              },
-              child: const Text('遷移(デバッグ)'),
-            ),
+            ComplicatedImageCarousel(),
+            PopularTopics(),
           ],
         ),
       ),
