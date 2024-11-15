320cbd78ef50d4d563380ee5f9d927df0eef5fbe
Tomoya <92212932+PenginPenPen@users.noreply.github.com>
2024-11-15 15:59:55 +0900

    マイページ画面の作成

--- lib/presentation/mypage/my_page.dart
+++ lib/presentation/mypage/my_page.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-
 import '../../gen/assets.gen.dart';
-import '../../utils/styles/app_color.dart';
+import '../../i18n/strings.g.dart';
 import '../../utils/styles/app_text_style.dart';
 import '../components/textArrowButton.dart';
-import '../../i18n/strings.g.dart';
 
 class MyPage extends StatelessWidget {
   const MyPage({super.key});
@@ -16,51 +14,50 @@ class MyPage extends StatelessWidget {
     final screenWidth = MediaQuery.of(context).size.width;
     final circleSize = screenWidth * 0.1;
     return Scaffold(
+      appBar: AppBar(
+        flexibleSpace: Padding(
+          padding: const EdgeInsets.only(top: 66, left: 16, right: 5),
+          child: Row(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(right: 10),
+                child: Container(
+                  width: circleSize,
+                  height: circleSize,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      fit: BoxFit.fill,
+                      image: AssetImage(
+                        Assets.images.usericon.path,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: Text(
+                  'Name',
+                  style: AppTextStyle.textStyle.copyWith(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              const Spacer(),
+              IconButton(
+                onPressed: () {
+                  debugPrint('TapEdit!');
+                },
+                icon: const Icon(Icons.edit, size: 24),
+              ),
+            ],
+          ),
+        ),
+      ),
       body: Column(
         children: [
-          SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.only(top: 50, left: 16, right: 5),
-              child: Row(
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.only(right: 10),
-                    child: Container(
-                      width: circleSize,
-                      height: circleSize,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        image: DecorationImage(
-                          fit: BoxFit.fill,
-                          image: AssetImage(
-                            Assets.images.usericon.path,
-                          ),
-                        ),
-                        // color: AppColor.blue400,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 10),
-                    child: Text(
-                      'Name',
-                      style: AppTextStyle.textStyle.copyWith(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  const Spacer(),
-                  IconButton(
-                    onPressed: () {
-                      debugPrint('TapEdit!');
-                    },
-                    icon: const Icon(Icons.edit, size: 24),
-                  ),
-                ],
-              ),
-            ),
-          ),
           Padding(
             padding: const EdgeInsets.all(16),
             child: Row(
@@ -92,35 +89,11 @@ class MyPage extends StatelessWidget {
               ],
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.only(left: 16, right: 16),
-            child: DecoratedBox(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: AppColor.grey200,
-              ),
-              child: Column(
-                children: [
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Padding(
-                      padding: const EdgeInsets.only(
-                          top: 16, bottom: 80, left: 16), //bottomは仮
-                      child: Text(
-                        '現在のグレードは${false ? 'プレミアム' : 'ベーシック'}です。',
-                        style: AppTextStyle.textStyle.copyWith(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 14,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
+          const Padding(
+            padding: EdgeInsets.only(left: 16, right: 16),
           ),
           Padding(
-            padding: const EdgeInsets.only(left: 16, top: 16),
+            padding: const EdgeInsets.only(left: 16),
             child: Align(
               alignment: Alignment.centerLeft,
               child: Text(
