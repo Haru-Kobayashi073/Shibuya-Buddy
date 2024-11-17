139fb963f12c750814228beab291e6bf72233c28
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2024-11-18 00:43:14 +0900

    認証のDataSourceを実装

---
+++ lib/infrastructure/authentication/authentication_data_source.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'authentication_data_source.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$authenticationDataSourceHash() =>
+    r'5beef656f7cd256a0d73e66a49b90fdf4dd420f1';
+
+/// See also [AuthenticationDataSource].
+@ProviderFor(AuthenticationDataSource)
+final authenticationDataSourceProvider =
+    AutoDisposeNotifierProvider<AuthenticationDataSource, void>.internal(
+  AuthenticationDataSource.new,
+  name: r'authenticationDataSourceProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$authenticationDataSourceHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$AuthenticationDataSource = AutoDisposeNotifier<void>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
