24c6f5ccd831478b306ced653f49d8764dcd7577
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2024-11-18 00:47:31 +0900

    SignInページのNotifier実装

---
+++ lib/presentation/sign_in/sign_in_page_notifier.dart
@@ -0,0 +1,49 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import '../../infrastructure/authentication/authentication_data_source.dart';
+import '../../utils/extensions/firebase_auth_exception.dart';
+import '../../utils/providers/scaffold_messenger/scaffold_messenger.dart';
+
+part 'sign_in_page_notifier.g.dart';
+
+@riverpod
+class SignInPageNotifier extends _$SignInPageNotifier {
+  AuthenticationDataSource get authenticationDataSource =>
+      ref.read(authenticationDataSourceProvider.notifier);
+
+  @override
+  void build() {
+    return;
+  }
+
+  Future<void> signInWithEmailAndPassword({
+    required String emailAddress,
+    required String password,
+    required Future<void> Function() onSuccess,
+  }) async {
+    try {
+      await authenticationDataSource.signInWithEmailAndPassword(
+        emailAddress,
+        password,
+      );
+      await onSuccess();
+    } on FirebaseAuthException catch (e) {
+      final exceptionMessage = e.toAfterTranslation;
+      ref
+          .read(scaffoldMessengerProvider.notifier)
+          .showExceptionSnackBar(exceptionMessage);
+    }
+  }
+
+  Future<void> signInWithGoogle() async {
+    try {
+      await authenticationDataSource.signInWithGoogle();
+    } on FirebaseAuthException catch (e) {
+      final exceptionMessage = e.toAfterTranslation;
+      ref
+          .read(scaffoldMessengerProvider.notifier)
+          .showExceptionSnackBar(exceptionMessage);
+    }
+  }
+}
---
+++ lib/presentation/sign_in/sign_in_page_notifier.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'sign_in_page_notifier.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$signInPageNotifierHash() =>
+    r'db3c08bbbea0d7a444797f6d9a4aff61cf6328a7';
+
+/// See also [SignInPageNotifier].
+@ProviderFor(SignInPageNotifier)
+final signInPageNotifierProvider =
+    AutoDisposeNotifierProvider<SignInPageNotifier, void>.internal(
+  SignInPageNotifier.new,
+  name: r'signInPageNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$signInPageNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$SignInPageNotifier = AutoDisposeNotifier<void>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
