3664427c73bee8460e3818d27b53122ff9bcbc53
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2024-11-18 00:44:24 +0900

    認証状態の購読をするProvider

---
+++ lib/infrastructure/firebase/firebase_auth_state_listener.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'firebase_auth_state_listener.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$firebaseAuthStateListenerHash() =>
+    r'd0795219742ef26b51bcc872a12176a6e7e19334';
+
+/// See also [firebaseAuthStateListener].
+@ProviderFor(firebaseAuthStateListener)
+final firebaseAuthStateListenerProvider = StreamProvider<User?>.internal(
+  firebaseAuthStateListener,
+  name: r'firebaseAuthStateListenerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$firebaseAuthStateListenerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef FirebaseAuthStateListenerRef = StreamProviderRef<User?>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
