fbd97e1c299083b0a55cf2e9ac367d7f69e940a4
Tomoya <92212932+PenginPenPen@users.noreply.github.com>
2024-11-18 16:03:46 +0900

    /を削除

--- lib/utils/routes/routes.dart
+++ lib/utils/routes/routes.dart
@@ -1,13 +1,13 @@
 class Routes {
-  static const aboutApp = '/aboutApp';
-  static const aboutDev = '/aboutDev';
-  static const account = '/account';
-  static const aiFunctionsDetail = '/aiFunctionsDetail';
-  static const changeLanguage = '/changeLanguage';
-  static const editProfile = '/editProfile';
+  static const aboutApp = 'aboutApp';
+  static const aboutDev = 'aboutDev';
+  static const account = 'account';
+  static const aiFunctionsDetail = 'aiFunctionsDetail';
+  static const changeLanguage = 'changeLanguage';
+  static const editProfile = 'editProfile';
   static const home = '/home';
   static const myPlan = '/myPlan';
   static const myPage = '/myPage';
-  static const termsOfUsePrivacyPolicy = '/termsOfUsePrivacyPolicy';
-  static const theme = '/theme';
+  static const termsOfUsePrivacyPolicy = 'termsOfUsePrivacyPolicy';
+  static const theme = 'theme';
 }
