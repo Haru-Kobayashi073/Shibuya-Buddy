31ca45d808bf21dddd82e495635738155aa2a03f
Tomoya <92212932+PenginPenPen@users.noreply.github.com>
2024-11-18 10:09:21 +0900

    スペルミスの修正

--- lib/utils/routes/routes.dart
+++ lib/utils/routes/routes.dart
@@ -1,13 +1,13 @@
 class Routes {
   static const aboutApp = '/aboutApp';
   static const aboutDev = '/aboutDev';
-  static const acount = '/acount';
-  static const aiFunctionsDetail ='/aiFunctionsDetail';
-  static const changelanguageage = '/changeLanguageage';
+  static const account = '/account';
+  static const aiFunctionsDetail = '/aiFunctionsDetail';
+  static const changeLanguage = '/changeLanguage';
   static const editProfile = '/editProfile';
   static const home = '/home';
   static const myPlan = '/myPlan';
   static const myPage = '/myPage';
   static const termsOfUsePrivacyPolicy = '/termsOfUsePrivacyPolicy';
-  static const thema = '/thema';
+  static const theme = '/theme';
 }
