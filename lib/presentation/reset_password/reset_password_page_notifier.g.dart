8ce5a7a18297697f75cbf35c5f6c10e5da4d900c
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2024-11-18 13:53:43 +0900

    リセットパスワードページのNotifier実装

---
+++ lib/presentation/reset_password/reset_password_page_notifier.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'reset_password_page_notifier.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$resetPasswordPageNotifierHash() =>
+    r'b2484c7de8dc51d1b0dc5183928f0918b557bb12';
+
+/// See also [ResetPasswordPageNotifier].
+@ProviderFor(ResetPasswordPageNotifier)
+final resetPasswordPageNotifierProvider =
+    AutoDisposeNotifierProvider<ResetPasswordPageNotifier, void>.internal(
+  ResetPasswordPageNotifier.new,
+  name: r'resetPasswordPageNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$resetPasswordPageNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$ResetPasswordPageNotifier = AutoDisposeNotifier<void>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
