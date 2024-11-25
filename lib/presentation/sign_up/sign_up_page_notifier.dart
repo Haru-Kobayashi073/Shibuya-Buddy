d2a00031a94485bf6228bc2c406941927d8c01ca
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2024-11-25 13:13:28 +0900

    新規登録ページのNotifierを実装

---
+++ lib/presentation/sign_up/sign_up_page_notifier.dart
@@ -0,0 +1,46 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import '../../infrastructure/authentication/authentication_data_source.dart';
+import '../../utils/extensions/firebase_auth_exception.dart';
+import '../../utils/providers/scaffold_messenger/scaffold_messenger.dart';
+
+part 'sign_up_page_notifier.g.dart';
+
+@riverpod
+class SignUpPageNotifier extends _$SignUpPageNotifier {
+  AuthenticationDataSource get authenticationDataSource =>
+      ref.read(authenticationDataSourceProvider.notifier);
+
+  @override
+  void build() {
+    return;
+  }
+
+  Future<void> signUpWithEmailAndPassword({
+    required String emailAddress,
+    required String password,
+    required Future<void> Function() onSuccess,
+  }) async {
+    try {
+      final credential =
+          await authenticationDataSource.signUpWithEmailAndPassword(
+        emailAddress,
+        password,
+      );
+      await _sendEmailVerification(credential);
+      await onSuccess();
+    } on FirebaseAuthException catch (e) {
+      final exceptionMessage = e.toLocalizedMessage;
+      ref
+          .read(scaffoldMessengerProvider.notifier)
+          .showExceptionSnackBar(exceptionMessage);
+    }
+  }
+
+  Future<void> _sendEmailVerification(
+    UserCredential userCredential,
+  ) async {
+    await authenticationDataSource.sendEmailVerification(userCredential);
+  }
+}
---
+++ lib/presentation/sign_up/sign_up_page_notifier.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'sign_up_page_notifier.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$signUpPageNotifierHash() =>
+    r'0335a1b7e3193b69f686d08d89148bbf9502b439';
+
+/// See also [SignUpPageNotifier].
+@ProviderFor(SignUpPageNotifier)
+final signUpPageNotifierProvider =
+    AutoDisposeNotifierProvider<SignUpPageNotifier, void>.internal(
+  SignUpPageNotifier.new,
+  name: r'signUpPageNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$signUpPageNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$SignUpPageNotifier = AutoDisposeNotifier<void>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
