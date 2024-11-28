26a6d5b6a7369fdef16016483b111f4063f09f89
Tomoya <92212932+PenginPenPen@users.noreply.github.com>
2024-11-29 02:37:33 +0900

    AppBarのタイトルカードの縦表示

--- lib/presentation/popular_topics/popular_topics_page.dart
+++ lib/presentation/popular_topics/popular_topics_page.dart
@@ -1,10 +1,66 @@
 import 'package:flutter/material.dart';
 
+import '/gen/assets.gen.dart';
+import '../../i18n/strings.g.dart';
+import '../../utils/styles/app_text_style.dart';
+import '../components/topic_card.dart';
+
+Map<String, Map<int, String>> topics = {
+  'アニメ': {
+    30: Assets.images.anime.path,
+  },
+  'ショッピング': {
+    208: Assets.images.shopping.path,
+  },
+  'アクティビティー': {
+    16: Assets.images.activity.path,
+  },
+  'グルメ': {
+    430: Assets.images.gourmet.path,
+  },
+  'もじもじもじもじもじもじもじもじもじもじもじもじもじもじもじ': {
+    430: Assets.images.gourmet.path,
+  },
+};
+
 class PopularTopicsPage extends StatelessWidget {
   const PopularTopicsPage({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final i18n = Translations.of(context);
+    final title = i18n.PopularTopics.section_name;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          title,
+          style: AppTextStyle.textStyle.copyWith(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: Column(
+        children: topics.entries.map((entry) {
+          final title = entry.key;
+          final path = entry.value;
+          final pathWidgets = <Widget>[];
+          for (final pathEntry in path.entries) {
+            final image = pathEntry.value;
+            final numberOfTopics = pathEntry.key;
+            pathWidgets.add(
+              TopicCard(
+                title: title,
+                imagePath: image,
+                numberOfTopics: numberOfTopics,
+              ),
+            );
+          }
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: pathWidgets,
+          );
+        }).toList(),
+      ),
+    );
   }
 }
