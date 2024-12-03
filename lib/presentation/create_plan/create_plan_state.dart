da2fd56ef2af5f8cdc6e6c0a03ccacd204bd4fa0
shi0n0 <shiono.yui1080@gmail.com>
2024-12-03 23:19:08 +0900

    freezedでstateを管理

---
+++ lib/presentation/create_plan/create_plan_state.dart
@@ -0,0 +1,15 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'create_plan_state.freezed.dart';
+
+@freezed
+abstract class CreatePlanState with _$CreatePlanState {
+  const factory CreatePlanState({
+    @Default('') String location,
+    @Default('') String date,
+    @Default('') String numberOfPeople,
+    @Default('') String transport,
+    @Default('') String category,
+    @Default([]) List<String> selectedTopics,
+  }) = _CreatePlanState;
+}
