2e01e2b19eeae283ae2f15019bc2a5ba71c87557
shi0n0 <shiono.yui1080@gmail.com>
2024-12-04 18:23:50 +0900

    createPlanのNotifierを生成

---
+++ lib/presentation/create_plan/create_plan_notifier.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'create_plan_notifier.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$createPlanNotifierHash() =>
+    r'7e8c14b81f5c4104db1ce7a44887fc010a16e90e';
+
+/// See also [CreatePlanNotifier].
+@ProviderFor(CreatePlanNotifier)
+final createPlanNotifierProvider =
+    AutoDisposeNotifierProvider<CreatePlanNotifier, CreatePlanState>.internal(
+  CreatePlanNotifier.new,
+  name: r'createPlanNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$createPlanNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$CreatePlanNotifier = AutoDisposeNotifier<CreatePlanState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
