e6117d07c119b2635b9d170cc6d6d9f96ddcb50c
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2024-12-07 13:59:02 +0900

    Uri型の画像フィールドをString型に変更

--- lib/domain/entities/topic.g.dart
+++ lib/domain/entities/topic.g.dart
@@ -9,7 +9,7 @@ part of 'topic.dart';
 _$TopicImpl _$$TopicImplFromJson(Map<String, dynamic> json) => _$TopicImpl(
       id: json['id'] as String,
       name: json['name'] as String,
-      thumbnailUrl: Uri.parse(json['thumbnailUrl'] as String),
+      thumbnailUrl: json['thumbnailUrl'] as String,
       totalCount: (json['totalCount'] as num).toInt(),
       ranking: (json['ranking'] as num?)?.toInt(),
       createdAt:
@@ -20,7 +20,7 @@ Map<String, dynamic> _$$TopicImplToJson(_$TopicImpl instance) =>
     <String, dynamic>{
       'id': instance.id,
       'name': instance.name,
-      'thumbnailUrl': instance.thumbnailUrl.toString(),
+      'thumbnailUrl': instance.thumbnailUrl,
       'totalCount': instance.totalCount,
       'ranking': instance.ranking,
       'createdAt': const DateTimeConverter().toJson(instance.createdAt),
