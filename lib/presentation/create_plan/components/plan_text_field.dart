6010e0a4241bf55d561c566e12e6138633873127
shi0n0 <shiono.yui1080@gmail.com>
2024-12-08 20:49:01 +0900

    プロパティを追加 | コントローラーの定義を削除

--- lib/presentation/create_plan/components/plan_text_field.dart
+++ lib/presentation/create_plan/components/plan_text_field.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '../../../utils/styles/app_color.dart';
 
@@ -10,8 +11,12 @@ class PlanTextField extends StatelessWidget {
     this.initialValue,
     this.prefixIcon,
     this.suffixIcon,
+    this.controller,
     this.readOnly = false,
+    this.keyboardType,
+    this.inputFormatters,
     this.onClear,
+    this.onTap,
   });
 
   final String? label;
@@ -19,30 +24,37 @@ class PlanTextField extends StatelessWidget {
   final String? initialValue;
   final Icon? prefixIcon;
   final Icon? suffixIcon;
+  final TextEditingController? controller;
   final bool readOnly;
+  final TextInputType? keyboardType;
+  final List<TextInputFormatter>? inputFormatters;
   final VoidCallback? onClear;
+  final VoidCallback? onTap;
 
   @override
   Widget build(BuildContext context) {
-    final controller = TextEditingController(text: initialValue);
-
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         TextField(
           controller: controller,
+          onTap: onTap,
           readOnly: readOnly,
+          keyboardType: keyboardType,
           textInputAction: TextInputAction.next,
+          inputFormatters: inputFormatters,
           decoration: InputDecoration(
             border: const OutlineInputBorder(),
             fillColor: AppColor.blue800Secondary,
             hintText: hintText,
             labelText: label,
             prefixIcon: prefixIcon,
-            suffixIcon: controller.text.isNotEmpty && suffixIcon != null
+            suffixIcon: (controller != null &&
+                    controller!.text.isNotEmpty &&
+                    suffixIcon != null)
                 ? IconButton(
                     onPressed: () {
-                      controller.clear();
+                      controller?.clear();
                       onClear?.call();
                     },
                     icon: suffixIcon!,
