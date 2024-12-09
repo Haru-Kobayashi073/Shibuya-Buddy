521f25cedf12cf82267ef76fcfba5170a2fd36c5
shi0n0 <shiono.yui1080@gmail.com>
2024-12-09 15:47:39 +0900

    SingleChildScrollViewでスクロールを可能に(エラー回避)

--- lib/presentation/create_plan/create_plan_page.dart
+++ lib/presentation/create_plan/create_plan_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:gap/gap.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:material_symbols_icons/material_symbols_icons.dart';
@@ -29,73 +30,115 @@ class CreatePlanPage extends ConsumerWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-        child: Column(
-          children: [
-            PlanTextField(
-              label: '目的地',
-              hintText: '渋谷',
-              prefixIcon: const Icon(Symbols.location_on),
-              initialValue: planState.location,
-            ),
-            const Gap(16),
-            PlanTextField(
-              label: '日程',
-              hintText: '12:00 ~ 13:30',
-              prefixIcon: const Icon(Symbols.calendar_month),
-              initialValue: planState.date,
-            ),
-            const Gap(16),
-            PlanTextField(
-              label: '人数',
-              hintText: '4人',
-              prefixIcon: const Icon(Symbols.supervisor_account),
-              initialValue: planState.numberOfPeople,
-            ),
-            const Gap(16),
-            PlanTextField(
-              label: '交通手段',
-              hintText: '電車・徒歩',
-              prefixIcon: const Icon(Symbols.commute),
-              initialValue: planState.transport,
-            ),
-            const Gap(16),
-            PlanTextField(
-              label: 'カテゴリ',
-              hintText: '子連れ向け',
-              prefixIcon: const Icon(Symbols.category),
-              initialValue: planState.category,
-            ),
-            const Gap(16),
-            PlanTextField(
-              label: '旅のトピック',
-              hintText: '旅のプラン',
-              prefixIcon: const Icon(Symbols.emoji_objects),
-              suffixIcon: const Icon(Symbols.close),
-              readOnly: true,
-              initialValue: planState.selectedTopics.join(', '),
-              onClear: planNotifier.clearTopics,
-            ),
-            TopicChipField(
-              topics: const [
-                'グルメ',
-                'ショッピング',
-                'アクティビティ',
-                '映画',
-              ],
-              selectedTopics: planState.selectedTopics,
-              onChange: (updatedTopics) {
-                planNotifier
-                  ..updateSelectedTopics(updatedTopics)
-                  ..updateTopicsController();
-              },
-            ),
-            const Gap(16),
-            WideButton(
-              label: 'プランをAIに伝える',
-              color: AppColor.yellow600Primary,
-              onPressed: () {},
-            ),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              PlanTextField(
+                label: '目的地',
+                hintText: '渋谷',
+                readOnly: true,
+                prefixIcon: const Icon(Symbols.location_on),
+                initialValue: planState.location,
+              ),
+              const Gap(16),
+              Row(
+                children: [
+                  Expanded(
+                    child: PlanTextField(
+                      label: '初日',
+                      hintText: '1月1日（月）',
+                      prefixIcon: const Icon(Symbols.calendar_month),
+                      controller: planNotifier.startDateController,
+                      keyboardType: TextInputType.none,
+                      onTap: () async {
+                        await planNotifier.showCupertinoDatePicker(
+                          context,
+                          planNotifier.startDateController,
+                        );
+                      },
+                    ),
+                  ),
+                  const Gap(8),
+                  Text(
+                    '〜',
+                    style: AppTextStyle.textStyle.copyWith(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const Gap(8),
+                  Expanded(
+                    child: PlanTextField(
+                      label: '最終日',
+                      hintText: '1月3日（水）',
+                      prefixIcon: const Icon(Symbols.calendar_month),
+                      controller: planNotifier.endDateController,
+                      keyboardType: TextInputType.none,
+                      onTap: () async {
+                        await planNotifier.showCupertinoDatePicker(
+                          context,
+                          planNotifier.endDateController,
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              const Gap(16),
+              PlanTextField(
+                label: '人数',
+                hintText: '4人',
+                prefixIcon: const Icon(Symbols.supervisor_account),
+                initialValue: planState.numberOfPeople,
+                keyboardType: TextInputType.number,
+                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+              ),
+              const Gap(16),
+              PlanTextField(
+                label: '交通手段',
+                hintText: '電車・徒歩',
+                prefixIcon: const Icon(Symbols.commute),
+                initialValue: planState.transport,
+              ),
+              const Gap(16),
+              PlanTextField(
+                label: 'カテゴリ',
+                hintText: '子連れ向け',
+                prefixIcon: const Icon(Symbols.category),
+                initialValue: planState.category,
+              ),
+              const Gap(16),
+              PlanTextField(
+                label: '旅のトピック',
+                hintText: '旅のプラン',
+                prefixIcon: const Icon(Symbols.emoji_objects),
+                suffixIcon: const Icon(Symbols.close),
+                controller: planNotifier.topicsController,
+                readOnly: true,
+                onClear: planNotifier.clearTopics,
+              ),
+              TopicChipField(
+                topics: const [
+                  'グルメ',
+                  'ショッピング',
+                  'アクティビティ',
+                  '映画',
+                ],
+                selectedTopics: planState.selectedTopics,
+                onChange: (updatedTopics) {
+                  planNotifier
+                    ..updateSelectedTopics(updatedTopics)
+                    ..updateTopicsController();
+                },
+              ),
+              const Gap(16),
+              WideButton(
+                label: 'プランをAIに伝える',
+                color: AppColor.yellow600Primary,
+                onPressed: () {},
+              ),
+            ],
+          ),
         ),
       ),
     );
