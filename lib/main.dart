c05ce6090c1266a2c21a8c23db495d32fda26ecd
shi0n0 <shiono.yui1080@gmail.com>
2024-12-09 15:46:37 +0900

    intlの初期化

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
 import 'i18n/strings.g.dart';
 import 'infrastructure/firebase/firebase_options_dev.dart' as dev;
 import 'infrastructure/firebase/firebase_options_prod.dart' as prod;
@@ -13,6 +15,7 @@ const flavor = String.fromEnvironment('flavor');
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await LocaleSettings.useDeviceLocale();
+  await initializeDateFormatting();
 
   final firebaseOptions = flavor == 'prod'
       ? prod.DefaultFirebaseOptions.currentPlatform
