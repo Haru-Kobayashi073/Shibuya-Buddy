4ea8124cb9d7b1251a32ba8d7c255e657b7f29f4
ryunosuke_sg4i <azatose8684@gmail.com>
2024-12-10 13:45:21 +0900

    プロフィール編集modalWIP

--- lib/presentation/mypage/edit_profile/edit_profile.dart
+++ lib/presentation/mypage/edit_profile/edit_profile.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import '../../../utils/routes/app_router.dart';
+import '../../../utils/styles/app_color.dart';
+import '../../../utils/styles/app_text_style.dart';
+
 class EditProfile extends StatelessWidget {
   const EditProfile({super.key});
 
@@ -7,7 +11,7 @@ class EditProfile extends StatelessWidget {
   Widget build(BuildContext context) {
     return ElevatedButton(
       onPressed: () async {
-        await _showEditProfileModal(context);
+        await _showEditProfileModal(rootNavigatorKey.currentContext!);
       },
       child: const Text('プロフィールを編集'),
     );
@@ -20,57 +24,69 @@ class EditProfile extends StatelessWidget {
       shape: const RoundedRectangleBorder(
         borderRadius: BorderRadius.vertical(top: Radius.circular(24)),
       ),
+      backgroundColor: Colors.white,
       builder: (BuildContext context) {
         return SizedBox(
           height: MediaQuery.of(context).size.height * 0.8,
-          child: Scaffold(
-            appBar: PreferredSize(
-              preferredSize: const Size.fromHeight(56),
-              child: ClipRRect(
-                borderRadius: const BorderRadius.vertical(
-                  top: Radius.circular(24),
-                ),
-                child: AppBar(
-                  title: const Text('編集'),
-                  backgroundColor: Theme.of(context).primaryColor,
-                  leading: IconButton(
-                    icon: const Icon(Icons.close),
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                  ),
-                ),
-              ),
-            ),
-            body: Padding(
-              padding: const EdgeInsets.all(16),
-              child: Column(
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      // 写真変更のロジックをここに追加
-                    },
-                    child: const Text('写真を変更'),
-                  ),
-                  const SizedBox(height: 16),
-                  const TextField(
-                    decoration: InputDecoration(labelText: '名前を変更'),
-                  ),
-                  const SizedBox(height: 16),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(24),
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.close),
+                    ),
+                    const Spacer(),
+                    Text(
+                      '編集',
+                      style: AppTextStyle.textStyle.copyWith(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
+                    const Spacer(),
+                    const SizedBox(width: 48),
+                  ],
+                ),
+                const Padding(
+                  padding: EdgeInsets.fromLTRB(0, 170, 0, 16),
+                  child: CircleAvatar(
+                    radius: 60,
+                    backgroundImage: NetworkImage(
+                      'https://kotobank.jp/image/dictionary/daijisen/media/104818.jpg',
+                    ),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.fromLTRB(16,16,16,210),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Name',
+                      alignLabelWithHint: true,
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(32),
+                      ),
+                      minimumSize: const Size(double.infinity, 48),
+                      backgroundColor: AppColor.yellow600Primary,
+                    ),
                     onPressed: () {
-                      // 保存のロジックをここに追加
                       Navigator.of(context).pop();
                     },
                     child: const Text('保存'),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         );
