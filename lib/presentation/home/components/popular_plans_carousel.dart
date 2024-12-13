e844ed0231b4633ae8bb337db5851c78255e9f95
Tomoya <92212932+PenginPenPen@users.noreply.github.com>
2024-12-14 01:52:20 +0900

    ページに遷移する機能の追加

--- lib/presentation/home/components/popular_plans_carousel.dart
+++ lib/presentation/home/components/popular_plans_carousel.dart
@@ -2,7 +2,9 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+
 import '../../../i18n/strings.g.dart';
+import '../../../utils/routes/app_router.dart';
 import '../../../utils/styles/app_color.dart';
 import '../../../utils/styles/app_text_style.dart';
 import 'category_tags.dart';
@@ -41,7 +43,9 @@ class PopularPlansCarousel extends HookWidget {
           padding: const EdgeInsets.symmetric(horizontal: 16),
           child: SectionTitle.large(
             label: i18n.homePage.popularPlans.title,
-            onPressed: () {},
+            onPressed: () async {
+              await const PopularPlansPageRouteData().push<void>(context);
+            },
           ),
         ),
         const SizedBox(height: 8),
