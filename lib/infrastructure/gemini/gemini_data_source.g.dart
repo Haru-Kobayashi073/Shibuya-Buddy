1fbb1d779081a44e54b1f2626f7b174a37352dcf
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2024-12-14 23:37:11 +0900

    Gemini用のDataSourceを実装

---
+++ lib/infrastructure/gemini/gemini_data_source.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'gemini_data_source.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$geminiDataSourceHash() => r'c562a7e903ac16d548131ad225dfcfc66da95011';
+
+/// See also [GeminiDataSource].
+@ProviderFor(GeminiDataSource)
+final geminiDataSourceProvider =
+    AutoDisposeNotifierProvider<GeminiDataSource, void>.internal(
+  GeminiDataSource.new,
+  name: r'geminiDataSourceProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$geminiDataSourceHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$GeminiDataSource = AutoDisposeNotifier<void>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
---
+++ lib/infrastructure/gemini/gemini_mock_data_source.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'gemini_mock_data_source.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$geminiMockDataSourceHash() =>
+    r'c16c4404fb4907e8fed5e465092fd6566f2e6a19';
+
+/// See also [GeminiMockDataSource].
+@ProviderFor(GeminiMockDataSource)
+final geminiMockDataSourceProvider =
+    AutoDisposeNotifierProvider<GeminiMockDataSource, void>.internal(
+  GeminiMockDataSource.new,
+  name: r'geminiMockDataSourceProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$geminiMockDataSourceHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$GeminiMockDataSource = AutoDisposeNotifier<void>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
