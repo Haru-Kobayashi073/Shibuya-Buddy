f714c662b1f8c04180eb6f89a98b218c7bd3b06d
Tomoya <92212932+PenginPenPen@users.noreply.github.com>
2024-12-17 23:28:55 +0900

    Apple用の状態管理の追加、不要なコードを削除

--- lib/presentation/account/account_page_state.dart
+++ lib/presentation/account/account_page_state.dart
@@ -1,4 +1,3 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'account_page_state.freezed.dart';
@@ -6,28 +5,7 @@ part 'account_page_state.freezed.dart';
 @freezed
 class AccountPageState with _$AccountPageState {
   const factory AccountPageState({
-    @Default(false) bool isGoogleSignedIn,
+    @Default(false) bool googleLinkage,
+    @Default(false) bool appleLinkage,
   }) = _AccountPageState;
 }
-
-class AccountPageStateNotifier extends StateNotifier<AccountPageState> {
-  AccountPageStateNotifier() : super(const AccountPageState());
-
-  // デバッグ用
-  void toggleGoogleSignIn() {
-    state = state.copyWith(isGoogleSignedIn: !state.isGoogleSignedIn);
-  }
-
-  void setGoogleSignedInTrue() {
-    state = state.copyWith(isGoogleSignedIn: true);
-  }
-
-  void setGoogleSignedInFalse() {
-    state = state.copyWith(isGoogleSignedIn: false);
-  }
-}
-
-final accountPageStateProvider =
-    StateNotifierProvider<AccountPageStateNotifier, AccountPageState>(
-  (ref) => AccountPageStateNotifier(),
-);
