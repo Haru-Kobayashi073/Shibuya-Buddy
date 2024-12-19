3131554b5c000d517fc18ef83cee485fe961a764
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2024-12-20 01:28:27 +0900

    DBから取得するUserデータを保持するNotifierの実装

---
+++ lib/utils/providers/current_user/current_user.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'current_user.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$currentUserHash() => r'f3737c0df23cbb991ace68865e9ed43a5014171b';
+
+/// See also [CurrentUser].
+@ProviderFor(CurrentUser)
+final currentUserProvider = NotifierProvider<CurrentUser, User>.internal(
+  CurrentUser.new,
+  name: r'currentUserProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$currentUserHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$CurrentUser = Notifier<User>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
