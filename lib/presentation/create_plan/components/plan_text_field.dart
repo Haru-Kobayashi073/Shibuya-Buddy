49fbd1484ad23026033dba2b8b94c08f1978a54f
shi0n0 <shiono.yui1080@gmail.com>
2024-12-19 22:16:08 +0900

    使用していないフィールドを削除

--- lib/presentation/create_plan/components/plan_text_field.dart
+++ lib/presentation/create_plan/components/plan_text_field.dart
@@ -9,25 +9,21 @@ class PlanTextField extends StatelessWidget {
     super.key,
     this.label,
     this.hintText,
-    this.initialValue,
     this.prefixIcon,
     this.controller,
     this.readOnly = false,
     this.keyboardType,
     this.inputFormatters,
-    this.onClear,
     this.onTap,
   });
 
   final String? label;
   final String? hintText;
-  final String? initialValue;
   final Icon? prefixIcon;
   final TextEditingController? controller;
   final bool readOnly;
   final TextInputType? keyboardType;
   final List<TextInputFormatter>? inputFormatters;
-  final VoidCallback? onClear;
   final VoidCallback? onTap;
 
   @override
