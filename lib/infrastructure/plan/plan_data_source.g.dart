47212797ec09b5699ec5c3151d698f924f1ce760
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2024-12-20 14:18:31 +0900

    plan作成のメソッドをDataSource層に実装

---
+++ lib/infrastructure/plan/plan_data_source.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'plan_data_source.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$planDataSourceHash() => r'8b4664baf9a0f662f6cf093f2b6f88c308b3ae05';
+
+/// See also [PlanDataSource].
+@ProviderFor(PlanDataSource)
+final planDataSourceProvider =
+    AutoDisposeNotifierProvider<PlanDataSource, void>.internal(
+  PlanDataSource.new,
+  name: r'planDataSourceProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$planDataSourceHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$PlanDataSource = AutoDisposeNotifier<void>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
