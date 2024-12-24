1228737d3da94e8fd7bc01c0047ce1e8c590fb0b
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2024-12-25 00:13:50 +0900

    プロフィール編集メソッドをDataSource層に実装

--- lib/domain/repositories/user_repository.dart
+++ lib/domain/repositories/user_repository.dart
@@ -1,7 +1,7 @@
 import '../entities/user.dart';
 
-// ignore: one_member_abstracts - This is a temporary rule to suppress the warning.
 abstract interface class UserRepository {
   Future<void> createUser({required User user});
   Future<User> fetchUser({required String userId});
+  Future<void> editUser({required User user});
 }
