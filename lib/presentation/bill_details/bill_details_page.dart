eb8bcc50384cf7d283e63ba65f69066c27cac723
shi0n0 <shiono.yui1080@gmail.com>
2024-12-27 22:18:57 +0900

    課金詳細ページを作成

---
+++ lib/presentation/bill_details/bill_details_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BillDetailsPage extends StatelessWidget {
+  const BillDetailsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
