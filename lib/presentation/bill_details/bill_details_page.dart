d3e4f78cfd4f2ca4582d62697cdad9b35aabe638
shi0n0 <shiono.yui1080@gmail.com>
2024-12-28 18:21:50 +0900

    bottomModal分の余白をSafeAreaで実装

--- lib/presentation/bill_details/bill_details_page.dart
+++ lib/presentation/bill_details/bill_details_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 
+import 'components/bottom_modal.dart'; // BottomModalをインポート
 import 'components/grade_based_features.dart';
 import 'components/plan_table.dart';
 import 'components/premium_plan.dart';
@@ -14,32 +15,40 @@ class BillDetailsPage extends StatelessWidget {
         'https://images.unsplash.com/photo-1588099768550-4014589e03e0?ixlib=rb-1.2.1&ixid='
         'MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80';
 
+    const bottomModalHeight = 200.0;
+
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            pinned: true,
-            expandedHeight: 250,
-            flexibleSpace: FlexibleSpaceBar(
-              background: Image.network(imageUrl, fit: BoxFit.cover),
-            ),
-          ),
-          const SliverToBoxAdapter(
-            child: Padding(
-              padding: EdgeInsets.all(16),
-              child: Column(
-                children: [
-                  PremiumPlan(),
-                  Gap(32),
-                  PlanTable(),
-                  Gap(32),
-                  GradeBasedFeatures(),
-                ],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(bottom: bottomModalHeight),
+          child: CustomScrollView(
+            slivers: [
+              SliverAppBar(
+                pinned: true,
+                expandedHeight: 250,
+                flexibleSpace: FlexibleSpaceBar(
+                  background: Image.network(imageUrl, fit: BoxFit.cover),
+                ),
               ),
-            ),
+              const SliverToBoxAdapter(
+                child: Padding(
+                  padding: EdgeInsets.all(16),
+                  child: Column(
+                    children: [
+                      PremiumPlan(),
+                      Gap(32),
+                      PlanTable(),
+                      Gap(32),
+                      GradeBasedFeatures(),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
+      bottomSheet: const BottomModal(),
     );
   }
 }
