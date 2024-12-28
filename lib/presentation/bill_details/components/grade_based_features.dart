b402969b60e533d6d3275bc736c175c4df62e993
shi0n0 <shiono.yui1080@gmail.com>
2024-12-28 16:26:30 +0900

    グレードごとの機能をコンポーネント化（WIP）

---
+++ lib/presentation/bill_details/components/grade_based_features.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GradeBasedFeatures extends StatelessWidget {
+  const GradeBasedFeatures({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('グレードごとの機能');
+  }
+}
