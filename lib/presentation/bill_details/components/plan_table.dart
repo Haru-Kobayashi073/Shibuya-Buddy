c53b5a8108b2f8b7d19a0d6e681a792be02948d0
shi0n0 <shiono.yui1080@gmail.com>
2024-12-28 16:23:39 +0900

    料金プランのタイトルをテーブルコンポーネントに含める

--- lib/presentation/bill_details/components/plan_table.dart
+++ lib/presentation/bill_details/components/plan_table.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 import '../../../utils/styles/app_color.dart';
 import '../../../utils/styles/app_text_style.dart';
@@ -17,111 +18,123 @@ class PlanTable extends StatelessWidget {
       fontWeight: FontWeight.bold,
     );
 
-    return Table(
-      defaultVerticalAlignment: TableCellVerticalAlignment.bottom,
-      columnWidths: const {
-        0: FlexColumnWidth(),
-        1: FlexColumnWidth(2),
-      },
+    return Column(
       children: [
-        TableRow(
-          children: [
-            ClipRRect(
-              borderRadius:
-                  const BorderRadius.only(topLeft: Radius.circular(8)),
-              child: Container(
-                height: 50,
-                margin: const EdgeInsets.only(right: 1, bottom: 1),
-                padding:
-                    const EdgeInsets.symmetric(vertical: 12, horizontal: 8),
-                color: AppColor.blue50Background,
-                child: Center(
-                  child: Text('スタンダード', style: headerStyle),
-                ),
-              ),
-            ),
-            ClipRRect(
-              borderRadius:
-                  const BorderRadius.only(topRight: Radius.circular(8)),
-              child: Container(
-                height: 50,
-                margin: const EdgeInsets.only(left: 1, bottom: 1),
-                padding:
-                    const EdgeInsets.symmetric(vertical: 12, horizontal: 8),
-                color: AppColor.yellow200,
-                child: Center(
-                  child: Text('プレミアム', style: headerStyle),
-                ),
-              ),
-            ),
-          ],
+        Text(
+          '料金プラン',
+          style: AppTextStyle.textStyle.copyWith(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
         ),
-        TableRow(
+        const Gap(16),
+        Table(
+          defaultVerticalAlignment: TableCellVerticalAlignment.bottom,
+          columnWidths: const {
+            0: FlexColumnWidth(),
+            1: FlexColumnWidth(2),
+          },
           children: [
-            ClipRRect(
-              borderRadius:
-                  const BorderRadius.only(bottomLeft: Radius.circular(8)),
-              child: Container(
-                height: 200,
-                margin: const EdgeInsets.only(right: 1),
-                alignment: Alignment.center,
-                padding: const EdgeInsets.all(16),
-                color: AppColor.blue50Background,
-                child: Text('無料 🎉', style: headerStyle),
-              ),
+            TableRow(
+              children: [
+                ClipRRect(
+                  borderRadius:
+                      const BorderRadius.only(topLeft: Radius.circular(8)),
+                  child: Container(
+                    height: 50,
+                    margin: const EdgeInsets.only(right: 1, bottom: 1),
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 12, horizontal: 8),
+                    color: AppColor.blue50Background,
+                    child: Center(
+                      child: Text('スタンダード', style: headerStyle),
+                    ),
+                  ),
+                ),
+                ClipRRect(
+                  borderRadius:
+                      const BorderRadius.only(topRight: Radius.circular(8)),
+                  child: Container(
+                    height: 50,
+                    margin: const EdgeInsets.only(left: 1, bottom: 1),
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 12, horizontal: 8),
+                    color: AppColor.yellow200,
+                    child: Center(
+                      child: Text('プレミアム', style: headerStyle),
+                    ),
+                  ),
+                ),
+              ],
             ),
-            ClipRRect(
-              borderRadius:
-                  const BorderRadius.only(bottomRight: Radius.circular(8)),
-              child: Container(
-                height: 200,
-                margin: const EdgeInsets.only(left: 1, bottom: 1),
-                color: AppColor.yellow200,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Column(
+            TableRow(
+              children: [
+                ClipRRect(
+                  borderRadius:
+                      const BorderRadius.only(bottomLeft: Radius.circular(8)),
+                  child: Container(
+                    height: 200,
+                    margin: const EdgeInsets.only(right: 1),
+                    alignment: Alignment.center,
+                    padding: const EdgeInsets.all(16),
+                    color: AppColor.blue50Background,
+                    child: Text('無料 🎉', style: headerStyle),
+                  ),
+                ),
+                ClipRRect(
+                  borderRadius:
+                      const BorderRadius.only(bottomRight: Radius.circular(8)),
+                  child: Container(
+                    height: 200,
+                    margin: const EdgeInsets.only(left: 1, bottom: 1),
+                    color: AppColor.yellow200,
+                    child: Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Text('日数分購入', style: planTextStyle),
                         Column(
                           mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            const SizedBox(height: 8),
-                            Text('・1日  300円', style: planTextStyle),
-                            Text('・3日  855円', style: planTextStyle),
-                            Text('・5日  1,480円', style: planTextStyle),
-                            Text('・7日  2,070円', style: planTextStyle),
+                            Text('日数分購入', style: planTextStyle),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const SizedBox(height: 8),
+                                Text('・1日  300円', style: planTextStyle),
+                                Text('・3日  855円', style: planTextStyle),
+                                Text('・5日  1,480円', style: planTextStyle),
+                                Text('・7日  2,070円', style: planTextStyle),
+                              ],
+                            ),
                           ],
                         ),
-                      ],
-                    ),
-                    const SizedBox(width: 12),
-                    Text(
-                      'または',
-                      style: AppTextStyle.textStyle.copyWith(
-                        fontSize: 12,
-                      ),
-                    ),
-                    const SizedBox(width: 12),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text('永年分', style: planTextStyle),
+                        const SizedBox(width: 12),
+                        Text(
+                          'または',
+                          style: AppTextStyle.textStyle.copyWith(
+                            fontSize: 12,
+                          ),
+                        ),
                         const SizedBox(width: 12),
                         Column(
                           mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Text('25800円', style: planTextStyle),
+                            Text('永年分', style: planTextStyle),
+                            const SizedBox(width: 12),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text('25800円', style: planTextStyle),
+                              ],
+                            ),
                           ],
                         ),
                       ],
                     ),
-                  ],
+                  ),
                 ),
-              ),
+              ],
             ),
           ],
         ),
