c3634a0e3fa21d2208a9f860e0d75233005ef98c
shi0n0 <shiono.yui1080@gmail.com>
2024-12-28 21:57:36 +0900

    余白を開ける高さが大きくて表示がおかしかったため修正

--- lib/presentation/bill_details/bill_details_page.dart
+++ lib/presentation/bill_details/bill_details_page.dart
@@ -15,7 +15,7 @@ class BillDetailsPage extends StatelessWidget {
         'https://images.unsplash.com/photo-1588099768550-4014589e03e0?ixlib=rb-1.2.1&ixid='
         'MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80';
 
-    const bottomModalHeight = 200.0;
+    const bottomModalHeight = 150.0;
 
     return Scaffold(
       body: SafeArea(
