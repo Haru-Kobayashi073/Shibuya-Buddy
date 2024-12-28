7461ccb0d105d38d333c5b48374c7971c4db654b
Tomoya <92212932+PenginPenPen@users.noreply.github.com>
2024-12-29 04:31:08 +0900

    背景の画像とボタンの追加

---
+++ lib/presentation/plan_detail/components/plan_header.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+import '../../../utils/styles/app_color.dart';
+import '../../../utils/styles/app_text_style.dart';
+import '../../home/components/category_tags.dart';
+
+class PlanHeader extends StatefulWidget {
+  const PlanHeader({
+    super.key,
+    required this.title,
+    required this.description,
+    required this.imageUrl,
+    required this.tags,
+  });
+  final String title;
+  final String description;
+  final String? imageUrl;
+  final List<String> tags;
+  @override
+  State<PlanHeader> createState() => _PlanHeaderState();
+}
+
+class _PlanHeaderState extends State<PlanHeader> {
+  @override
+  Widget build(BuildContext context) {
+    final mediaQueryData = MediaQuery.of(context);
+    final screenWidth = mediaQueryData.size.width;
+    return Container(
+      child: Column(
+        children: [
+          SizedBox(
+            width: screenWidth,
+            child: Image(image: NetworkImage(widget.imageUrl.toString())),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              children: [
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    widget.title,
+                    style: AppTextStyle.textStyle.copyWith(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 16, bottom: 16),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: CategoryTags(
+                      tags: widget.tags,
+                      tagColor: AppColor.grey200,
+                      spacing: 4,
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    widget.description,
+                    style: AppTextStyle.textStyle.copyWith(fontSize: 14),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
