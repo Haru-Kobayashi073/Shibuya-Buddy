1dedb0d63bbe30f9d60180b3f7ac75be34dc6d5c
shi0n0 <shiono.yui1080@gmail.com>
2024-12-30 00:29:07 +0900

    ステータスバーの透明化と戻るボタンのスタイル調整

--- lib/presentation/bill_details/bill_details_page.dart
+++ lib/presentation/bill_details/bill_details_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
+import 'package:material_symbols_icons/symbols.dart';
 
-import 'components/bottom_modal.dart'; // BottomModalをインポート
+import '../../utils/styles/app_color.dart';
+import 'components/bottom_modal.dart';
 import 'components/grade_based_features.dart';
 import 'components/plan_table.dart';
 import 'components/premium_plan.dart';
@@ -17,15 +19,46 @@ class BillDetailsPage extends StatelessWidget {
 
     return Scaffold(
       body: SafeArea(
+        top: false,
         child: Padding(
           padding: const EdgeInsets.only(bottom: bottomModalHeight),
           child: CustomScrollView(
             slivers: [
               SliverAppBar(
-                pinned: true,
                 expandedHeight: 250,
                 flexibleSpace: FlexibleSpaceBar(
                   background: Image.asset(imagePath, fit: BoxFit.cover),
+                ),
+                pinned: true,
+                backgroundColor: Colors.transparent,
+                surfaceTintColor: Colors.transparent,
+                leading: Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: DecoratedBox(
+                    decoration: BoxDecoration(
+                      color: AppColor.white,
+                      borderRadius: BorderRadius.circular(50),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.1),
+                          blurRadius: 4,
+                          offset: const Offset(0, 2),
+                        ),
+                      ],
+                    ),
+                    child: IconButton(
+                      alignment: Alignment.center,
+                      iconSize: 24,
+                      icon: const Icon(
+                        Symbols.arrow_back_ios_new,
+                        color: AppColor.black,
+                        weight: 600,
+                      ),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                  ),
                 ),
               ),
               const SliverToBoxAdapter(
