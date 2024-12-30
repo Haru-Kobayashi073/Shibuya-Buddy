7c3138f0c4be7b6327bd4450f8a53c8d561919ef
Tomoya <92212932+PenginPenPen@users.noreply.github.com>
2024-12-30 22:58:39 +0900

    placeを直接受け取るように変更

--- lib/presentation/plan_detail/components/plan_header.dart
+++ lib/presentation/plan_detail/components/plan_header.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../../../domain/entities/place.dart';
 import '../../../utils/styles/app_color.dart';
 import '../../../utils/styles/app_text_style.dart';
 import '../../home/components/category_tags.dart';
@@ -7,53 +8,46 @@ import '../../home/components/category_tags.dart';
 class PlanHeader extends StatelessWidget {
   const PlanHeader({
     super.key,
-    required this.title,
-    required this.description,
-    required this.tags,
+    required this.place,
   });
-  final String title;
-  final String description;
-  final List<String> tags;
+  final Place place;
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(top: 16, left: 16, right: 16),
-          child: Column(
-            children: [
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  title,
-                  style: AppTextStyle.textStyle.copyWith(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
+    return Padding(
+      padding: const EdgeInsets.only(top: 16, left: 16, right: 16),
+      child: Column(
+        children: [
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              place.name,
+              style: AppTextStyle.textStyle.copyWith(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
               ),
-              Padding(
-                padding: const EdgeInsets.only(top: 16, bottom: 16),
-                child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: CategoryTags(
-                    tags: tags,
-                    tagColor: AppColor.grey200,
-                    spacing: 4,
-                  ),
-                ),
-              ),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  description,
-                  style: AppTextStyle.textStyle.copyWith(fontSize: 14),
-                ),
-              ),
-            ],
+            ),
           ),
-        ),
-      ],
+          Padding(
+            padding: const EdgeInsets.only(top: 16, bottom: 16),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: CategoryTags(
+                tags: place.tags ?? [],
+                tagColor: AppColor.grey200,
+                spacing: 4,
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              place.title,
+              style: AppTextStyle.textStyle.copyWith(fontSize: 14),
+            ),
+          ),
+          const SizedBox(height: 8),
+        ],
+      ),
     );
   }
 }
