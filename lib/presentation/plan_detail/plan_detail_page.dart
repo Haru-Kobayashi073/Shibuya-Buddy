a82711cc9947e962c82b3965acee264ba752e78f
Tomoya <92212932+PenginPenPen@users.noreply.github.com>
2024-12-30 16:31:47 +0900

    コンポーネントの呼び出し方を修正

--- lib/presentation/plan_detail/plan_detail_page.dart
+++ lib/presentation/plan_detail/plan_detail_page.dart
@@ -6,11 +6,22 @@ import '../../i18n/strings.g.dart';
 import '../../utils/styles/app_color.dart';
 import '../../utils/styles/app_text_style.dart';
 import '../components/place_card.dart';
+import '../components/wide_button.dart';
 import 'components/circle_icon_button.dart';
-import 'components/left_icon_wide_button.dart';
 import 'components/plan_header.dart';
 
 List<Place> dummyPlans = [
+  const Place(
+    id: '0',
+    name: '宮下パークでショッピング',
+    thumbnailUrl:
+        'https://www.wirerope.co.jp/products/jakob/wordpress/wp-content/uploads/2022/03/%E5%AE%AE%E4%B8%8B%E5%85%AC%E5%9C%92_%E6%89%8B%E6%91%BA1.jpg',
+    title: '''
+    今回、Buddyが提案したこのプランは、渋谷駅前にある若者集まる商業施設の”宮下パーク”がメインのプランです！一階には日本食を楽しめる居酒屋が並ぶ
+      とともに、他の階では、モダンなファッション店や雑貨屋が並んでいます。最後に、緑感じる屋上でアクティビティや一休みなどパーソナルな時間をお楽しみください。
+    ''',
+    tags: ['＃ショッピング ', '＃アクティビティ', '＃グルメ', '時間: 1時間〜'],
+  ),
   Place(
     id: '1',
     name: 'TitleTitleTitleTitleTitleTitleTitleTitle',
@@ -49,47 +60,15 @@ List<Place> dummyPlans = [
   ),
 ];
 
-class PlanInfo {
-  PlanInfo({
-    required this.title,
-    required this.description,
-    required this.tags,
-    required this.imageUrl,
-  });
-
-  factory PlanInfo.fromMap(Map<String, dynamic> map) {
-    return PlanInfo(
-      title: map['title'] as String,
-      description: map['description'] as String,
-      tags: List<String>.from(map['tags'] as List<dynamic>),
-      imageUrl: map['imageUrl'] as String,
-    );
-  }
-  final String title;
-  final String description;
-  final List<String> tags;
-  final String imageUrl;
-}
-
-final Map<String, dynamic> planInfo = {
-  'title': '宮下パークでショッピング',
-  'description': '''
-      今回、Buddyが提案したこのプランは、渋谷駅前にある若者集まる商業施設の”宮下パーク”がメインのプランです！一階には日本食を楽しめる居酒屋が並ぶ
-      とともに、他の階では、モダンなファッション店や雑貨屋が並んでいます。最後に、緑感じる屋上でアクティビティや一休みなどパーソナルな時間をお楽しみください。
-      ''',
-  'tags': ['＃ショッピング ', '＃アクティビティ', '＃グルメ', '時間: 1時間〜'],
-  'imageUrl':
-      'https://www.wirerope.co.jp/products/jakob/wordpress/wp-content/uploads/2022/03/%E5%AE%AE%E4%B8%8B%E5%85%AC%E5%9C%92_%E6%89%8B%E6%91%BA1.jpg',
-};
-
 class PlanDetailPage extends StatelessWidget {
   const PlanDetailPage({super.key});
 
-  List<Widget> placetrans(List<Place> dummyPlans) {
-    final plans = <Widget>[];
+  List<Widget> placetrans(List<Place> plans) {
+    plans.removeAt(0);
+    final widgets = <Widget>[];
     var i = 0;
-    for (final plan in dummyPlans) {
-      plans.add(
+    for (final plan in plans) {
+      widgets.add(
         PlaceCard(
           place: plan,
           index: i,
@@ -98,17 +77,26 @@ class PlanDetailPage extends StatelessWidget {
       );
       i++;
     }
-    return plans;
+    return widgets;
+  }
+
+  PlanHeader planinfo(List<Place> plans) {
+    final planInfo = plans[0];
+    return PlanHeader(
+      title: planInfo.name,
+      description: planInfo.title,
+      imageUrl: planInfo.thumbnailUrl,
+      tags: planInfo.tags ?? [],
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     final i18n = Translations.of(context);
-    final planDetailPage = i18n.planDetailsPage;
-    final plan = PlanInfo.fromMap(planInfo);
+    final planDetailPagei18n = i18n.planDetailsPage;
 
-    final outputFormat =
-        DateFormat(planDetailPage.dateTime.dateFormat).format(DateTime.now());
+    final outputFormat = DateFormat(planDetailPagei18n.dateTime.dateFormat)
+        .format(DateTime.now());
 
     return Scaffold(
       body: Stack(
@@ -116,12 +104,7 @@ class PlanDetailPage extends StatelessWidget {
           ListView(
             padding: EdgeInsets.zero,
             children: [
-              PlanHeader(
-                title: plan.title,
-                description: plan.description,
-                imageUrl: plan.imageUrl,
-                tags: plan.tags,
-              ),
+              planinfo(dummyPlans),
               Padding(
                 padding: const EdgeInsets.only(left: 16, right: 16),
                 child: Column(
@@ -129,7 +112,8 @@ class PlanDetailPage extends StatelessWidget {
                     Align(
                       alignment: Alignment.centerLeft,
                       child: Text(
-                        planDetailPage.dateTime.createOn(date: outputFormat),
+                        planDetailPagei18n.dateTime
+                            .createOn(date: outputFormat),
                         style: AppTextStyle.textStyle.copyWith(
                           fontSize: 14,
                           color: AppColor.grey600,
@@ -139,9 +123,11 @@ class PlanDetailPage extends StatelessWidget {
                     const SizedBox(
                       height: 16,
                     ),
-                    LeftIconWideButton(
-                      title: planDetailPage.item.viewOnMap,
-                      icon: Icons.pin_drop_outlined,
+                    WideButton(
+                      icon: const Icon(Icons.pin_drop_outlined),
+                      label: planDetailPagei18n.item.viewOnMap,
+                      color: AppColor.grey200,
+                      onPressed: () {},
                     ),
                     const SizedBox(
                       height: 16,
