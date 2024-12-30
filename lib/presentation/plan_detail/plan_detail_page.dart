87e15d38de1975633a9017454822b3a9484bdf6a
Tomoya <92212932+PenginPenPen@users.noreply.github.com>
2024-12-30 19:47:14 +0900

    planの定義の位置を修正

--- lib/presentation/plan_detail/plan_detail_page.dart
+++ lib/presentation/plan_detail/plan_detail_page.dart
@@ -12,56 +12,6 @@ import '../components/wide_button.dart';
 import 'components/circle_icon_button.dart';
 import 'components/plan_header.dart';
 
-List<Place> dummyPlans = [
-  const Place(
-    id: '0',
-    name: '宮下パークでショッピング',
-    thumbnailUrl:
-        'https://www.wirerope.co.jp/products/jakob/wordpress/wp-content/uploads/2022/03/%E5%AE%AE%E4%B8%8B%E5%85%AC%E5%9C%92_%E6%89%8B%E6%91%BA1.jpg',
-    title: '''
-    今回、Buddyが提案したこのプランは、渋谷駅前にある若者集まる商業施設の”宮下パーク”がメインのプランです！一階には日本食を楽しめる居酒屋が並ぶ
-      とともに、他の階では、モダンなファッション店や雑貨屋が並んでいます。最後に、緑感じる屋上でアクティビティや一休みなどパーソナルな時間をお楽しみください。
-    ''',
-    tags: ['＃ショッピング ', '＃アクティビティ', '＃グルメ', '時間: 1時間〜'],
-  ),
-  Place(
-    id: '1',
-    name: 'TitleTitleTitleTitleTitleTitleTitleTitle',
-    title: '世界的に有名な交差点。世界的に有名な交差点世界的に有名な交差点世界的に有名な交差点世界的に有名な交差点世界的に有名な交差点',
-    thumbnailUrl:
-        'https://www.cnn.co.jp/storage/2019/08/11/fbb9c1766abe6fa21b92a2808505d1e3/shibuya-crossing-tokyo-003.jpg',
-    openingHours: (
-      openTime: DateTime(2024, 1, 1, 9),
-      closeTime: DateTime(2024, 1, 1, 17),
-    ),
-    avevageAmount: '1200円',
-  ),
-  Place(
-    id: '2',
-    name: 'ハチ公像',
-    title: '渋谷のシンボル。',
-    thumbnailUrl:
-        'https://www.azabuderukui.info/wp-content/uploads/2024/01/title-chie-hachiko.jpg',
-    openingHours: (
-      openTime: DateTime(2024, 1, 1, 9),
-      closeTime: DateTime(2024, 1, 1, 17),
-    ),
-    avevageAmount: '1200円',
-  ),
-  Place(
-    id: '3',
-    name: '渋谷ヒカリエ',
-    title: '複合商業施設',
-    thumbnailUrl:
-        'https://town.mec-h.com/mh-omotesando/wp-content/uploads/2018/10/257358_13-01shibuya.jpg',
-    openingHours: (
-      openTime: DateTime(2024, 1, 1, 9),
-      closeTime: DateTime(2024, 1, 1, 17),
-    ),
-    avevageAmount: '1200円',
-  ),
-];
-
 class PlanDetailPage extends StatelessWidget {
   const PlanDetailPage({super.key});
 
@@ -88,7 +38,7 @@ class PlanDetailPage extends StatelessWidget {
         PlaceCard(
           place: plan,
           index: i,
-          endindex: dummyPlans.length - 1,
+          endindex: plans.length - 1,
         ),
       );
       i++;
@@ -100,7 +50,55 @@ class PlanDetailPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final i18n = Translations.of(context);
     final planDetailPagei18n = i18n.planDetailsPage;
-
+    final dummyPlans = <Place>[
+      const Place(
+        id: '0',
+        name: '宮下パークでショッピング',
+        thumbnailUrl:
+            'https://www.wirerope.co.jp/products/jakob/wordpress/wp-content/uploads/2022/03/%E5%AE%AE%E4%B8%8B%E5%85%AC%E5%9C%92_%E6%89%8B%E6%91%BA1.jpg',
+        title: '''
+    今回、Buddyが提案したこのプランは、渋谷駅前にある若者集まる商業施設の”宮下パーク”がメインのプランです！一階には日本食を楽しめる居酒屋が並ぶ
+      とともに、他の階では、モダンなファッション店や雑貨屋が並んでいます。最後に、緑感じる屋上でアクティビティや一休みなどパーソナルな時間をお楽しみください。
+    ''',
+        tags: ['＃ショッピング ', '＃アクティビティ', '＃グルメ', '時間: 1時間〜'],
+      ),
+      Place(
+        id: '1',
+        name: 'TitleTitleTitleTitleTitleTitleTitleTitle',
+        title: '世界的に有名な交差点。世界的に有名な交差点世界的に有名な交差点世界的に有名な交差点世界的に有名な交差点世界的に有名な交差点',
+        thumbnailUrl:
+            'https://www.cnn.co.jp/storage/2019/08/11/fbb9c1766abe6fa21b92a2808505d1e3/shibuya-crossing-tokyo-003.jpg',
+        openingHours: (
+          openTime: DateTime(2024, 1, 1, 9),
+          closeTime: DateTime(2024, 1, 1, 17),
+        ),
+        avevageAmount: '1200円',
+      ),
+      Place(
+        id: '2',
+        name: 'ハチ公像',
+        title: '渋谷のシンボル。',
+        thumbnailUrl:
+            'https://www.azabuderukui.info/wp-content/uploads/2024/01/title-chie-hachiko.jpg',
+        openingHours: (
+          openTime: DateTime(2024, 1, 1, 9),
+          closeTime: DateTime(2024, 1, 1, 17),
+        ),
+        avevageAmount: '1200円',
+      ),
+      Place(
+        id: '3',
+        name: '渋谷ヒカリエ',
+        title: '複合商業施設',
+        thumbnailUrl:
+            'https://town.mec-h.com/mh-omotesando/wp-content/uploads/2018/10/257358_13-01shibuya.jpg',
+        openingHours: (
+          openTime: DateTime(2024, 1, 1, 9),
+          closeTime: DateTime(2024, 1, 1, 17),
+        ),
+        avevageAmount: '1200円',
+      ),
+    ];
     final outputFormat = DateFormat(planDetailPagei18n.dateTime.dateFormat)
         .format(DateTime.now());
     return Scaffold(
