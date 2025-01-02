54426392e79cf95e6db9ed5f2204a8bda4e067dc
Tomoya <92212932+PenginPenPen@users.noreply.github.com>
2025-01-03 08:54:28 +0900

    自動生成

--- lib/presentation/my_plan/my_plan_notifier.g.dart
+++ lib/presentation/my_plan/my_plan_notifier.g.dart
@@ -1,26 +1,26 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'file_data_source.dart';
+part of 'my_plan_notifier.dart';
 
 // **************************************************************************
 // RiverpodGenerator
 // **************************************************************************
 
-String _$fileDataSourceHash() => r'6deda5807fa9d63bd021501586d53f0a8ceb10d5';
+String _$myPlanNotifierHash() => r'b5fffa7bcdcc1ed2d9f0d93824a59ddcc418d704';
 
-/// See also [FileDataSource].
-@ProviderFor(FileDataSource)
-final fileDataSourceProvider =
-    AutoDisposeNotifierProvider<FileDataSource, void>.internal(
-  FileDataSource.new,
-  name: r'fileDataSourceProvider',
+/// See also [MyPlanNotifier].
+@ProviderFor(MyPlanNotifier)
+final myPlanNotifierProvider =
+    AutoDisposeNotifierProvider<MyPlanNotifier, void>.internal(
+  MyPlanNotifier.new,
+  name: r'myPlanNotifierProvider',
   debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
       ? null
-      : _$fileDataSourceHash,
+      : _$myPlanNotifierHash,
   dependencies: null,
   allTransitiveDependencies: null,
 );
 
-typedef _$FileDataSource = AutoDisposeNotifier<void>;
+typedef _$MyPlanNotifier = AutoDisposeNotifier<void>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
