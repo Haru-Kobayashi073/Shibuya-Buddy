ffd5b825fd868e4577fed8846c5014e60adebf52
Tomoya <92212932+PenginPenPen@users.noreply.github.com>
2025-01-04 04:34:19 +0900

    データが一つもない時に表示するウィジェット（仮）

---
+++ lib/presentation/my_plan/components/nondata.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import '../../../utils/styles/app_text_style.dart';
+
+class Nondata extends StatelessWidget {
+  const Nondata({
+    super.key,
+    required this.message,
+  });
+  final String message;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        message,
+        style: AppTextStyle.textStyle.copyWith(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+}
