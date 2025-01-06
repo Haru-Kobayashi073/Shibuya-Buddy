9702cfe1399dd029f007b56f2ac5efd92931251b
Tomoya <92212932+PenginPenPen@users.noreply.github.com>
2025-01-06 21:30:43 +0900

    自動生成

--- lib/presentation/my_plan/my_plan_notifier.g.dart
+++ lib/presentation/my_plan/my_plan_notifier.g.dart
@@ -6,12 +6,12 @@ part of 'my_plan_notifier.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$myPlanNotifierHash() => r'b5fffa7bcdcc1ed2d9f0d93824a59ddcc418d704';
+String _$myPlanNotifierHash() => r'f825cb32c945ae8663ea1ee035f2b9dd2d68ef4d';
 
 /// See also [MyPlanNotifier].
 @ProviderFor(MyPlanNotifier)
 final myPlanNotifierProvider =
-    AutoDisposeNotifierProvider<MyPlanNotifier, void>.internal(
+    AutoDisposeAsyncNotifierProvider<MyPlanNotifier, MyPlanState>.internal(
   MyPlanNotifier.new,
   name: r'myPlanNotifierProvider',
   debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
@@ -21,6 +21,6 @@ final myPlanNotifierProvider =
   allTransitiveDependencies: null,
 );
 
-typedef _$MyPlanNotifier = AutoDisposeNotifier<void>;
+typedef _$MyPlanNotifier = AutoDisposeAsyncNotifier<MyPlanState>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
