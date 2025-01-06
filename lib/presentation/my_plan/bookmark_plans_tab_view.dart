75283ab2f22e62c6be2e1a89e758eec35490c5e5
Tomoya <92212932+PenginPenPen@users.noreply.github.com>
2025-01-06 21:30:10 +0900

    asyncStateを使いブックマークしたプランを表示するよう変更

--- lib/presentation/my_plan/bookmark_plans_tab_view.dart
+++ lib/presentation/my_plan/bookmark_plans_tab_view.dart
@@ -1,21 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-import '../home/components/plan_list.dart';
-import 'mock.dart';
+import 'components/bookmark_plan_list.dart';
+import 'components/nondata.dart';
+import 'my_plan_notifier.dart';
 
-class BookmarkPlansTabView extends StatefulWidget {
+class BookmarkPlansTabView extends ConsumerWidget {
   const BookmarkPlansTabView({super.key});
 
   @override
-  State<BookmarkPlansTabView> createState() => _BookmarkPlansTabViewState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    final asyncState = ref.watch(myPlanNotifierProvider);
 
-class _BookmarkPlansTabViewState extends State<BookmarkPlansTabView> {
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16),
-      child: PlanList(plans: dummyPlan),
+    return asyncState.when(
+      data: (state) {
+        final plans = state.bookmarkPlanList;
+        return plans.isEmpty
+            ? const Nondata(message: 'ブックマークしているプランはありません。')
+            : ListView(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: BookmarkPlanList(plans: plans),
+                  ),
+                ],
+              );
+      },
+      loading: () {
+        return const Center(child: CircularProgressIndicator());
+      },
+      error: (error, stack) {
+        return Center(
+          child: Text('エラーが発生しました: $error'), //エラーページ
+        );
+      },
     );
   }
 }
--- lib/presentation/my_plan/my_plan_notifier.dart
+++ lib/presentation/my_plan/my_plan_notifier.dart
@@ -1,21 +1,35 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 import '../../domain/entities/plan.dart';
+import '../../domain/entities/topic.dart';
 import '../../infrastructure/firebase/cloud_firestore_provider.dart';
+import '../../infrastructure/firebase/firebase_auth_provider.dart';
+import '../../utils/providers/scaffold_messenger/scaffold_messenger.dart'
+    as custom;
+import '../../utils/providers/scaffold_messenger/scaffold_messenger.dart';
+import 'my_plan_state.dart';
 
 part 'my_plan_notifier.g.dart';
 
 @riverpod
 class MyPlanNotifier extends _$MyPlanNotifier {
   FirebaseFirestore get firestore => ref.read(cloudFirestoreProvider);
+  FirebaseAuth get fireauth => ref.read(firebaseAuthProvider);
+
+  custom.ScaffoldMessenger get scaffoldMessenger =>
+      ref.read(scaffoldMessengerProvider.notifier);
+
   @override
-  void build() {
-    return;
+  Future<MyPlanState> build() async {
+    final uid = fireauth.currentUser?.uid;
+    final plans = await _buildBookmarkPlans(userId: uid.toString());
+    return MyPlanState(bookmarkPlanList: plans);
   }
 
-  Future<List<String>> getBookmarkedPlanIds({
+  Future<List<String>> _getBookmarkedPlanIds({
     required String userId,
   }) async {
     try {
@@ -25,29 +39,30 @@ class MyPlanNotifier extends _$MyPlanNotifier {
           !snapshot.data()!.containsKey('bookmarkedPlanIds')) {
         return [];
       }
-
       final bookmarkedPlanIds = snapshot.data()!['bookmarkedPlanIds'];
-      if (bookmarkedPlanIds == null) {
+      if (bookmarkedPlanIds == null || bookmarkedPlanIds is! List<dynamic>) {
         return [];
       }
 
-      return List<String>.from(bookmarkedPlanIds as Iterable<dynamic>);
-    } catch (e) {
-      debugPrint('ブックマークプランの取得エラー: $e');
+      return List<String>.from(bookmarkedPlanIds);
+    } on FirebaseException catch (e) {
+      scaffoldMessenger.showExceptionSnackBar('プランIDの取得に失敗しました: $e');
       return [];
     }
   }
 
-  Future<List<Plan>> buildBookmarkPlans({required String userId}) async {
+  Future<List<Plan>> _buildBookmarkPlans({required String userId}) async {
     try {
-      final planIds = await getBookmarkedPlanIds(userId: userId);
+      final planIds = await _getBookmarkedPlanIds(userId: userId);
       if (planIds.isEmpty) {
         return [];
       }
 
       final plans = <Plan>[];
       final planSnapshots = await Future.wait(
-        planIds.map((id) => firestore.collection('plans').doc(id).get()),
+        planIds.map((id) async {
+          return firestore.collection('PopularPlans').doc(id).get();
+        }),
       );
 
       for (final snapshot in planSnapshots) {
@@ -55,31 +70,52 @@ class MyPlanNotifier extends _$MyPlanNotifier {
           continue;
         }
 
-        final data = snapshot.data()!;
+        final data = snapshot.data();
+        if (data == null) {
+          continue;
+        }
+        final topicList = <Topic>[];
+        for (final topic in data['topics'] as List<dynamic>) {
+          topicList.add(Topic(name: topic.toString(), thumbnailUrl: ''));
+        }
+
         plans.add(
           Plan(
             title: data['title'].toString(),
             description: data['description'].toString(),
             thumbnailUrl: data['thumbnailUrl'].toString(),
-            topics: [],
+            topics: topicList,
           ),
         );
       }
 
       return plans;
-    } catch (e) {
-      debugPrint('ブックマークプランの構築エラー: $e');
+    } on FirebaseException catch (e) {
+      scaffoldMessenger.showExceptionSnackBar('プランの取得中にエラーが発生しました: $e');
+      debugPrint(e.toString());
       return [];
     }
   }
 
-  Future<void> addPlanToBookmarks({
-    required String userId,
-    required String planId,
-  }) async {
-    await firestore.collection('users').doc(userId).update({
-      'bookmarkedPlanIds': FieldValue.arrayUnion([planId]),
-    });
-    debugPrint('Add!');
-  }
+  // Future<void> addPlans(String userId) async {
+  //   final planMap = {
+  //     'title': '宮下パークでショッピング',
+  //     'description': '説明説明',
+  //     'thumbnailUrl': 'https://placehold.jp/80x50.png',
+  //     'topics': [
+  //       '所要時間: 1時間〜',
+  //       '人数: １人〜',
+  //       '＃ショッピング',
+  //       'アクティビティ',
+  //     ],
+  //   };
+
+  //   final docRef = await firestore.collection('PopularPlans').add(planMap);
+  //   final newPlanId = docRef.id;
+  //   final userDocRef =
+  //       firestore.collection('users').doc(userId);
+  //   await userDocRef.update({
+  //     'bookmarkedPlanIds': FieldValue.arrayUnion([newPlanId]),
+  //   });
+  // }
 }
