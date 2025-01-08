99b367ca5b3bf5971f2fb10b4f8b7fd0afa6225c
Tomoya <92212932+PenginPenPen@users.noreply.github.com>
2025-01-08 22:53:02 +0900

    analyzeのエラーが出ないよう修正

--- lib/presentation/my_plan/components/bookmark_plan_list.dart
+++ lib/presentation/my_plan/components/bookmark_plan_list.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../../../domain/entities/plan.dart';
 import '../../../utils/styles/app_color.dart';
