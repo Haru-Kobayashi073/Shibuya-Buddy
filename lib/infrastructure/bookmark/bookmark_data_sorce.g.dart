748eb9bb6c924a2bde0191013e974523ff18c062
Tomoya <92212932+PenginPenPen@users.noreply.github.com>
2025-01-08 20:18:43 +0900

    自動生成

--- lib/infrastructure/bookmark/bookmark_data_sorce.g.dart
+++ lib/infrastructure/bookmark/bookmark_data_sorce.g.dart
@@ -1,26 +1,26 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'my_plan_notifier.dart';
+part of 'bookmark_data_sorce.dart';
 
 // **************************************************************************
 // RiverpodGenerator
 // **************************************************************************
 
-String _$myPlanNotifierHash() => r'3150e6d9b3d1588e0ded68c7388cd412f614a4e4';
+String _$bookmarkDataSorceHash() => r'673ed2ed18c299e0c9dc11d2c79d5be3ce854bda';
 
-/// See also [MyPlanNotifier].
-@ProviderFor(MyPlanNotifier)
-final myPlanNotifierProvider =
-    AutoDisposeAsyncNotifierProvider<MyPlanNotifier, MyPlanState>.internal(
-  MyPlanNotifier.new,
-  name: r'myPlanNotifierProvider',
+/// See also [BookmarkDataSorce].
+@ProviderFor(BookmarkDataSorce)
+final bookmarkDataSorceProvider =
+    AutoDisposeNotifierProvider<BookmarkDataSorce, void>.internal(
+  BookmarkDataSorce.new,
+  name: r'bookmarkDataSorceProvider',
   debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
       ? null
-      : _$myPlanNotifierHash,
+      : _$bookmarkDataSorceHash,
   dependencies: null,
   allTransitiveDependencies: null,
 );
 
-typedef _$MyPlanNotifier = AutoDisposeAsyncNotifier<MyPlanState>;
+typedef _$BookmarkDataSorce = AutoDisposeNotifier<void>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
