9f8cc9eebb573d1145e849d71eb986011bc2d848
shi0n0 <shiono.yui1080@gmail.com>
2025-01-11 13:19:35 +0900

    GoogleMapを設置

--- lib/presentation/map/map_page.dart
+++ lib/presentation/map/map_page.dart
@@ -1,14 +1,33 @@
+// lib/presentation/pages/google_map_page.dart
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../../utils/styles/app_color.dart';
 import '../../utils/styles/app_text_style.dart';
+import './map_page_notifier.dart';
 import 'components/day_tab_bar.dart';
 
-class MapPage extends StatelessWidget {
+class MapPage extends HookConsumerWidget {
   const MapPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final location = ref.watch(mapNotifierProvider);
+    final mapController = useState<GoogleMapController?>(null);
+
+    useEffect(
+      () {
+        Future.microtask(() async {
+          await ref.read(mapNotifierProvider.notifier).loadCurrentLocation();
+        });
+
+        return null;
+      },
+      [],
+    );
+
     return DefaultTabController(
       length: 2,
       child: Scaffold(
@@ -27,6 +46,15 @@ class MapPage extends StatelessWidget {
             child: const DayTabBar(),
           ),
         ),
+        body: location == null
+            ? const Center(child: CircularProgressIndicator())
+            : GoogleMap(
+                initialCameraPosition: CameraPosition(
+                  target: LatLng(location.latitude, location.longitude),
+                  zoom: 14,
+                ),
+                onMapCreated: (controller) => mapController.value = controller,
+              ),
       ),
     );
   }
