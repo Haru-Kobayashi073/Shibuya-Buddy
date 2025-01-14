635edf0ac65e8fadb214e376fe8979d8fc367538
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2025-01-15 03:14:56 +0900

    ブックマーク機能実装のためwipコミット

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,30 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+const firestore = admin.firestore();
+
+export const rankingPlan = functions.https.onCall((_, __) => {
+    const plansRef = firestore.collection("plans");
+    const popularPlansRef = firestore.collection("popular_plans");
+
+    popularPlansRef.get().then((snapshot) => {
+        snapshot.docs.map((doc) => {
+            popularPlansRef.doc(doc.id).delete();
+        });
+    });
+
+    return plansRef
+        .orderBy("bookmarkedUserIds", "desc")
+        .limit(10)
+        .get()
+        .then((snapshot) => {
+            snapshot.docs.map((doc) => {
+                const data = doc.data();
+                popularPlansRef.doc(doc.id).set(data);
+            });
+        })
+        .catch((error) => {
+            console.error("Error ranking plans: ", error);
+        });
+});
