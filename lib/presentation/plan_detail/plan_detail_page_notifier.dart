d730dc4c4602449a97a64dd94e7f415358e144c3
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2025-01-15 03:43:56 +0900

    プラン詳細のnotifierでブックマーク処理を挟む

--- lib/presentation/plan_detail/plan_detail_page_notifier.dart
+++ lib/presentation/plan_detail/plan_detail_page_notifier.dart
@@ -3,14 +3,20 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 import '../../domain/entities/place.dart';
 import '../../domain/entities/plan.dart';
 import '../../infrastructure/place/place_data_source.dart';
+import '../../infrastructure/plan/plan_data_source.dart';
+import '../../utils/providers/scaffold_messenger/scaffold_messenger.dart';
 import 'plan_detail_page_state.dart';
 
 part 'plan_detail_page_notifier.g.dart';
 
 @riverpod
 class PlanDetailPageNotifier extends _$PlanDetailPageNotifier {
+  PlanDataSource get planDataSource =>
+      ref.read(planDataSourceProvider.notifier);
   PlaceDataSource get placeDataSource =>
       ref.read(placeDataSourceProvider.notifier);
+  ScaffoldMessenger get scaffoldMessenger =>
+      ref.read(scaffoldMessengerProvider.notifier);
 
   @override
   Future<PlanDetailPageState> build(Plan plan) async {
@@ -24,12 +30,35 @@ class PlanDetailPageNotifier extends _$PlanDetailPageNotifier {
     return places;
   }
 
-  void onBookmarkButtonTap() {
-    state = AsyncValue.data(
-      state.requireValue.copyWith(
-        plan: state.requireValue.plan
-            .copyWith(isBookmarked: !state.requireValue.plan.isBookmarked),
-      ),
-    );
+  Future<void> onBookmarkButtonTap() async {
+    final plan = state.requireValue.plan;
+    final isBookmark = plan.isBookmarked;
+
+    try {
+      if (isBookmark) {
+        final updatedPlan = plan.copyWith(
+          isBookmarked: false,
+          bookmarkCount: plan.bookmarkCount - 1,
+        );
+        // ブックマーク解除の処理を追加
+
+        state = AsyncValue.data(
+          state.requireValue.copyWith(plan: updatedPlan),
+        );
+      } else {
+        final updatedPlan = plan.copyWith(
+          isBookmarked: true,
+          bookmarkCount: plan.bookmarkCount + 1,
+        );
+        await planDataSource.bookmarkPlan(plan: updatedPlan);
+        state = AsyncValue.data(
+          state.requireValue.copyWith(plan: updatedPlan),
+        );
+      }
+    } on Exception catch (_) {
+      scaffoldMessenger.showExceptionSnackBar(
+        'Failed to bookmark the plan. Please try again later.',
+      );
+    }
   }
 }
