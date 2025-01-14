f85bbe610e782d65cbb146a4f230eb764327f37e
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2025-01-15 05:40:59 +0900

    各々で状態監視をせず、ルートで行うよう変更

--- lib/presentation/my_plan/bookmark_plans_tab_view.dart
+++ lib/presentation/my_plan/bookmark_plans_tab_view.dart
@@ -1,76 +1,65 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-import '../../error_page.dart';
+import '../../domain/entities/plan.dart';
 import '../../i18n/strings.g.dart';
 import '../../utils/styles/app_color.dart';
-import '../components/loading_overlay.dart';
 import '../home/components/recent_plan.dart';
 import 'components/empty_data.dart';
 import 'my_plan_page_notifier.dart';
 
 class BookmarkPlansTabView extends ConsumerWidget {
-  const BookmarkPlansTabView({super.key});
+  const BookmarkPlansTabView({
+    super.key,
+    required this.plans,
+  });
+  final List<Plan> plans;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final asyncState = ref.watch(myPlanPageNotifierProvider);
-    final notifier = ref.watch(myPlanPageNotifierProvider.notifier);
+    final notifier = ref.read(myPlanPageNotifierProvider.notifier);
     final i18n = Translations.of(context);
     final bookmarkItemi18n = i18n.myPlanPage.bookmarkItems;
     final width = MediaQuery.of(context).size.width;
 
-    return asyncState.when(
-      data: (state) {
-        final plans = state.bookmarkPlanList;
-        return plans.isEmpty
-            ? EmptyDataView(
-                message: bookmarkItemi18n.nondata,
-                labelText: bookmarkItemi18n.reloading,
-                onPressed: () async {
-                  await notifier.buildBookmarkPlans();
-                },
-              )
-            : RefreshIndicator(
-                onRefresh: () async {
-                  await notifier.buildBookmarkPlans();
-                },
-                child: Padding(
-                  padding: const EdgeInsets.all(16),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: plans
-                        .map(
-                          (plan) => Row(
-                            children: [
-                              SizedBox(
-                                width: width * 0.8,
-                                child: RecentPlan(
-                                  plan: plan,
-                                ),
-                              ),
-                              const Spacer(),
-                              IconButton(
-                                onPressed: () async {
-                                  await notifier.unBookmark(plan: plan);
-                                },
-                                icon: const Icon(
-                                  Icons.bookmark,
-                                  color: AppColor.yellow600Primary,
-                                ),
-                              ),
-                            ],
+    return plans.isNotEmpty
+        ? RefreshIndicator(
+            onRefresh: () async => ref.invalidate(myPlanPageNotifierProvider),
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: plans
+                    .map(
+                      (plan) => Row(
+                        children: [
+                          SizedBox(
+                            width: width * 0.8,
+                            child: RecentPlan(
+                              plan: plan,
+                            ),
                           ),
-                        )
-                        .toList(),
-                  ),
-                ),
-              );
-      },
-      loading: Loading.new,
-      error: (_, __) => ErrorPage(
-        onRetry: () async => notifier.buildBookmarkPlans(),
-      ),
-    );
+                          const Spacer(),
+                          IconButton(
+                            onPressed: () async {
+                              await notifier.unBookmark(plan: plan);
+                            },
+                            icon: const Icon(
+                              Icons.bookmark,
+                              color: AppColor.yellow600Primary,
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                    .toList(),
+              ),
+            ),
+          )
+        : EmptyDataView(
+            message: bookmarkItemi18n.nondata,
+            labelText: bookmarkItemi18n.reloading,
+            onPressed: () => ref.invalidate(myPlanPageNotifierProvider),
+          );
   }
 }
