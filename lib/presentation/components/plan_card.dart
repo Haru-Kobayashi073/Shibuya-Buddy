35cb6104a9ae9f61a17a945278a9463d6600d473
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2025-01-16 05:24:10 +0900

    人気のプラン・トピックを取得する際にranking順にソートする

--- lib/presentation/components/plan_card.dart
+++ lib/presentation/components/plan_card.dart
@@ -21,77 +21,71 @@ class PlanCard extends StatelessWidget {
         ),
         child: GestureDetector(
           onTap: () {},
-          child: DecoratedBox(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-            ),
-            child: Stack(
-              alignment: Alignment.topLeft,
-              children: <Widget>[
-                Column(
-                  children: [
-                    AspectRatio(
-                      aspectRatio: 16 / 12,
-                      child: ClipRRect(
-                        borderRadius: const BorderRadius.only(
-                          topLeft: Radius.circular(12),
-                          topRight: Radius.circular(12),
-                        ),
-                        child: PersistentCachedNetworkImage(
-                          imageUrl: plan.thumbnailUrl,
-                        ),
+          child: Stack(
+            alignment: Alignment.topLeft,
+            children: <Widget>[
+              Column(
+                children: [
+                  AspectRatio(
+                    aspectRatio: 16 / 12,
+                    child: ClipRRect(
+                      borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(12),
+                        topRight: Radius.circular(12),
+                      ),
+                      child: PersistentCachedNetworkImage(
+                        imageUrl: plan.thumbnailUrl,
                       ),
                     ),
-                    AspectRatio(
-                      aspectRatio: 16 / 7,
-                      child: DecoratedBox(
-                        decoration: const BoxDecoration(
-                          color: AppColor.blue50Background,
-                          borderRadius: BorderRadius.only(
-                            bottomLeft: Radius.circular(12),
-                            bottomRight: Radius.circular(12),
-                          ),
+                  ),
+                  AspectRatio(
+                    aspectRatio: 16 / 7,
+                    child: DecoratedBox(
+                      decoration: const BoxDecoration(
+                        color: AppColor.blue50Background,
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(12),
+                          bottomRight: Radius.circular(12),
                         ),
-                        child: Padding(
-                          padding: const EdgeInsets.all(8),
-                          child: Column(
-                            children: [
-                              Flexible(
-                                child: Text(
-                                  plan.title,
-                                  maxLines: 1,
-                                  overflow: TextOverflow.ellipsis,
-                                  style: AppTextStyle.textStyle.copyWith(
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.bold,
-                                    overflow: TextOverflow.clip,
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8),
+                        child: Column(
+                          children: [
+                            Flexible(
+                              child: Text(
+                                plan.title,
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                                style: AppTextStyle.textStyle.copyWith(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold,
+                                  overflow: TextOverflow.clip,
+                                ),
+                              ),
+                            ),
+                            Flexible(
+                              child: Align(
+                                alignment: Alignment.topLeft,
+                                child: Offstage(
+                                  child: CategoryTags(
+                                    tags:
+                                        plan.topics.map((e) => e.name).toList(),
+                                    tagColor: AppColor.white,
+                                    spacing: 4,
                                   ),
                                 ),
                               ),
-                              Flexible(
-                                child: Align(
-                                  alignment: Alignment.topLeft,
-                                  child: Offstage(
-                                    child: CategoryTags(
-                                      tags: plan.topics
-                                          .map((e) => e.name)
-                                          .toList(),
-                                      tagColor: AppColor.white,
-                                      spacing: 4,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
+                            ),
+                          ],
                         ),
                       ),
                     ),
-                  ],
-                ),
-                RankingLabel(ranking: plan.ranking),
-              ],
-            ),
+                  ),
+                ],
+              ),
+              RankingLabel(ranking: plan.ranking),
+            ],
           ),
         ),
       ),
