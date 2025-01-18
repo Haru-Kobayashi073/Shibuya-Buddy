cd149e35b82be10b4b11c5926d06a0a60aab973f
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2025-01-18 19:23:40 +0900

    課金詳細ページのNotifierを実装

---
+++ lib/presentation/bill_details/bill_detail_page_notifier.dart
@@ -0,0 +1,47 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import '../../i18n/strings.g.dart';
+import '../../utils/providers/in_app_purchase/in_app_purchase_service.dart';
+
+part 'bill_detail_page_notifier.g.dart';
+
+@riverpod
+class BillDetailPageNotifier extends _$BillDetailPageNotifier {
+  InAppPurchaseService get inAppPurchaseService =>
+      ref.read(inAppPurchaseServiceProvider.notifier);
+
+  @override
+  String build() {
+    return t.billDetailsPage.pricingOptions.oneDay.duration;
+  }
+
+  void selectPlan(String duration) {
+    state = duration;
+    return;
+  }
+
+  Future<void> purchaseItem() async {
+    try {
+      final packageId = getPackageId();
+      await inAppPurchaseService.makePurchase(packageId);
+    } on Exception catch (e) {
+      print('purchaseItem error: $e');
+    }
+  }
+
+  String getPackageId() {
+    if (state == t.billDetailsPage.pricingOptions.oneDay.duration) {
+      return r'$rc_lifetime';
+    } else if (state == t.billDetailsPage.pricingOptions.threeDays.duration) {
+      return '3day-premium';
+    } else if (state == t.billDetailsPage.pricingOptions.fiveDays.duration) {
+      return '5day-premium';
+    } else if (state == t.billDetailsPage.pricingOptions.sevenDays.duration) {
+      return '7day-premium';
+    } else if (state == t.billDetailsPage.pricingOptions.lifetime.duration) {
+      return 'unlimited-premium';
+    } else {
+      return '';
+    }
+  }
+}
---
+++ lib/presentation/bill_details/bill_detail_page_notifier.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'bill_detail_page_notifier.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$billDetailPageNotifierHash() =>
+    r'e4660c7f11f165cfed9ff23bfe7e5af006389488';
+
+/// See also [BillDetailPageNotifier].
+@ProviderFor(BillDetailPageNotifier)
+final billDetailPageNotifierProvider =
+    AutoDisposeNotifierProvider<BillDetailPageNotifier, String>.internal(
+  BillDetailPageNotifier.new,
+  name: r'billDetailPageNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$billDetailPageNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$BillDetailPageNotifier = AutoDisposeNotifier<String>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
