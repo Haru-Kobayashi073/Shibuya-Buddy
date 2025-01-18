b2d35d1a395d42b4b106082da3d5f61dfbd6c606
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2025-01-18 17:00:44 +0900

    自動生成差分

--- lib/i18n/strings.g.dart
+++ lib/i18n/strings.g.dart
@@ -6,7 +6,7 @@
 /// Locales: 4
 /// Strings: 908 (227 per locale)
 ///
-/// Built on 2025-01-17 at 07:52 UTC
+/// Built on 2025-01-18 at 06:54 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint, unused_import
