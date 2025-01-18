927c6f03fccf0a83c31db26f4313c5d8b71bcc8d
shi0n0 <shiono.yui1080@gmail.com>
2025-01-18 16:09:35 +0900

    マップの初期ロケーションを更新(WIP)

--- lib/presentation/map/map_page.dart
+++ lib/presentation/map/map_page.dart
@@ -1,55 +1,40 @@
-// lib/presentation/pages/google_map_page.dart
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-import '../../domain/entities/plan.dart';
-import '../../utils/styles/app_color.dart';
-import '../../utils/styles/app_text_style.dart';
-import '../components/loading_overlay.dart';
-import './map_page_notifier.dart';
+import '../../domain/entities/place.dart';
 
-class MapPage extends HookConsumerWidget {
-  const MapPage({super.key, required this.plan});
-  final Plan plan;
+class MapPage extends StatelessWidget {
+  const MapPage({super.key, required this.place});
+  final List<Place> place;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final location = ref.watch(mapNotifierProvider);
-    final mapController = useState<GoogleMapController?>(null);
-
-    useEffect(
-      () {
-        Future.microtask(() async {
-          await ref.read(mapNotifierProvider.notifier).loadCurrentLocation();
-        });
-
-        return null;
-      },
-      [],
-    );
-
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: AppColor.blue50Background,
-        title: Text(
-          '東京一泊二日',
-          style: AppTextStyle.textStyle.copyWith(
-            fontWeight: FontWeight.bold,
-            fontSize: 14,
-          ),
-        ),
+        title: const Text('マップ'),
       ),
-      body: location == null
-          ? const LoadingOverlay()
-          : GoogleMap(
-              initialCameraPosition: CameraPosition(
-                target: LatLng(location.latitude, location.longitude),
-                zoom: 14,
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: LatLng(
+            place.first.location.latitude,
+            place.first.location.longitude,
+          ),
+          zoom: 14,
+        ),
+        markers: place
+            .map(
+              (place) => Marker(
+                markerId: MarkerId(
+                  '${place.location.latitude},${place.location.longitude}',
+                ),
+                position: LatLng(
+                  place.location.latitude,
+                  place.location.longitude,
+                ),
               ),
-              onMapCreated: (controller) => mapController.value = controller,
-            ),
+            )
+            .toSet(),
+      ),
     );
   }
 }
