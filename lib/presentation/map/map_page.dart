0427e33777e83f7a21ebcd05267f9998ecdde973
shi0n0 <shiono.yui1080@gmail.com>
2025-01-19 22:09:39 +0900

    不要なコメントアウトを削除し微修正

--- lib/presentation/map/map_page.dart
+++ lib/presentation/map/map_page.dart
@@ -5,52 +5,44 @@ import 'package:url_launcher/url_launcher.dart';
 import '../../domain/entities/place.dart';
 
 class MapPage extends StatelessWidget {
-  const MapPage({super.key, required this.place});
+  const MapPage({
+    super.key,
+    required this.places,
+  });
 
-  final List<Place> place;
+  final List<Place> places;
 
-  /// Google Maps アプリがあればアプリを開き、なければブラウザで Google Maps を開く
-  Future<void> _openGoogleMapApp(double lat, double lng) async {
-    // Google Maps アプリを呼び出すための URL スキーム
-    final googleMapSchemeUrl = 'comgooglemaps://?q=$lat,$lng';
-    final uriScheme = Uri.parse(googleMapSchemeUrl);
+  Future<void> _openGoogleMapApp(double latitude, double longitude) async {
+    final uriScheme = Uri.parse('comgooglemaps://?q=$latitude,$longitude');
+    final uriFallback = Uri.parse(
+      'https://www.google.com/maps/search/?api=1&query=$latitude,$longitude',
+    );
 
-    // アプリを開けない場合のフォールバック（ブラウザで開く）
-    final fallbackUrl =
-        'https://www.google.com/maps/search/?api=1&query=$lat,$lng';
-    final uriFallback = Uri.parse(fallbackUrl);
-
-    // url_launcher の canLaunchUrl / launchUrl を使用
     if (await canLaunchUrl(uriScheme)) {
-      // Google Maps アプリを起動
       await launchUrl(uriScheme);
     } else if (await canLaunchUrl(uriFallback)) {
-      // ブラウザで Google Maps を開く
       await launchUrl(uriFallback);
     } else {
-      // 開けない場合のエラー処理
       debugPrint('Could not open Google Maps.');
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    // Marker リストを生成
-    final markers = place.map((p) {
-      final lat = p.location.latitude;
-      final lng = p.location.longitude;
+    final markers = places.map((place) {
+      final lat = place.location.latitude;
+      final lng = place.location.longitude;
+      final markerPosition = LatLng(lat, lng);
       return Marker(
-        markerId: MarkerId(p.name),
-        position: LatLng(lat, lng),
-        // Marker タップ時に Google Maps アプリへ飛ばしたい場合
-        onTap: () {
-          _openGoogleMapApp(lat, lng);
+        markerId: MarkerId(place.name),
+        position: markerPosition,
+        onTap: () async {
+          await _openGoogleMapApp(lat, lng);
         },
         infoWindow: InfoWindow(
-          title: p.name,
-          // 吹き出し（InfoWindow）自体をタップしたときに Google Maps アプリへ飛ばしたい場合
-          onTap: () {
-            _openGoogleMapApp(lat, lng);
+          title: place.name,
+          onTap: () async {
+            await _openGoogleMapApp(lat, lng);
           },
         ),
       );
@@ -63,8 +55,8 @@ class MapPage extends StatelessWidget {
       body: GoogleMap(
         initialCameraPosition: CameraPosition(
           target: LatLng(
-            place.first.location.latitude,
-            place.first.location.longitude,
+            places.first.location.latitude,
+            places.first.location.longitude,
           ),
           zoom: 14,
         ),
