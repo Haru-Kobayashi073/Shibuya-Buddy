e3df299e880a701cb46680fde2a2fc7810be0fe6
shi0n0 <shiono.yui1080@gmail.com>
2025-01-19 23:02:56 +0900

    location_repositoryを削除

--- lib/domain/repositories/location_repository.dart
+++
@@ -1,6 +0,0 @@
-// lib/domain/repositories/location_repository.dart
-import '../entities/location.dart';
-
-abstract interface class LocationRepository {
-  Future<Location> getCurrentLocation();
-}
