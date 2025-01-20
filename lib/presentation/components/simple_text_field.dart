c00a42065bb2a62ec40581a2c210553d1dec89b1
shi0n0 <shiono.yui1080@gmail.com>
2025-01-20 15:52:54 +0900

    Byte数で入力文字数を制限する処理を追加

--- lib/presentation/components/simple_text_field.dart
+++ lib/presentation/components/simple_text_field.dart
@@ -15,7 +15,9 @@ class SimpleTextField extends StatelessWidget {
     required this.onFieldSubmitted,
     this.icon,
     required this.label,
+    this.maxBytes,
   });
+
   final TextEditingController controller;
   final FocusNode? focusNode;
   final TextInputType keyboardType;
@@ -25,6 +27,7 @@ class SimpleTextField extends StatelessWidget {
   final void Function(String) onFieldSubmitted;
   final Widget? icon;
   final String label;
+  final int? maxBytes;
 
   @override
   Widget build(BuildContext context) {
@@ -39,6 +42,31 @@ class SimpleTextField extends StatelessWidget {
       style: AppTextStyle.textStyle.copyWith(
         color: AppColor.black,
       ),
+      onChanged: (value) {
+        if (maxBytes != null) {
+          final byteCount = value.runes.fold(0, (sum, char) {
+            return sum + (char > 255 ? 2 : 1); // 全角:2バイト, 半角:1バイト
+          });
+          if (byteCount > maxBytes!) {
+            // 制限を超えた場合にトリミング
+            var truncatedValue = '';
+            var currentByteCount = 0;
+            for (final char in value.runes) {
+              final charBytes = char > 255 ? 2 : 1;
+              if (currentByteCount + charBytes <= maxBytes!) {
+                truncatedValue += String.fromCharCode(char);
+              }
+              currentByteCount += charBytes;
+            }
+            controller
+              ..text = truncatedValue
+              ..selection = TextSelection.fromPosition(
+                TextPosition(offset: truncatedValue.length),
+              );
+          }
+        }
+      },
+      onFieldSubmitted: onFieldSubmitted,
       decoration: InputDecoration(
         suffixIcon: icon,
         border: const OutlineInputBorder(
