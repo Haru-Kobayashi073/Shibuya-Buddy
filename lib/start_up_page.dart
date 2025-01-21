3aad306276a563517407860388476f7838b3d78a
Tomoya <92212932+PenginPenPen@users.noreply.github.com>
2025-01-21 16:38:03 +0900

    不要な処理の削除

--- lib/start_up_page.dart
+++ lib/start_up_page.dart
@@ -2,10 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-import 'i18n/strings.g.dart';
 import 'infrastructure/firebase/firebase_auth_state_listener.dart';
 import 'presentation/components/loading_overlay.dart';
-import 'utils/providers/scaffold_messenger/scaffold_messenger.dart';
 import 'utils/routes/app_router.dart';
 
 class StartUpPage extends HookConsumerWidget {
@@ -14,23 +12,11 @@ class StartUpPage extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final user = ref.watch(firebaseAuthStateListenerProvider);
-    final message = ref.watch(scaffoldMessengerProvider.notifier);
-    final i18n = t.authentication.emailVerificationPage.snackBar;
     WidgetsBinding.instance.addPostFrameCallback((_) {
       user.when(
         data: (user) {
           if (user != null) {
-            if (!user.emailVerified) {
-              context.go(
-                EmailVerificationPageRouteData(email: user.email.toString())
-                    .location,
-              );
-              WidgetsBinding.instance.addPostFrameCallback((_) {
-                message.showExceptionSnackBar(i18n.emailVerification);
-              });
-            } else {
-              context.go(const HomeScreenRouteData().location);
-            }
+            context.go(const HomeScreenRouteData().location);
           } else {
             context.go(const SignInPageRouteData().location);
           }
