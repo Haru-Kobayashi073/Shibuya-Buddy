3db1085cee12d38388131d1c1a1027cb8ed4553f
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2025-01-22 20:18:09 +0900

    StatefulWidgetからStatelessに変更

--- lib/presentation/my_plan/my_plan_page.dart
+++ lib/presentation/my_plan/my_plan_page.dart
@@ -9,82 +9,68 @@ import 'bookmark_plans_tab_view.dart';
 import 'created_plans_tab_view.dart';
 import 'my_plan_page_notifier.dart';
 
-class MyPlanPage extends ConsumerStatefulWidget {
+class MyPlanPage extends ConsumerWidget {
   const MyPlanPage({super.key});
 
   @override
-  ConsumerState<MyPlanPage> createState() => _MyPlanPageState();
-}
-
-class _MyPlanPageState extends ConsumerState<MyPlanPage>
-    with SingleTickerProviderStateMixin {
-  late TabController _tabController;
-
-  @override
-  void initState() {
-    super.initState();
-    _tabController = TabController(vsync: this, length: 2)
-      ..addListener(() {
-        ref.read(selectedIndexProvider.notifier).state = _tabController.index;
-      });
-  }
-
-  @override
-  void dispose() {
-    _tabController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final selectedIndex = ref.watch(selectedIndexProvider);
     final state = ref.watch(myPlanPageNotifierProvider);
     final i18n = Translations.of(context);
     final titlei18n = i18n.myPlanPage.title;
     final tabi18n = i18n.myPlanPage.tabs;
 
-    _tabController.index = selectedIndex;
-
     return state.when(
       data: (value) {
         return DefaultTabController(
           length: 2,
-          child: Scaffold(
-            appBar: AppBar(
-              title: Text(
-                titlei18n,
-                style: AppTextStyle.textStyle.copyWith(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
+          child: Builder(
+            builder: (context) {
+              final tabController = DefaultTabController.of(context)
+                ..index = selectedIndex;
+              tabController.addListener(() {
+                ref.read(selectedIndexProvider.notifier).state =
+                    tabController.index;
+              });
+
+              return Scaffold(
+                appBar: AppBar(
+                  title: Text(
+                    titlei18n,
+                    style: AppTextStyle.textStyle.copyWith(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  bottom: TabBar(
+                    controller: tabController,
+                    onTap: (index) {
+                      ref.read(selectedIndexProvider.notifier).state = index;
+                    },
+                    indicatorWeight: 4,
+                    labelStyle: AppTextStyle.textStyle
+                        .copyWith(fontSize: 14, fontWeight: FontWeight.bold),
+                    unselectedLabelStyle: AppTextStyle.textStyle
+                        .copyWith(fontSize: 14, fontWeight: FontWeight.bold),
+                    labelColor: AppColor.black,
+                    unselectedLabelColor: AppColor.black,
+                    indicatorColor: AppColor.blue800Secondary,
+                    dividerColor: AppColor.blue900Tertiary,
+                    tabs: <Widget>[
+                      Tab(text: tabi18n.createdPlans),
+                      Tab(text: tabi18n.bookmark),
+                    ],
+                  ),
                 ),
-              ),
-              bottom: TabBar(
-                controller: _tabController,
-                onTap: (index) {
-                  ref.read(selectedIndexProvider.notifier).state = index;
-                },
-                indicatorWeight: 4,
-                labelStyle: AppTextStyle.textStyle
-                    .copyWith(fontSize: 14, fontWeight: FontWeight.bold),
-                unselectedLabelStyle: AppTextStyle.textStyle
-                    .copyWith(fontSize: 14, fontWeight: FontWeight.bold),
-                labelColor: AppColor.black,
-                unselectedLabelColor: AppColor.black,
-                indicatorColor: AppColor.blue800Secondary,
-                dividerColor: AppColor.blue900Tertiary,
-                tabs: <Widget>[
-                  Tab(text: tabi18n.createdPlans),
-                  Tab(text: tabi18n.bookmark),
-                ],
-              ),
-            ),
-            body: TabBarView(
-              controller: _tabController,
-              children: [
-                CreatedPlanTabView(plans: value.createPlanList),
-                BookmarkPlansTabView(plans: value.bookmarkPlanList),
-              ],
-            ),
+                body: TabBarView(
+                  controller: tabController,
+                  children: [
+                    CreatedPlanTabView(plans: value.createPlanList),
+                    BookmarkPlansTabView(plans: value.bookmarkPlanList),
+                  ],
+                ),
+              );
+            },
           ),
         );
       },
