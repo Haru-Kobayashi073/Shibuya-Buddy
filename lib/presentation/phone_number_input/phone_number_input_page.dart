14494bf60640c30c5b0d2ca0e60e1b4fb53e871e
shi0n0 <shiono.yui1080@gmail.com>
2025-01-25 23:09:48 +0900

    電話番号入力ページを作成

---
+++ lib/presentation/phone_number_input/phone_number_input_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PhoneNumberInputPage extends StatelessWidget {
+  const PhoneNumberInputPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
