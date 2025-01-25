e224f834047a593d7baa88cda0a165748a72a71d
shi0n0 <shiono.yui1080@gmail.com>
2025-01-25 15:26:09 +0900

    AuthenticationRepositoryにSMS関連のメソッドを追加

--- lib/domain/repositories/authentication_repository.dart
+++ lib/domain/repositories/authentication_repository.dart
@@ -13,6 +13,8 @@ abstract interface class AuthenticationRepository {
   Future<void> signInWithApple();
   Future<void> sendPasswordResetEmail(String email);
   Future<bool> isEmailVerified();
+  Future<void> linkPhoneNumber(String verificationId, String smsCode);
+  Future<bool> isSmsVerified();
   Future<void> unlink(SocialAuthDomain domain);
   Future<void> linkWithCredential(AuthCredential credential);
   Future<void> signOut();
