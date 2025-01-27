3186ec43daf920c4995e4aadf2e5cf640687d8e2
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2025-01-28 00:07:32 +0900

    不必要のコメントを削除

--- lib/domain/repositories/purchase_repository.dart
+++ lib/domain/repositories/purchase_repository.dart
@@ -1,6 +1,5 @@
 import '../entities/purchase_recipt.dart';
 
-// ignore: one_member_abstracts For alert suppression
 abstract interface class PurchaseRepository {
   Future<void> createPurchaseRecipt({required PurchaseRecipt purchaseRecipt});
   Future<void> updatePurchaseInformation({
