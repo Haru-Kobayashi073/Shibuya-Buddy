e5ac92eccca0d91fcc447ce7c3dc0f8465e36a34
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2025-01-28 00:59:33 +0900

    ファイル名の修正

--- lib/utils/routes/app_router.dart
+++ lib/utils/routes/app_router.dart
@@ -15,7 +15,7 @@ import '../../presentation/about_app/about_app_page.dart';
 import '../../presentation/about_dev/about_dev_page.dart';
 import '../../presentation/account/account_page.dart';
 import '../../presentation/ai_functions_detail/ai_functions_detail_page.dart';
-import '../../presentation/bill_details/bill_details_page.dart';
+import '../../presentation/bill_details/bill_detail_page.dart';
 import '../../presentation/buddy_chat/buddy_chat_page.dart';
 import '../../presentation/change_language/change_language_page.dart';
 import '../../presentation/change_theme/change_theme_page.dart';
