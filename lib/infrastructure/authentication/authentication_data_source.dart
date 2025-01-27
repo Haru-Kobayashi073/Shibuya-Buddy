596a749a947aeea13a8cb1062b10150656b19eff
shi0n0 <shiono.yui1080@gmail.com>
2025-01-27 22:38:34 +0900

    make format

--- lib/infrastructure/authentication/authentication_data_source.dart
+++ lib/infrastructure/authentication/authentication_data_source.dart
@@ -88,27 +88,24 @@ class AuthenticationDataSource extends _$AuthenticationDataSource
     }
   }
 
-@override
-Future<void> sendSmsCode({
-  required String phoneNumber,
-  required void Function(String verificationId) onCodeSent,
-  required void Function(FirebaseAuthException error) onError,
-}) async {
-
-  await firebaseAuth.verifyPhoneNumber(
-    phoneNumber: phoneNumber,
-    verificationCompleted: (PhoneAuthCredential credential) async {
-    },
-    verificationFailed: onError,
-    codeSent: (String verificationId, int? resendToken) {
-      onCodeSent(verificationId);
-    },
-    codeAutoRetrievalTimeout: (String verificationId) {
-      onCodeSent(verificationId);
-    },
-  );
-}
-
+  @override
+  Future<void> sendSmsCode({
+    required String phoneNumber,
+    required void Function(String verificationId) onCodeSent,
+    required void Function(FirebaseAuthException error) onError,
+  }) async {
+    await firebaseAuth.verifyPhoneNumber(
+      phoneNumber: phoneNumber,
+      verificationCompleted: (PhoneAuthCredential credential) async {},
+      verificationFailed: onError,
+      codeSent: (String verificationId, int? resendToken) {
+        onCodeSent(verificationId);
+      },
+      codeAutoRetrievalTimeout: (String verificationId) {
+        onCodeSent(verificationId);
+      },
+    );
+  }
 
   @override
   Future<void> linkPhoneNumber(String verificationId, String smsCode) async {
--- lib/presentation/sms_verification/sms_verification_state.dart
+++ lib/presentation/sms_verification/sms_verification_state.dart
@@ -10,8 +10,8 @@ class SmsVerificationState with _$SmsVerificationState {
     @Default('') String verificationId, // 現在の認証ID
     @Default('') String phoneNumber, // 電話番号
     @Default(false) bool isSmsVerified, // SMS認証が成功したかどうか
-    @Default(SmsVerificationButtonState.initialize) 
-        SmsVerificationButtonState buttonState, // ボタンの状態
+    @Default(SmsVerificationButtonState.initialize)
+    SmsVerificationButtonState buttonState, // ボタンの状態
     @Default(60) int resendCooldown, // 再送信のクールダウン残り時間
     String? errorMessage, // エラーメッセージ（表示用）
   }) = _SmsVerificationState;
