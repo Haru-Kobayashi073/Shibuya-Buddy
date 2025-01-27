cc5d04d5c765df794df2e78a60ea388df92ce9db
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2025-01-28 04:15:16 +0900

    スクロールできるよう修正

--- lib/presentation/phone_number_input/phone_number_input_page.dart
+++ lib/presentation/phone_number_input/phone_number_input_page.dart
@@ -30,101 +30,103 @@ class PhoneNumberInputPage extends HookConsumerWidget {
         forceMaterialTransparency: true,
         elevation: 0,
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 32),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Gap(context.deviceHeight * 0.05),
-            Text(
-              i18nPhoneNumberInputPage.title,
-              style: AppTextStyle.textStyle.copyWith(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const Gap(16),
-            RichText(
-              text: TextSpan(
-                style: AppTextStyle.textStyle.copyWith(
-                  fontSize: 16,
-                ),
-                children: [
-                  TextSpan(
-                    text: i18nPhoneNumberInputPage.discription.receive,
-                  ),
-                  TextSpan(
-                    text: i18nPhoneNumberInputPage
-                        .discription.internationalFormat,
-                    style: const TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  TextSpan(text: i18nPhoneNumberInputPage.discription.input),
-                ],
-              ),
-            ),
-            const Gap(8),
-            // 日本+81が選択されていた場合のみ注釈を出す
-            if (selectedCountryCode.value == 'JP') ...[
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 32),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Gap(context.deviceHeight * 0.05),
               Text(
-                '例：日本の電話番号「090-1234-5678」の場合、'
-                '国番号「+81」を付けて「+81 90-1234-5678」と入力してください。',
+                i18nPhoneNumberInputPage.title,
                 style: AppTextStyle.textStyle.copyWith(
-                  fontSize: 16,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const Gap(16),
+              RichText(
+                text: TextSpan(
+                  style: AppTextStyle.textStyle.copyWith(
+                    fontSize: 16,
+                  ),
+                  children: [
+                    TextSpan(
+                      text: i18nPhoneNumberInputPage.discription.receive,
+                    ),
+                    TextSpan(
+                      text: i18nPhoneNumberInputPage
+                          .discription.internationalFormat,
+                      style: const TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    TextSpan(text: i18nPhoneNumberInputPage.discription.input),
+                  ],
                 ),
               ),
               const Gap(8),
-            ],
-            const Gap(32),
-            IntlPhoneField(
-              controller: phoneNumberController,
-              cursorColor: AppColor.blue800Secondary,
-              decoration: InputDecoration(
-                labelText: i18nPhoneNumberInputPage.phoneNumber,
-                labelStyle: AppTextStyle.textStyle.copyWith(
-                  color: AppColor.blue900Tertiary,
-                ),
-                border: const OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: AppColor.blue800Secondary,
+              // 日本+81が選択されていた場合のみ注釈を出す
+              if (selectedCountryCode.value == 'JP') ...[
+                Text(
+                  '例：日本の電話番号「090-1234-5678」の場合、'
+                  '国番号「+81」を付けて「+81 90-1234-5678」と入力してください。',
+                  style: AppTextStyle.textStyle.copyWith(
+                    fontSize: 16,
                   ),
                 ),
-                enabledBorder: const OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: AppColor.blue800Secondary,
+                const Gap(8),
+              ],
+              const Gap(32),
+              IntlPhoneField(
+                controller: phoneNumberController,
+                cursorColor: AppColor.blue800Secondary,
+                decoration: InputDecoration(
+                  labelText: i18nPhoneNumberInputPage.phoneNumber,
+                  labelStyle: AppTextStyle.textStyle.copyWith(
+                    color: AppColor.blue900Tertiary,
+                  ),
+                  border: const OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: AppColor.blue800Secondary,
+                    ),
+                  ),
+                  enabledBorder: const OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: AppColor.blue800Secondary,
+                    ),
+                  ),
+                  focusedBorder: const OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: AppColor.blue800Secondary,
+                      width: 2,
+                    ),
                   ),
                 ),
-                focusedBorder: const OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: AppColor.blue800Secondary,
-                    width: 2,
-                  ),
-                ),
+                initialCountryCode: 'US',
+                onChanged: (phone) {
+                  completePhoneNumber.value = phone.completeNumber;
+                },
+                onCountryChanged: (country) {
+                  selectedCountryCode.value = country.code;
+                },
               ),
-              initialCountryCode: 'US',
-              onChanged: (phone) {
-                completePhoneNumber.value = phone.completeNumber;
-              },
-              onCountryChanged: (country) {
-                selectedCountryCode.value = country.code;
-              },
-            ),
-            const Gap(32),
-            WideButton(
-              label: i18nPhoneNumberInputPage.sendSmsCode,
-              color: AppColor.yellow600Primary,
-              onPressed: () async {
-                await notifier.sendSmsCode(
-                  completePhoneNumber.value,
-                  (verificationId) async {
-                    await SMSVerificationPageRouteData(
-                      phoneNumber: completePhoneNumber.value,
-                      verificationId: verificationId,
-                    ).push<void>(context);
-                  },
-                );
-              },
-            ),
-          ],
+              const Gap(32),
+              WideButton(
+                label: i18nPhoneNumberInputPage.sendSmsCode,
+                color: AppColor.yellow600Primary,
+                onPressed: () async {
+                  await notifier.sendSmsCode(
+                    completePhoneNumber.value,
+                    (verificationId) async {
+                      await SMSVerificationPageRouteData(
+                        phoneNumber: completePhoneNumber.value,
+                        verificationId: verificationId,
+                      ).push<void>(context);
+                    },
+                  );
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
