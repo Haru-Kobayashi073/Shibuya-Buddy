6f6b7c854ddfd3c1d18deada4fff790ff0debe7c
shi0n0 <shiono.yui1080@gmail.com>
2025-01-29 02:05:13 +0900

    生成差分

--- lib/i18n/strings.g.dart
+++ lib/i18n/strings.g.dart
@@ -6,7 +6,7 @@
 /// Locales: 5
 /// Strings: 1375 (275 per locale)
 ///
-/// Built on 2025-01-27 at 18:08 UTC
+/// Built on 2025-01-28 at 17:04 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint, unused_import
