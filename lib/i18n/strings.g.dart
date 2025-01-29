90dca4026ec8726ce529d94d1837ce2422c0db1a
ryunosuke_sg4i <azatose8684@gmail.com>
2025-01-29 14:27:44 +0900

    bookmarkのmessageに改行を追加

--- lib/i18n/strings.g.dart
+++ lib/i18n/strings.g.dart
@@ -6,7 +6,7 @@
 /// Locales: 5
 /// Strings: 1375 (275 per locale)
 ///
-/// Built on 2025-01-28 at 13:43 UTC
+/// Built on 2025-01-29 at 05:14 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint, unused_import
