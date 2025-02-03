70d0b2a14c15e55ca13eb3622e9356d5ca555d58
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2025-02-04 04:46:12 +0900

    言語をキーとしたトピックのvalueを持つマップをフィールドとして追加

--- lib/domain/entities/topic.g.dart
+++ lib/domain/entities/topic.g.dart
@@ -8,7 +8,10 @@ part of 'topic.dart';
 
 _$TopicImpl _$$TopicImplFromJson(Map<String, dynamic> json) => _$TopicImpl(
       id: json['id'] as String? ?? '',
-      name: json['name'] as String,
+      translatedNameMap:
+          (json['translated_name_map'] as Map<String, dynamic>).map(
+        (k, e) => MapEntry($enumDecode(_$AppLocaleEnumMap, k), e as String),
+      ),
       thumbnailUrl: json['thumbnail_url'] as String,
       totalCount: (json['total_count'] as num?)?.toInt() ?? 0,
       relatedPlanIds: (json['related_plan_ids'] as List<dynamic>?)
@@ -22,10 +25,19 @@ _$TopicImpl _$$TopicImplFromJson(Map<String, dynamic> json) => _$TopicImpl(
 Map<String, dynamic> _$$TopicImplToJson(_$TopicImpl instance) =>
     <String, dynamic>{
       'id': instance.id,
-      'name': instance.name,
+      'translated_name_map': instance.translatedNameMap
+          .map((k, e) => MapEntry(_$AppLocaleEnumMap[k]!, e)),
       'thumbnail_url': instance.thumbnailUrl,
       'total_count': instance.totalCount,
       'related_plan_ids': instance.relatedPlanIds,
       'ranking': instance.ranking,
       'created_at': instance.createdAt,
     };
+
+const _$AppLocaleEnumMap = {
+  AppLocale.en: 'en',
+  AppLocale.ja: 'ja',
+  AppLocale.ko: 'ko',
+  AppLocale.zhHans: 'zhHans',
+  AppLocale.zhHant: 'zhHant',
+};
