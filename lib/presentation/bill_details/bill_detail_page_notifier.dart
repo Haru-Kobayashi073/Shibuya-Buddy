36525d816864ce990b78fca5cd94392d94632c73
shi0n0 <shiono.yui1080@gmail.com>
2025-02-05 01:31:53 +0900

    launchNotion関数を追加

--- lib/presentation/bill_details/bill_detail_page_notifier.dart
+++ lib/presentation/bill_details/bill_detail_page_notifier.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../../i18n/strings.g.dart';
 import '../../utils/providers/in_app_purchase/in_app_purchase_service.dart';
@@ -97,6 +98,17 @@ class BillDetailPageNotifier extends _$BillDetailPageNotifier {
       return PurchaseItemConfig.unlimitedPremium.packageId;
     } else {
       return '';
+    }
+  }
+
+  Future<void> launchNotion() async {
+    final url = Uri.parse(
+      'https://pond-link-c69.notion.site/1904d7d3b38980f0a608e48ccd13dce4?pvs=4',
+    );
+    if (await canLaunchUrl(url)) {
+      await launchUrl(url, mode: LaunchMode.externalApplication);
+    } else {
+      debugPrint('Could not launch $url');
     }
   }
 }
