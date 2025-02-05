e237108be235608d0cea6ad7f79853699f39612b
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2025-02-05 14:55:02 +0900

    カスタムのLoggerを定義

---
+++ lib/utils/custom_logger.dart
@@ -0,0 +1,39 @@
+import 'dart:developer' as dev;
+
+import 'package:flutter/foundation.dart';
+import 'package:logger/logger.dart';
+
+final logger = CustomLogger();
+
+/// ログ出力をカスタマイズします。
+///
+/// t,dはconsoleに出力します。
+/// i,wはFBAにログを送信します。
+/// (wは集計して、どのユーザが遭遇しているかをみたい。)
+/// e,fはFirebaseCrashlyticsにログを送信します。
+class CustomLogger extends Logger {
+  CustomLogger()
+      : super(
+          output: CustomOutput(),
+          printer: PrettyPrinter(
+            methodCount: 1, // 表示されるコールスタックの数
+            errorMethodCount: 5, // 表示されるスタックトレースのコールスタックの数
+            lineLength: 80, // 区切りラインの長さ
+            dateTimeFormat: DateTimeFormat.dateAndTime,
+          ),
+          level: kReleaseMode ? Level.info : Level.trace,
+        );
+}
+
+/// log出力をカスタマイズします。
+///
+/// デフォルトではiOSにて色が出力されないため、修正します。
+class CustomOutput extends LogOutput {
+  @override
+  void output(OutputEvent event) {
+    // log出します。
+    for (final e in event.lines) {
+      dev.log(e, name: 'logger');
+    }
+  }
+}
