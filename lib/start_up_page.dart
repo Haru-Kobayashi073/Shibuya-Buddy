fe609cd4c7b2792c01f27e10158929e5397344ba
Tomoya <92212932+PenginPenPen@users.noreply.github.com>
2025-02-06 23:16:55 +0900

    不要なコードを削除

--- lib/start_up_page.dart
+++ lib/start_up_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:logger/web.dart';
 
 import 'i18n/strings.g.dart';
 import 'infrastructure/firebase/firebase_auth_state_listener.dart';
