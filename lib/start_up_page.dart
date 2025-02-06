0167d048e36775638997ae7caa358201040d3475
Tomoya <92212932+PenginPenPen@users.noreply.github.com>
2025-02-06 19:19:48 +0900

    電話番号がブランクだった時の処理を追加

--- lib/start_up_page.dart
+++ lib/start_up_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:logger/web.dart';
 
 import 'i18n/strings.g.dart';
 import 'infrastructure/firebase/firebase_auth_state_listener.dart';
@@ -17,11 +18,15 @@ class StartUpPage extends HookConsumerWidget {
     final message = ref.watch(scaffoldMessengerProvider.notifier);
     final i18nEmail = t.authentication.emailVerificationPage.snackBar;
     final i18nPhone = t.authentication.phoneNumberInputPage.scaffoldMessenger;
-
+    final logger = Logger();
     WidgetsBinding.instance.addPostFrameCallback((_) {
       user.when(
         data: (user) {
           if (user != null) {
+            logger
+              ..i('電話番号:${user.phoneNumber == null}')
+              ..i('メール認証:${user.emailVerified}')
+              ..i('名前:${user.uid}');
             if (!user.emailVerified) {
               context.go(
                 EmailVerificationPageRouteData(email: user.email.toString())
@@ -30,7 +35,7 @@ class StartUpPage extends HookConsumerWidget {
               WidgetsBinding.instance.addPostFrameCallback((_) {
                 message.showExceptionSnackBar(i18nEmail.emailVerification);
               });
-            } else if (user.phoneNumber == null) {
+            } else if (user.phoneNumber == null || user.phoneNumber == '') {
               context.go(const PhoneNumberInputPageRouteData().location);
               WidgetsBinding.instance.addPostFrameCallback((_) {
                 message
