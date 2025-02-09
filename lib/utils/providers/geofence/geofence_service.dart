660d7a0b110b21f23042fdd3e74d0581e6b31f69
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2025-02-10 05:09:46 +0900

    GeofenceServiceクラスの実装

---
+++ lib/utils/providers/geofence/geofence_service.dart
@@ -0,0 +1,69 @@
+import 'package:native_geofence/native_geofence.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import '../../../main.dart';
+import '../../custom_logger.dart';
+
+part 'geofence_service.g.dart';
+
+@riverpod
+class GeofenceService extends _$GeofenceService {
+  NativeGeofenceManager get nativeGeofenceManager =>
+      NativeGeofenceManager.instance;
+  @override
+  void build() {
+    return;
+  }
+
+  Future<void> initialize() async {
+    await nativeGeofenceManager.initialize();
+    await checkPermissionAboutLocation();
+    logger.d('initializeAppServices: geofenceService initialized');
+    await getRegisteredGenfences();
+  }
+
+  Future<void> addGeofence({
+    required Location location,
+    required String id,
+  }) async {
+    final zone = Geofence(
+      id: id,
+      location: location,
+      radiusMeters: 50,
+      triggers: {
+        GeofenceEvent.enter,
+      },
+      iosSettings: const IosGeofenceSettings(
+        initialTrigger: true,
+      ),
+      androidSettings: const AndroidGeofenceSettings(
+        initialTriggers: {GeofenceEvent.enter},
+        expiration: Duration(days: 7),
+        notificationResponsiveness: Duration(minutes: 5),
+      ),
+    );
+
+    if (await checkPermissionAboutLocation()) {
+      await nativeGeofenceManager.createGeofence(zone, geofenceTriggered);
+    }
+  }
+
+  Future<void> deleteGeofence({required String id}) async {
+    await nativeGeofenceManager.removeGeofenceById(id);
+  }
+
+  Future<bool> checkPermissionAboutLocation() async {
+    final locationPerm = await Permission.locationWhenInUse.request();
+    final backgroundLocationPerm = await Permission.locationAlways.request();
+    final notificationPerm = await Permission.notification.request();
+    return locationPerm.isGranted &&
+        backgroundLocationPerm.isGranted &&
+        notificationPerm.isGranted;
+  }
+
+  Future<void> getRegisteredGenfences() async {
+    final geofences = await nativeGeofenceManager.getRegisteredGeofences();
+    logger.d('geofences: $geofences');
+  }
+}
