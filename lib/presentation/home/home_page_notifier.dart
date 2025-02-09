db1bc2b217c45f50a0ec32be7f49d5328fe06c16
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2025-02-10 05:14:31 +0900

    サービス類の初期化をメソッド分け

--- lib/presentation/home/home_page_notifier.dart
+++ lib/presentation/home/home_page_notifier.dart
@@ -1,3 +1,6 @@
+import 'dart:isolate';
+import 'dart:ui';
+
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 import '../../domain/entities/plan.dart';
@@ -8,6 +11,7 @@ import '../../infrastructure/topic/topic_data_source.dart';
 import '../../utils/billing_grade_options.dart';
 import '../../utils/custom_logger.dart';
 import '../../utils/providers/current_user/current_user.dart';
+import '../../utils/providers/geofence/geofence_service.dart';
 import '../../utils/providers/in_app_purchase/in_app_purchase_service.dart';
 import 'home_page_state.dart';
 
@@ -22,11 +26,8 @@ class HomePageNotifier extends _$HomePageNotifier {
 
   @override
   Future<HomePageState> build() async {
-    const flavor = String.fromEnvironment('flavor');
-    await ref.read(currentUserProvider.notifier).fetchUser();
-    if (flavor == 'prod') {
-      await ref.read(inAppPurchaseServiceProvider.notifier).build();
-    }
+    await initializeAppServices();
+
     final popularPlans = await getPopularPlans();
     final popularTopics = await getPopularTopics();
     final recentPlans = await getRecentPlans();
@@ -36,6 +37,37 @@ class HomePageNotifier extends _$HomePageNotifier {
       popularTopics: popularTopics,
       recentPlans: recentPlans ?? <Plan>[],
     );
+  }
+
+  Future<void> initializeAppServices() async {
+    try {
+      /// ユーザー情報を取得
+      await ref.read(currentUserProvider.notifier).fetchUser();
+
+      /// Geofencingを初期化
+      final port = ReceivePort();
+      IsolateNameServer.registerPortWithName(
+        port.sendPort,
+        'native_geofence_send_port',
+      );
+      port.listen((dynamic data) async {
+        logger.d('geofenceState: $data');
+        for (final id in data as List<String>) {
+          await ref
+              .read(geofenceServiceProvider.notifier)
+              .deleteGeofence(id: id);
+        }
+      });
+      await ref.read(geofenceServiceProvider.notifier).initialize();
+
+      /// In-App Purchaseを初期化
+      const flavor = String.fromEnvironment('flavor');
+      if (flavor == 'prod') {
+        await ref.read(inAppPurchaseServiceProvider.notifier).build();
+      }
+    } on Exception catch (e) {
+      logger.e('initializeAppServices: $e');
+    }
   }
 
   Future<List<Plan>> getPopularPlans() async {
