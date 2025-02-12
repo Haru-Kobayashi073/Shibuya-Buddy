e3de30f2596a4b32775a671746b394061499f963
shi0n0 <shiono.yui1080@gmail.com>
2025-02-13 01:40:58 +0900

    AnalyticsNotifierの微修正

--- lib/utils/providers/analytics/analytics.dart
+++ lib/utils/providers/analytics/analytics.dart
@@ -4,20 +4,18 @@ import '../../../infrastructure/analytics/analytics_data_source.dart';
 import '../../../utils/analytics_event.dart';
 
 class AnalyticsNotifier {
-  AnalyticsNotifier(this._analyticsDataSource);
-  final FirebaseAnalyticsDataSource _analyticsDataSource;
+  AnalyticsNotifier(this._dataSource);
+  final FirebaseAnalyticsDataSource _dataSource;
 
   Future<void> logEvent(
     AnalyticsEvent event, {
     Map<String, Object>? parameters,
   }) async {
-    await _analyticsDataSource.logEvent(
-      event,
-      parameters: parameters,
-    ); 
+    await _dataSource.logEvent(event, parameters: parameters);
   }
 }
 
+// Provider: Notifier を提供
 final analyticsNotifierProvider = Provider<AnalyticsNotifier>((ref) {
   final dataSource = ref.read(firebaseAnalyticsDataSourceProvider);
   return AnalyticsNotifier(dataSource);
