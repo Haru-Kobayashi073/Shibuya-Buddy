bcdd0d7353c30c474331ea6c00be41ed647b9362
shi0n0 <shiono.yui1080@gmail.com>
2025-02-13 02:29:09 +0900

    BillDetailDialogとDeleteAccountを追加

--- lib/utils/analytics_event.dart
+++ lib/utils/analytics_event.dart
@@ -1,9 +1,10 @@
 enum AnalyticsEvent {
   planDetailPageView('plan_detail_page_view'),
   billDetailPageView('bill_detail_page_view'),
+  billDetailDialogView('bill_detail_dialog_view'),
   mapPageView('map_page_view'),
   errorView('error_view'),
-  deleteUser('delete_user');
+  deleteAccount('delete_account');
 
   const AnalyticsEvent(this.key);
   final String key;
