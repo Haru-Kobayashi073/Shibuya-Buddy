88babfc04c266a16aa3ed461acfde5099f521ddc
shi0n0 <shiono.yui1080@gmail.com>
2025-02-13 15:55:33 +0900

    イベントをScreenViewEventとUserActionEventに分けた

--- lib/utils/analytics_event.dart
+++ lib/utils/analytics_event.dart
@@ -1,13 +1,20 @@
-enum AnalyticsEvent {
+enum ScreenViewEvent {
   planDetailPageView('plan_detail_page_view'),
   billDetailPageView('bill_detail_page_view'),
   billDetailDialogView('bill_detail_dialog_view'),
   mapPageView('map_page_view'),
-  errorView('error_view'),
+  errorView('error_view');
+
+  const ScreenViewEvent(this.key);
+  final String key;
+}
+
+enum UserActionEvent {
   deleteAccount('delete_account'),
   chatLimitReached('chat_limit_reached'),
-  completeCreatePlan('complete_create_plan');
+  completeCreatePlan('complete_create_plan'),
+  error('error');
 
-  const AnalyticsEvent(this.key);
+  const UserActionEvent(this.key);
   final String key;
 }
