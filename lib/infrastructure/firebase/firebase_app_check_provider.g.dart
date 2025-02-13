f84f80c30bac8342a50be469f9a56f6c6f75834b
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2025-02-14 03:58:23 +0900

    AppCheckのインスタンスを持つProviderを実装

---
+++ lib/infrastructure/firebase/firebase_app_check_provider.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'firebase_app_check_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$firebaseAppCheckHash() => r'9a5bd98b9feefa24bd4b95196a120f8d18dc7c3c';
+
+/// See also [firebaseAppCheck].
+@ProviderFor(firebaseAppCheck)
+final firebaseAppCheckProvider = Provider<FirebaseAppCheck>.internal(
+  firebaseAppCheck,
+  name: r'firebaseAppCheckProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$firebaseAppCheckHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef FirebaseAppCheckRef = ProviderRef<FirebaseAppCheck>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
