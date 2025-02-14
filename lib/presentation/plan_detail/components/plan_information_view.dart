ad148e54479306c2638b45acab04eed9f56aec76
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2025-02-14 22:15:03 +0900

    情報タブの実装

---
+++ lib/presentation/plan_detail/components/plan_information_view.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:gap/gap.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:intl/intl.dart';
+
+import '../../../domain/entities/place.dart';
+import '../../../domain/entities/plan.dart';
+import '../../../i18n/strings.g.dart';
+import '../../../utils/extensions/context.dart';
+import '../../../utils/providers/ad_helper/ad_helper.dart';
+import '../../../utils/routes/app_router.dart';
+import '../../../utils/styles/app_color.dart';
+import '../../../utils/styles/app_text_style.dart';
+import '../../components/expandable_text.dart';
+import '../../components/place_card.dart';
+import '../../components/wide_button.dart';
+
+class PlanInformationView extends HookConsumerWidget {
+  const PlanInformationView({
+    super.key,
+    required this.plan,
+    required this.places,
+  });
+  final Plan plan;
+  final List<Place> places;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final i18n = Translations.of(context);
+    final planDetailPagei18n = i18n.planDetailsPage;
+    final ads = ref.watch(adHelperProvider);
+    final outputFormat = DateFormat(planDetailPagei18n.dateTime.dateFormat)
+        .format(DateTime.now());
+
+    useEffect(
+      () {
+        Future.microtask(
+          () async => ref.read(adHelperProvider.notifier).loadNativeAd(),
+        );
+        return null;
+      },
+      const [],
+    );
+
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          const Gap(16),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: ExpandableText(plan.description),
+          ),
+          const Gap(8),
+          Column(
+            children: [
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  planDetailPagei18n.dateTime.createOn(date: outputFormat),
+                  style: AppTextStyle.textStyle.copyWith(
+                    fontSize: 14,
+                    color: AppColor.grey600,
+                  ),
+                ),
+              ),
+              const Gap(16),
+              WideButton(
+                icon: const Icon(
+                  Icons.pin_drop_outlined,
+                  color: AppColor.black,
+                ),
+                label: planDetailPagei18n.item.viewOnMap,
+                color: AppColor.blue50Background,
+                onPressed: () async {
+                  await MapPageRouteData($extra: places).push<void>(context);
+                },
+              ),
+              const Gap(16),
+              Column(
+                children: places
+                    .map(
+                      (place) => PlaceCard(
+                        place: place,
+                        index: places.indexOf(place),
+                        endindex: places.length - 1,
+                      ),
+                    )
+                    .toList(),
+              ),
+              const Gap(32),
+              if (ads.nativeAd != null && ads.isLoadedNativeAd)
+                ConstrainedBox(
+                  constraints: BoxConstraints(
+                    minWidth: 320, // minimum recommended width
+                    minHeight: 320, // minimum recommended height
+                    maxWidth: context.deviceWidth,
+                    maxHeight: 400,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: AdWidget(ad: ads.nativeAd!),
+                  ),
+                ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
