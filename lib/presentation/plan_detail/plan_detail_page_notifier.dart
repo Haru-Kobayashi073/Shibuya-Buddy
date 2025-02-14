93df78aa809aa8d186045e082377aeb6dd88ada3
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2025-02-14 22:14:49 +0900

    広告の読み込みを削除

--- lib/presentation/plan_detail/plan_detail_page_notifier.dart
+++ lib/presentation/plan_detail/plan_detail_page_notifier.dart
@@ -7,7 +7,6 @@ import '../../i18n/strings.g.dart';
 import '../../infrastructure/place/place_data_source.dart';
 import '../../infrastructure/plan/plan_data_source.dart';
 import '../../utils/custom_logger.dart';
-import '../../utils/providers/ad_helper/ad_helper.dart';
 import '../../utils/providers/current_user/current_user.dart';
 import '../../utils/providers/scaffold_messenger/scaffold_messenger.dart';
 import '../my_plan/my_plan_page_notifier.dart';
@@ -23,14 +22,12 @@ class PlanDetailPageNotifier extends _$PlanDetailPageNotifier {
       ref.read(placeDataSourceProvider.notifier);
   ScaffoldMessenger get scaffoldMessenger =>
       ref.read(scaffoldMessengerProvider.notifier);
-  AdHelper get adHelper => ref.read(adHelperProvider.notifier);
   User get currentUser => ref.read(currentUserProvider);
 
   @override
   Future<PlanDetailPageState> build(Plan plan) async {
     final latestPlan = await getPlan();
     final places = await getPlaces();
-    await adHelper.loadNativeAd();
 
     return PlanDetailPageState(
       plan: latestPlan,
