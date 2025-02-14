f37145130a29774244da9bc5325e0848dbbe3dbb
Tomoya <92212932+PenginPenPen@users.noreply.github.com>
2025-02-14 19:19:52 +0900

    クラス名の変更

--- lib/presentation/components/create_plan_loading/create_loading_notifier.g.dart
+++ lib/presentation/components/create_plan_loading/create_loading_notifier.g.dart
@@ -6,21 +6,23 @@ part of 'create_loading_notifier.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$createLoadingViewHash() => r'e118f4fbf7e4d26ba91a1005e916dfed2f4dc71d';
+String _$createLoadingViewNotifierHash() =>
+    r'ab5b58d2a6a9af0108b1e3087d381983ce34e191';
 
-/// See also [CreateLoadingView].
-@ProviderFor(CreateLoadingView)
-final createLoadingViewProvider = AutoDisposeNotifierProvider<CreateLoadingView,
-    CreateLoadingViewState>.internal(
-  CreateLoadingView.new,
-  name: r'createLoadingViewProvider',
+/// See also [CreateLoadingViewNotifier].
+@ProviderFor(CreateLoadingViewNotifier)
+final createLoadingViewNotifierProvider = AutoDisposeNotifierProvider<
+    CreateLoadingViewNotifier, CreateLoadingViewState>.internal(
+  CreateLoadingViewNotifier.new,
+  name: r'createLoadingViewNotifierProvider',
   debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
       ? null
-      : _$createLoadingViewHash,
+      : _$createLoadingViewNotifierHash,
   dependencies: null,
   allTransitiveDependencies: null,
 );
 
-typedef _$CreateLoadingView = AutoDisposeNotifier<CreateLoadingViewState>;
+typedef _$CreateLoadingViewNotifier
+    = AutoDisposeNotifier<CreateLoadingViewState>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
