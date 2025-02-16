3cb4c0fb25eaab3cc20aaf27ebc321144952d734
shi0n0 <shiono.yui1080@gmail.com>
2025-02-17 01:17:35 +0900

    人数操作の関数を追加

--- lib/presentation/create_plan/create_plan_notifier.dart
+++ lib/presentation/create_plan/create_plan_notifier.dart
@@ -45,9 +45,7 @@ class CreatePlanNotifier extends _$CreatePlanNotifier {
   void updateSelectedTopics(Topic topic, {required bool isSelected}) {
     final updatedTopics = List<Topic>.from(state.requireValue.selectedTopics);
     if (isSelected) {
-      if (!updatedTopics.contains(topic)) {
-        updatedTopics.add(topic);
-      }
+      if (!updatedTopics.contains(topic)) updatedTopics.add(topic);
     } else {
       updatedTopics.remove(topic);
     }
@@ -65,15 +63,7 @@ class CreatePlanNotifier extends _$CreatePlanNotifier {
           'zh-Hans': 'M月d日 EEEE hh:mm a',
         }[currentLocale] ??
         'MMM d, EEEE HH:mm';
-
-    final formatter = DateFormat(pattern, currentLocale);
-    return formatter.format(date);
-  }
-
-  void updateNumberOfPeople(String selecteNum) {
-    state = AsyncValue.data(
-      state.requireValue.copyWith(numberOfPeople: selecteNum),
-    );
+    return DateFormat(pattern, currentLocale).format(date);
   }
 
   void updateTransport(String selectedTransport) {
@@ -111,19 +101,15 @@ class CreatePlanNotifier extends _$CreatePlanNotifier {
     required bool isStartDate,
   }) async {
     var chosenDate = DateTime.now();
-
     await showCupertinoModalPopup<void>(
       context: context,
-      builder: (context) {
-        return CustomCupertinoDatePicker(
-          onDateTimeChanged: (date) {
-            chosenDate = date;
-            targetController.text = _formatDate(date);
-          },
-        );
-      },
+      builder: (context) => CustomCupertinoDatePicker(
+        onDateTimeChanged: (date) {
+          chosenDate = date;
+          targetController.text = _formatDate(date);
+        },
+      ),
     );
-
     if (isStartDate) {
       state = AsyncValue.data(
         state.requireValue.copyWith(startDate: _formatDate(chosenDate)),
@@ -144,24 +130,22 @@ class CreatePlanNotifier extends _$CreatePlanNotifier {
       );
       return;
     }
-
     final startDate = _parseDate(state.requireValue.startDate!);
     final endDate = _parseDate(state.requireValue.endDate!);
-
     if (startDate.isAfter(endDate)) {
       scaffoldMessenger.showExceptionSnackBar(
         t.createPlanPage.snackBar.error.invalidDateRange,
       );
       return;
     }
-
     final planPrompt = PlanPrompt(
       id: const Uuid().v4(),
       schedules: (
         firstDate: state.requireValue.startDate!,
-        lastDate: state.requireValue.endDate!,
+        lastDate: state.requireValue.endDate!
       ),
-      numberOfPeople: state.requireValue.numberOfPeople,
+      numberOfPeople:
+          '${state.requireValue.adultCount + state.requireValue.childCount}',
       transports: state.requireValue.transports,
       categories: state.requireValue.categories,
       topics: state.requireValue.selectedTopics,
@@ -180,7 +164,42 @@ class CreatePlanNotifier extends _$CreatePlanNotifier {
         }[currentLocale] ??
         'MMM d, EEEE HH:mm';
 
-    final formatter = DateFormat(pattern, currentLocale);
-    return formatter.parse(dateString);
+    return DateFormat(pattern, currentLocale).parse(dateString);
+  }
+
+  // 大人の人数操作
+  void incrementAdult() {
+    final newAdultCount = state.requireValue.adultCount + 1;
+    state =
+        AsyncValue.data(state.requireValue.copyWith(adultCount: newAdultCount));
+    print('大人の人数: $newAdultCount');
+  }
+
+  void decrementAdult() {
+    if (state.requireValue.adultCount > 0) {
+      final newAdultCount = state.requireValue.adultCount - 1;
+      state = AsyncValue.data(
+        state.requireValue.copyWith(adultCount: newAdultCount),
+      );
+      print('大人の人数: $newAdultCount');
+    }
+  }
+
+  // 子供の人数操作
+  void incrementChild() {
+    final newChildCount = state.requireValue.childCount + 1;
+    state =
+        AsyncValue.data(state.requireValue.copyWith(childCount: newChildCount));
+    print('子供の人数: $newChildCount');
+  }
+
+  void decrementChild() {
+    if (state.requireValue.childCount > 0) {
+      final newChildCount = state.requireValue.childCount - 1;
+      state = AsyncValue.data(
+        state.requireValue.copyWith(childCount: newChildCount),
+      );
+      print('子供の人数: $newChildCount');
+    }
   }
 }
