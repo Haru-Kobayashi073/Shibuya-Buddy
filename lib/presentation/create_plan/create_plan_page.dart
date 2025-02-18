cb9c9b6b4c1d871ae04e55eafe32c1ed53320c32
shi0n0 <shiono.yui1080@gmail.com>
2025-02-19 00:32:20 +0900

    各コンポーネントの更新を全て適応

--- lib/presentation/create_plan/create_plan_page.dart
+++ lib/presentation/create_plan/create_plan_page.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:gap/gap.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:material_symbols_icons/material_symbols_icons.dart';
 
 import '../../i18n/strings.g.dart';
 import '../../utils/providers/locale/locale_service.dart';
@@ -15,10 +13,11 @@ import '../components/confirm_dialog.dart';
 import '../components/error_view.dart';
 import '../components/loading_overlay.dart';
 import '../components/wide_button.dart';
+import './components/trasnport_selecter.dart';
+import 'components/number_of_people_selecter.dart';
 import 'components/plan_text_field.dart';
-import 'components/selection_modal.dart';
+import 'components/schedule_selecter.dart';
 import 'components/topic_chip_field.dart';
-import 'components/topic_text_field.dart';
 import 'create_plan_notifier.dart';
 
 class CreatePlanPage extends HookConsumerWidget {
@@ -26,30 +25,20 @@ class CreatePlanPage extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final planState = ref.watch(createPlanNotifierProvider);
+    final planStateAsync = ref.watch(createPlanNotifierProvider);
     final planNotifier = ref.watch(createPlanNotifierProvider.notifier);
+    final localeNotifier = ref.read(localeServiceProvider.notifier);
 
+    // 各種コントローラーの生成
     final locationController =
         useTextEditingController(text: t.createPlanPage.hintText.location);
     final startDateController = useTextEditingController();
     final endDateController = useTextEditingController();
-    final numberOfPeopleController = useTextEditingController();
-    final transportController = useTextEditingController();
-    final categoryController = useTextEditingController();
-    final topicsController = useTextEditingController();
-    final localeNotifier = ref.read(localeServiceProvider.notifier);
 
-    return planState.when(
-      data: (value) {
-        final planState = value;
+    return planStateAsync.when(
+      data: (planState) {
         useEffect(
           () {
-            topicsController.text = planState.selectedTopics
-                .map(localeNotifier.getTranslatedTopicName)
-                .join(', ');
-            numberOfPeopleController.text = planState.numberOfPeople;
-            transportController.text = planState.transports.join(', ');
-            categoryController.text = planState.categories.join(', ');
             startDateController.text = planState.startDate ?? '';
             endDateController.text = planState.endDate ?? '';
             return null;
@@ -85,28 +74,41 @@ class CreatePlanPage extends HookConsumerWidget {
               padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
               child: SingleChildScrollView(
                 child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
+                    // 場所
+                    Text(
+                      t.createPlanPage.label.location,
+                      style: AppTextStyle.textStyle.copyWith(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const Gap(8),
                     PlanTextField(
-                      label: t.createPlanPage.label.location,
-                      prefixIcon: const Icon(Symbols.location_on),
+                      label: '',
+                      prefixIcon: const Icon(Icons.location_on),
                       controller: locationController,
                     ),
+                    //場所終わり
                     const Gap(16),
+                    // 日程入力のタイトル（共通：スケジュール）
+                    Text(
+                      'スケジュール',
+                      style: AppTextStyle.textStyle.copyWith(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const Gap(8),
+                    // 日程入力（開始日／終了日）
                     Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Expanded(
-                          child: PlanTextField(
-                            prefixIcon: const Icon(Symbols.calendar_month),
-                            label: t.createPlanPage.label.scheduleStart,
-                            controller: startDateController,
-                            onTap: () async {
-                              await planNotifier.showCupertinoDatePicker(
-                                context,
-                                startDateController,
-                                isStartDate: true,
-                              );
-                            },
-                          ),
+                        ScheduleSelecter(
+                          dateController: startDateController,
+                          planNotifier: planNotifier,
+                          isStartDate: true,
                         ),
                         const Gap(8),
                         Text(
@@ -117,100 +119,58 @@ class CreatePlanPage extends HookConsumerWidget {
                           ),
                         ),
                         const Gap(8),
-                        Expanded(
-                          child: PlanTextField(
-                            label: t.createPlanPage.label.scheduleEnd,
-                            controller: endDateController,
-                            prefixIcon: const Icon(Symbols.calendar_month),
-                            onTap: () async {
-                              await planNotifier.showCupertinoDatePicker(
-                                context,
-                                endDateController,
-                                isStartDate: false,
-                              );
-                            },
-                          ),
+                        ScheduleSelecter(
+                          dateController: endDateController,
+                          planNotifier: planNotifier,
+                          isStartDate: false,
                         ),
                       ],
                     ),
                     const Gap(16),
-                    PlanTextField(
-                      label: t.createPlanPage.label.numberOfPeople,
-                      prefixIcon: const Icon(Symbols.supervisor_account),
-                      controller: numberOfPeopleController,
-                      keyboardType: TextInputType.none,
-                      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                      onTap: () async {
-                        final selectedValue =
-                            await showModalBottomSheet<String>(
-                          context: context,
-                          builder: (_) {
-                            return SizedBox(
-                              height: 300,
-                              child: SelectionModal(
-                                selectionField: SelectionField.numberOfPeople,
-                                title: t.createPlanPage.label.numberOfPeople,
-                                onTapCheckBox:
-                                    planNotifier.updateNumberOfPeople,
-                              ),
-                            );
-                          },
-                        );
-                        if (selectedValue != null) {
-                          numberOfPeopleController.text = selectedValue;
-                        }
-                      },
+                    // 日程入力おわり
+
+                    // 人数選択
+                    Text(
+                      t.createPlanPage.label.numberOfPeople,
+                      style: AppTextStyle.textStyle.copyWith(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const Gap(8),
+                    NumberOfPeopleSelecter(
+                      adultCount: planState.adultCount,
+                      childCount: planState.childCount,
+                      onIncrementAdult: planNotifier.incrementAdult,
+                      onDecrementAdult: planNotifier.decrementAdult,
+                      onIncrementChild: planNotifier.incrementChild,
+                      onDecrementChild: planNotifier.decrementChild,
                     ),
                     const Gap(16),
-                    PlanTextField(
-                      label: t.createPlanPage.label.transport,
-                      prefixIcon: const Icon(Icons.commute),
-                      controller: transportController,
-                      onTap: () async {
-                        await showModalBottomSheet<void>(
-                          context: context,
-                          builder: (_) {
-                            return SizedBox(
-                              height: 300,
-                              child: SelectionModal(
-                                selectionField: SelectionField.transport,
-                                title: t.createPlanPage.label.transport,
-                                onTapCheckBox: planNotifier.updateTransport,
-                              ),
-                            );
-                          },
-                        );
-                      },
+
+                    // 交通手段選択
+                    Text(
+                      t.createPlanPage.label.transport,
+                      style: AppTextStyle.textStyle.copyWith(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const Gap(8),
+                    TransportSelector(
+                      transportOptions: t.createPlanPage.transportOptions,
+                      selectedTransports: planState.transports,
+                      onTransportSelected: planNotifier.updateTransport,
                     ),
                     const Gap(16),
-                    PlanTextField(
-                      label: t.createPlanPage.label.category,
-                      prefixIcon: const Icon(Symbols.category),
-                      controller: categoryController,
-                      onTap: () async {
-                        final selectedValue =
-                            await showModalBottomSheet<String>(
-                          context: context,
-                          builder: (_) {
-                            return SizedBox(
-                              height: 300,
-                              child: SelectionModal(
-                                selectionField: SelectionField.category,
-                                title: t.createPlanPage.label.category,
-                                onTapCheckBox: planNotifier.updateCategory,
-                              ),
-                            );
-                          },
-                        );
-                        if (selectedValue != null) {
-                          categoryController.text = selectedValue;
-                        }
-                      },
-                    ),
-                    const Gap(16),
-                    TopicTextField(
-                      controller: topicsController,
-                      onClear: planNotifier.clearSelectedTopics,
+
+                    // トピック選択
+                    Text(
+                      t.createPlanPage.label.topics,
+                      style: AppTextStyle.textStyle.copyWith(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                     TopicChipField(
                       topics: planState.topics,
@@ -218,6 +178,8 @@ class CreatePlanPage extends HookConsumerWidget {
                       onSelected: planNotifier.updateSelectedTopics,
                     ),
                     const Gap(16),
+
+                    // プラン送信ボタン
                     WideButton(
                       label: t.createPlanPage.submitButton,
                       color: AppColor.yellow600Primary,
