7d936195acef239f2b950641d6c708f3f8111a10
shi0n0 <shiono.yui1080@gmail.com>
2025-02-19 02:46:38 +0900

    planTextFieldを整理

--- lib/presentation/create_plan/components/plan_text_field.dart
+++ lib/presentation/create_plan/components/plan_text_field.dart
@@ -7,52 +7,56 @@ import '../../../utils/styles/app_text_style.dart';
 class PlanTextField extends StatelessWidget {
   const PlanTextField({
     super.key,
-    required this.label,
     required this.prefixIcon,
     required this.controller,
     this.keyboardType,
     this.inputFormatters,
     this.onTap,
-    this.readOnly = true,
+    this.readOnly = false,
+    this.hintText,
   });
 
-  final String label;
   final Widget prefixIcon;
   final TextEditingController controller;
   final TextInputType? keyboardType;
   final List<TextInputFormatter>? inputFormatters;
   final VoidCallback? onTap;
   final bool readOnly;
+  final String? hintText;
 
   @override
   Widget build(BuildContext context) {
+    // readOnly フラグをもとに、TextField の有効／無効を切り替える
     return TextField(
-      onTap: onTap,
-      minLines: 1,
-      maxLines: 2,
+      // readOnly でもユーザーがタップできてしまうので、
+      // 見た目上わかりやすいように enabled を制御する
       readOnly: readOnly,
+      // readOnly の場合はカーソルを非表示にする
+      showCursor: false,
+      onTap: onTap,
       controller: controller,
       keyboardType: keyboardType,
       textInputAction: TextInputAction.next,
       inputFormatters: inputFormatters,
+      minLines: 1,
+      maxLines: 2,
       decoration: InputDecoration(
-        enabledBorder: const OutlineInputBorder(
-          borderSide: BorderSide(color: AppColor.blue800Secondary),
-        ),
-        focusedBorder: const OutlineInputBorder(
-          borderSide: BorderSide(color: AppColor.blue800Secondary),
-        ),
-        disabledBorder: const OutlineInputBorder(
-          borderSide: BorderSide(color: AppColor.blue800Secondary),
-        ),
-        border: const OutlineInputBorder(
-          borderSide: BorderSide(color: AppColor.blue800Secondary),
-        ),
-        labelStyle: AppTextStyle.textStyle.copyWith(
-          color: AppColor.black,
-        ),
-        labelText: label,
+        // ラベルテキスト
+        // 背景色を「編集不可」のときだけ変えたい場合は条件分岐する
+        filled: true,
+        fillColor: AppColor.blue50Background,
         prefixIcon: prefixIcon,
+        isDense: true,
+        contentPadding: const EdgeInsets.symmetric(
+          vertical: 14,
+          horizontal: 8,
+        ),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: BorderSide.none,
+        ),
+        hintText: hintText,
+        hintStyle: AppTextStyle.textStyle.copyWith(color: AppColor.black),
       ),
     );
   }
