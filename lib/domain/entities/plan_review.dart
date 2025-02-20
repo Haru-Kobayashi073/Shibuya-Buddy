6a007c6a0cc2715cfb32af81242f01f725af0f8d
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2025-02-20 18:55:00 +0900

    PlanReviewデータモデルを実装

---
+++ lib/domain/entities/plan_review.dart
@@ -0,0 +1,23 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import '../../utils/date_time_converter.dart';
+
+part 'plan_review.freezed.dart';
+part 'plan_review.g.dart';
+
+@freezed
+abstract class PlanReview with _$PlanReview {
+  @JsonSerializable(fieldRename: FieldRename.snake, explicitToJson: true)
+  const factory PlanReview({
+    required String id,
+    required String planId,
+    required String authorId,
+    required int reviewRating,
+    String? content,
+    @DateTimeConverter() required DateTime createdAt,
+    @DateTimeConverter() DateTime? updatedAt,
+  }) = _PlanReview;
+
+  factory PlanReview.fromJson(Map<String, dynamic> json) =>
+      _$PlanReviewFromJson(json);
+}
