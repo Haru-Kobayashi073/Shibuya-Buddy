cb8f2b3557cdbe3931c40ec21a014c9bd64a54cf
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2025-02-20 19:01:48 +0900

    プラン詳細ページの管理Stateモデルをレビュー関連を持った形に変更

--- lib/presentation/plan_detail/plan_detail_page_state.freezed.dart
+++ lib/presentation/plan_detail/plan_detail_page_state.freezed.dart
@@ -20,6 +20,10 @@ mixin _$PlanDetailPageState {
   List<Place> get places => throw _privateConstructorUsedError;
   bool get isBookmarked => throw _privateConstructorUsedError;
   bool get haveUsedPlan => throw _privateConstructorUsedError;
+  List<PlanReview> get reviewsWithContent => throw _privateConstructorUsedError;
+  int get comprehensiveRating => throw _privateConstructorUsedError;
+  int get reviewCount => throw _privateConstructorUsedError;
+  PlanReview? get currentUserReview => throw _privateConstructorUsedError;
 
   /// Create a copy of PlanDetailPageState
   /// with the given fields replaced by the non-null parameter values.
@@ -35,9 +39,17 @@ abstract class $PlanDetailPageStateCopyWith<$Res> {
       _$PlanDetailPageStateCopyWithImpl<$Res, PlanDetailPageState>;
   @useResult
   $Res call(
-      {Plan plan, List<Place> places, bool isBookmarked, bool haveUsedPlan});
+      {Plan plan,
+      List<Place> places,
+      bool isBookmarked,
+      bool haveUsedPlan,
+      List<PlanReview> reviewsWithContent,
+      int comprehensiveRating,
+      int reviewCount,
+      PlanReview? currentUserReview});
 
   $PlanCopyWith<$Res> get plan;
+  $PlanReviewCopyWith<$Res>? get currentUserReview;
 }
 
 /// @nodoc
@@ -59,6 +71,10 @@ class _$PlanDetailPageStateCopyWithImpl<$Res, $Val extends PlanDetailPageState>
     Object? places = null,
     Object? isBookmarked = null,
     Object? haveUsedPlan = null,
+    Object? reviewsWithContent = null,
+    Object? comprehensiveRating = null,
+    Object? reviewCount = null,
+    Object? currentUserReview = freezed,
   }) {
     return _then(_value.copyWith(
       plan: null == plan
@@ -77,6 +93,22 @@ class _$PlanDetailPageStateCopyWithImpl<$Res, $Val extends PlanDetailPageState>
           ? _value.haveUsedPlan
           : haveUsedPlan // ignore: cast_nullable_to_non_nullable
               as bool,
+      reviewsWithContent: null == reviewsWithContent
+          ? _value.reviewsWithContent
+          : reviewsWithContent // ignore: cast_nullable_to_non_nullable
+              as List<PlanReview>,
+      comprehensiveRating: null == comprehensiveRating
+          ? _value.comprehensiveRating
+          : comprehensiveRating // ignore: cast_nullable_to_non_nullable
+              as int,
+      reviewCount: null == reviewCount
+          ? _value.reviewCount
+          : reviewCount // ignore: cast_nullable_to_non_nullable
+              as int,
+      currentUserReview: freezed == currentUserReview
+          ? _value.currentUserReview
+          : currentUserReview // ignore: cast_nullable_to_non_nullable
+              as PlanReview?,
     ) as $Val);
   }
 
@@ -87,6 +119,20 @@ class _$PlanDetailPageStateCopyWithImpl<$Res, $Val extends PlanDetailPageState>
   $PlanCopyWith<$Res> get plan {
     return $PlanCopyWith<$Res>(_value.plan, (value) {
       return _then(_value.copyWith(plan: value) as $Val);
+    });
+  }
+
+  /// Create a copy of PlanDetailPageState
+  /// with the given fields replaced by the non-null parameter values.
+  @override
+  @pragma('vm:prefer-inline')
+  $PlanReviewCopyWith<$Res>? get currentUserReview {
+    if (_value.currentUserReview == null) {
+      return null;
+    }
+
+    return $PlanReviewCopyWith<$Res>(_value.currentUserReview!, (value) {
+      return _then(_value.copyWith(currentUserReview: value) as $Val);
     });
   }
 }
@@ -100,10 +146,19 @@ abstract class _$$PlanDetailPageStateImplCopyWith<$Res>
   @override
   @useResult
   $Res call(
-      {Plan plan, List<Place> places, bool isBookmarked, bool haveUsedPlan});
+      {Plan plan,
+      List<Place> places,
+      bool isBookmarked,
+      bool haveUsedPlan,
+      List<PlanReview> reviewsWithContent,
+      int comprehensiveRating,
+      int reviewCount,
+      PlanReview? currentUserReview});
 
   @override
   $PlanCopyWith<$Res> get plan;
+  @override
+  $PlanReviewCopyWith<$Res>? get currentUserReview;
 }
 
 /// @nodoc
@@ -123,6 +178,10 @@ class __$$PlanDetailPageStateImplCopyWithImpl<$Res>
     Object? places = null,
     Object? isBookmarked = null,
     Object? haveUsedPlan = null,
+    Object? reviewsWithContent = null,
+    Object? comprehensiveRating = null,
+    Object? reviewCount = null,
+    Object? currentUserReview = freezed,
   }) {
     return _then(_$PlanDetailPageStateImpl(
       plan: null == plan
@@ -141,6 +200,22 @@ class __$$PlanDetailPageStateImplCopyWithImpl<$Res>
           ? _value.haveUsedPlan
           : haveUsedPlan // ignore: cast_nullable_to_non_nullable
               as bool,
+      reviewsWithContent: null == reviewsWithContent
+          ? _value._reviewsWithContent
+          : reviewsWithContent // ignore: cast_nullable_to_non_nullable
+              as List<PlanReview>,
+      comprehensiveRating: null == comprehensiveRating
+          ? _value.comprehensiveRating
+          : comprehensiveRating // ignore: cast_nullable_to_non_nullable
+              as int,
+      reviewCount: null == reviewCount
+          ? _value.reviewCount
+          : reviewCount // ignore: cast_nullable_to_non_nullable
+              as int,
+      currentUserReview: freezed == currentUserReview
+          ? _value.currentUserReview
+          : currentUserReview // ignore: cast_nullable_to_non_nullable
+              as PlanReview?,
     ));
   }
 }
@@ -152,8 +227,13 @@ class _$PlanDetailPageStateImpl implements _PlanDetailPageState {
       {required this.plan,
       required final List<Place> places,
       required this.isBookmarked,
-      required this.haveUsedPlan})
-      : _places = places;
+      required this.haveUsedPlan,
+      final List<PlanReview> reviewsWithContent = const [],
+      this.comprehensiveRating = 0,
+      this.reviewCount = 0,
+      this.currentUserReview})
+      : _places = places,
+        _reviewsWithContent = reviewsWithContent;
 
   @override
   final Plan plan;
@@ -169,10 +249,28 @@ class _$PlanDetailPageStateImpl implements _PlanDetailPageState {
   final bool isBookmarked;
   @override
   final bool haveUsedPlan;
+  final List<PlanReview> _reviewsWithContent;
+  @override
+  @JsonKey()
+  List<PlanReview> get reviewsWithContent {
+    if (_reviewsWithContent is EqualUnmodifiableListView)
+      return _reviewsWithContent;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(_reviewsWithContent);
+  }
+
+  @override
+  @JsonKey()
+  final int comprehensiveRating;
+  @override
+  @JsonKey()
+  final int reviewCount;
+  @override
+  final PlanReview? currentUserReview;
 
   @override
   String toString() {
-    return 'PlanDetailPageState(plan: $plan, places: $places, isBookmarked: $isBookmarked, haveUsedPlan: $haveUsedPlan)';
+    return 'PlanDetailPageState(plan: $plan, places: $places, isBookmarked: $isBookmarked, haveUsedPlan: $haveUsedPlan, reviewsWithContent: $reviewsWithContent, comprehensiveRating: $comprehensiveRating, reviewCount: $reviewCount, currentUserReview: $currentUserReview)';
   }
 
   @override
@@ -185,12 +283,28 @@ class _$PlanDetailPageStateImpl implements _PlanDetailPageState {
             (identical(other.isBookmarked, isBookmarked) ||
                 other.isBookmarked == isBookmarked) &&
             (identical(other.haveUsedPlan, haveUsedPlan) ||
-                other.haveUsedPlan == haveUsedPlan));
+                other.haveUsedPlan == haveUsedPlan) &&
+            const DeepCollectionEquality()
+                .equals(other._reviewsWithContent, _reviewsWithContent) &&
+            (identical(other.comprehensiveRating, comprehensiveRating) ||
+                other.comprehensiveRating == comprehensiveRating) &&
+            (identical(other.reviewCount, reviewCount) ||
+                other.reviewCount == reviewCount) &&
+            (identical(other.currentUserReview, currentUserReview) ||
+                other.currentUserReview == currentUserReview));
   }
 
   @override
-  int get hashCode => Object.hash(runtimeType, plan,
-      const DeepCollectionEquality().hash(_places), isBookmarked, haveUsedPlan);
+  int get hashCode => Object.hash(
+      runtimeType,
+      plan,
+      const DeepCollectionEquality().hash(_places),
+      isBookmarked,
+      haveUsedPlan,
+      const DeepCollectionEquality().hash(_reviewsWithContent),
+      comprehensiveRating,
+      reviewCount,
+      currentUserReview);
 
   /// Create a copy of PlanDetailPageState
   /// with the given fields replaced by the non-null parameter values.
@@ -207,7 +321,11 @@ abstract class _PlanDetailPageState implements PlanDetailPageState {
       {required final Plan plan,
       required final List<Place> places,
       required final bool isBookmarked,
-      required final bool haveUsedPlan}) = _$PlanDetailPageStateImpl;
+      required final bool haveUsedPlan,
+      final List<PlanReview> reviewsWithContent,
+      final int comprehensiveRating,
+      final int reviewCount,
+      final PlanReview? currentUserReview}) = _$PlanDetailPageStateImpl;
 
   @override
   Plan get plan;
@@ -217,6 +335,14 @@ abstract class _PlanDetailPageState implements PlanDetailPageState {
   bool get isBookmarked;
   @override
   bool get haveUsedPlan;
+  @override
+  List<PlanReview> get reviewsWithContent;
+  @override
+  int get comprehensiveRating;
+  @override
+  int get reviewCount;
+  @override
+  PlanReview? get currentUserReview;
 
   /// Create a copy of PlanDetailPageState
   /// with the given fields replaced by the non-null parameter values.
